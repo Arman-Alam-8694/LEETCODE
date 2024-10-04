dae8eebff6872b3f269ef3d9013238b668f657a4
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-10-04 15:57:45 +0530

    Time: 365 ms (98.05%) | Memory: 30.4 MB (13.81%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
@@ -1,26 +1,12 @@
 class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
         count=Counter(skill)
-        n=len(skill)
-        minn=min(skill)
-        maxx=max(skill)
-        desired=minn+maxx
+        target=sum(skill)//(len(skill)//2)
         result=0
-        visited=set()
-        if n==2:
-            return minn*maxx
-        else:
-            for i in skill:
-                if i not in visited:
-
-                    if count[i]!=count[desired-i]:
-                        return -1
-                    if i!=desired-i:
-                        result+=(i*(desired-i))*count[i]
-                        visited.add(i)
-                        visited.add(desired-i)
-                    else:
-                        result+=(i*i)*(count[i]//2)
-                        visited.add(i)
-        return result
+    
+        for k,v in count.items():
+            if v!=count[target-k]:
+                return -1
+            result+=(k*(target-k))*v
+        return result//2
         
