7847860d3e0f233927c373b972fabf63d28dc655
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-10-05 11:01:39 +0530

    Time: 45 ms (95.99%) | Memory: 16.7 MB (22.10%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,21 +1,26 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        size=len(s1)
-        count=Counter(s1)
-        windows=Counter(s2[:size])
-        if count==windows:
-            return True
-        print(count)
-        for i in range(1,len(s2)-size+1):
-            print(windows)
-            windows[s2[i-1]]-=1
-            if windows[s2[i-1]]==0:
-                del windows[s2[i-1]]
-            windows[s2[i+size-1]]=windows.get(s2[i+size-1],0)+1
-            if count==windows:
-                return True
-        return False
+        n1 = len(s1)
+        n2 = len(s2)
 
+        s1_counts = [0] * 26
+        s2_counts = [0] * 26
 
-
+        if n1>n2:
+            return False
+        for i in range(n1):
+            s1_counts[ord(s1[i]) - 97] += 1
+            s2_counts[ord(s2[i]) - 97] += 1
         
+        if s1_counts == s2_counts:
+            return True
+        
+        for i in range(n1, n2):
+            s2_counts[ord(s2[i]) - 97] += 1
+            s2_counts[ord(s2[i-n1]) - 97] -= 1
+            if s1_counts == s2_counts:
+                return True
+
+        return False
+        
+            
