005e69f098f6ccd1c94f027bed16516d75c26579
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-10-13 17:35:04 +0530

    Time: 181 ms (90.62%) | Memory: 23.2 MB (79.95%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.py
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.py
@@ -1,28 +1,30 @@
 class Solution:
     def smallestRange(self, nums: List[List[int]]) -> List[int]:
-        min_heap = []
-        max_val = float('-inf')
+        start=float("inf")
+        end=float("inf")
+        rangee=[start,end]
+        k=len(nums)
+        min_dif=float("inf")
+        heap=[(nums[i][0],0,i) for i in range(k)]
+        maxx=max(heap)[0]
+        heapq.heapify(heap)
+        while len(heap)==k:
+            minn,eidx,lidx=heapq.heappop(heap)
+            if maxx-minn<min_dif:
+                rangee=[minn,maxx]
+                start=minn
+                end=maxx
+                min_dif=maxx-minn
+            if len(nums[lidx])==eidx+1:
+                return rangee
+            heapq.heappush(heap,(nums[lidx][eidx+1],eidx+1,lidx))
+            new=nums[lidx][eidx+1]
+            if new>maxx:
+                maxx=new
+        return rangee
+
+
+
+
+
         
-        # Initialize the heap with the first element from each list
-        for i, num_list in enumerate(nums):
-            if num_list:
-                heapq.heappush(min_heap, (num_list[0], i, 0))
-                max_val = max(max_val, num_list[0])
-        
-        result = [float('-inf'), float('inf')]
-        
-        while len(min_heap) == len(nums):
-            min_val, list_index, element_index = heapq.heappop(min_heap)
-            
-            # Update the result if we find a smaller range
-            if max_val - min_val < result[1] - result[0] or (max_val - min_val == result[1] - result[0] and min_val < result[0]):
-                result = [min_val, max_val]
-            
-            # Move to the next element in the list
-            if element_index + 1 < len(nums[list_index]):
-                next_val = nums[list_index][element_index + 1]
-                heapq.heappush(min_heap, (next_val, list_index, element_index + 1))
-                max_val = max(max_val, next_val)
-        
-        return result
-            
