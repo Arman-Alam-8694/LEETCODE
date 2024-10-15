bdcbfe7eafde0724682950755971ebee68999c5e
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-10-15 10:22:50 +0530

    Time: 109 ms (44.48%) | Memory: 17.6 MB (31.34%) - LeetSync

---
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        prev=-1
+        cont=0
+        swaps=0
+        for right in s:
+            if prev=="1" and right=="0":
+                if cont:
+            
+                    swaps+=cont
+                    continue
+                swaps+=1
+                prev="1"
+            elif prev=="1" and right=="1":
+                if cont:
+                    cont+=1
+                else:
+                    cont+=2
+            elif prev=="0" and right=="1":
+                prev="1"
+            else:
+                prev=right
+        return swaps
+
+        #0100011
+        #010110101
+
+        
