41922d935e6aaeba804e76fc39c85382e4ed9b7c
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-10-18 11:35:29 +0530

    Time: 3 ms (100.00%) | Memory: 16.8 MB (30.08%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.py
@@ -1,16 +1,19 @@
 class Solution:
     def countMaxOrSubsets(self, nums: List[int]) -> int:
-        count=0
         maxx=0
+        count={0:1}
         for i in nums:
-            maxx|=i
-       
-        n=len(nums)
-        for i in range(0,2**n):
-            cal=0
-            for j in range(n):
-                if (i&1<<j)!=0:
-                    cal|=nums[j]
-            if cal==maxx:
-                count+=1
-        return count
+            current={}
+            for k,v in count.items():
+                new=i|k
+                maxx=max(maxx,new)
+                if new in current:
+                    current[new]+=v
+                else:
+                    current[new]=v
+            for k,v in current.items():
+                if k in count:
+                    count[k]+=v
+                else:
+                    count[k]=v
+        return count[maxx]
