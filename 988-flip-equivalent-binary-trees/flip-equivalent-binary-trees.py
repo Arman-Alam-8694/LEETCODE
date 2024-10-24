1c4094450ba0c93ae9cf27dbca4919f7b75882e2
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-10-24 14:23:42 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (16.61%) - LeetSync

---
+++ 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.py
@@ -0,0 +1,50 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        lookup=set()
+        lookup2=set()
+        if not root1:
+            if not root2:
+                return True
+            return False
+        if not root2:
+            if not root1:
+                return True
+            return False
+        
+        
+        stack=[root1]
+        while stack:
+            tstack=[]
+            while stack:
+                nodes=stack.pop()
+                if nodes.right:
+                    lookup.add((nodes.val,nodes.right.val))
+                    tstack.append(nodes.right)
+                if nodes.left:
+                    lookup.add((nodes.val,nodes.left.val))
+                    tstack.append(nodes.left)
+            stack=tstack
+        stack=[root2]
+        while stack:
+            tstack=[]
+            while stack:
+                nodes=stack.pop()
+                if nodes.right:
+                    lookup2.add((nodes.val,nodes.right.val))
+                    tstack.append(nodes.right)
+                if nodes.left:
+                    lookup2.add((nodes.val,nodes.left.val))
+                    tstack.append(nodes.left)
+            stack=tstack
+        if lookup==lookup2:
+            return True
+        return False
+        
+
+
