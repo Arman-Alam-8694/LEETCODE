678721363b772275c9638148a0daa71b7e414e45
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-10-30 23:23:38 +0530

    Time: 15 ms (93.02%) | Memory: 16.8 MB (62.07%) - LeetSync

--- 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.py
+++ 1766-minimum-number-of-removals-to-make-mountain-array/minimum-number-of-removals-to-make-mountain-array.py
@@ -1,48 +1,25 @@
 class Solution:
-    def getLongestIncreasingSubsequenceLength(self, v: List[int]) -> List[int]:
-        lis_len = [1] * len(v)
-        lis = [v[0]]
-
-        for i in range(1, len(v)):
-            index = self.lowerBound(lis, v[i])
-
-            # Add to the list if v[i] is greater than the last element
-            if index == len(lis):
-                lis.append(v[i])
-            else:
-                # Replace the element at index with v[i]
-                lis[index] = v[i]
-
-            lis_len[i] = len(lis)
-
-        return lis_len
-
-    # Returns the index of the first element which is equal to or greater than target.
-    def lowerBound(self, lis: List[int], target: int) -> int:
-        left, right = 0, len(lis)
-        while left < right:
-            mid = left + (right - left) // 2
-            if lis[mid] < target:
-                left = mid + 1
-            else:
-                right = mid
-        return left
-
     def minimumMountainRemovals(self, nums: List[int]) -> int:
-        N = len(nums)
+        n = len(nums)
 
-        lis_length = self.getLongestIncreasingSubsequenceLength(nums)
+        def getLis(arr):
+            res = [0] * n
+            stack = []
+            for i in range(n):
+                if stack and stack[-1] >= arr[i]:
+                    j = bisect_left(stack, arr[i])
+                    stack[j] = arr[i]
+                    res[i] = j+1
+                else:
+                    stack.append(arr[i])
+                    res[i] = len(stack)
+            return res
+        
+        left, right = getLis(nums), getLis(nums[::-1])[::-1]
+        #print(left, right)
+        ans = float('inf')
+        for i in range(n):
+            if left[i] > 1 and right[i] > 1:
+                ans = min(ans, n - (left[i] + right[i]-1))
 
-        nums.reverse()
-        lds_length = self.getLongestIncreasingSubsequenceLength(nums)
-        # Reverse the length array to correctly depict the lenght of longest decreasing subsequnec for each index.
-        lds_length.reverse()
-
-        min_removals = float("inf")
-        for i in range(N):
-            if lis_length[i] > 1 and lds_length[i] > 1:
-                min_removals = min(
-                    min_removals, N - lis_length[i] - lds_length[i] + 1
-                )
-
-        return min_removals
+        return ans
