fdf2106677951988e57867b943795d8df693dd89
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-11-01 09:52:15 +0530

    Time: 163 ms (98.20%) | Memory: 18.5 MB (73.10%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
@@ -1,20 +1,17 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
-        count=0
+        count=1
         chr=None
         result=[]
         i=0
-        while i<len(s):
-            if chr!=s[i]:
-                count=0
-                chr=s[i]
-                continue
-            count+=1
-            if count==3:
-                count-=1
-                i+=1
-                continue
-            result.append(chr)
-            i+=1
+        prev=None
+        for i in s:
+            if prev==i:
+                count+=1
+            else:
+                count=1
+            prev=i
+            if count<3:
+                result.append(prev)
         return "".join(result)
         
