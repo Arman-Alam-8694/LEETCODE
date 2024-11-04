7cb5b8f768e119f8209490a49cd651df612199d7
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-11-04 13:11:44 +0530

    Time: 311 ms (20.57%) | Memory: 27.8 MB (14.76%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.py
+++ 3451-string-compression-iii/string-compression-iii.py
@@ -1,26 +1,28 @@
 class Solution:
     def compressedString(self, word: str) -> str:
-        result=""
+        result=[]
         dictt={}
         for i in word:
             if i not in dictt:
                 if len(dictt)==1:
                     chr,count=list(dictt.items())[0]
-                    result+=str(count)+chr
+                    result.append(str(count))
+                    result.append(chr)
                     dictt.clear()
                 dictt[i]=1
             else:
                 dictt[i]+=1
                 if dictt[i]==9:
                     chr,count=list(dictt.items())[0]
-                    result+=str(count)+chr
+                    result.append(str(count))
+                    result.append(chr)
                     dictt.clear()
                     continue
         if dictt:
             chr,count=list(dictt.items())[0]
-            result+=str(count)+chr
-            
-        return result
+            result.append(str(count))
+            result.append(chr)
+        return "".join(result)
             
             
         
