53394b0f1af5dfb4cb34b44d36cd5fb310fc442d
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-11-06 12:52:59 +0530

    Time: 15 ms (39.09%) | Memory: 16.7 MB (23.35%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.py
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.py
@@ -43,14 +43,14 @@ class Solution:
             return count
 
 
-        # sortedd=True
+        sortedd=True
         n=len(nums)
-        # for i in range(n-1):
-        #     if not nums[i]<=nums[i+1]:
-        #         sortedd=False
-        #         break
-        # if sortedd==True or n==1:
-        #     return True
+        for i in range(n-1):
+            if not nums[i]<=nums[i+1]:
+                sortedd=False
+                break
+        if sortedd==True or n==1:
+            return True
         result=[]
         prev=None
         minn=-1
