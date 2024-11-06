61910a24b2525a66a33f7b53dd5a3eddc76fafb3
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-11-06 12:31:42 +0530

    Time: 19 ms (38.18%) | Memory: 16.7 MB (43.17%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.py
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.py
@@ -1,42 +1,85 @@
 class Solution:
     def canSortArray(self, nums: List[int]) -> bool:
+        # sortedd=True
+        # n=len(nums)
+        # for i in range(n-1):
+        #     if not nums[i]<=nums[i+1]:
+        #         sortedd=False
+        #         break
+        # if sortedd==True:
+        #     return True
+          
+        # dictt={}
+        # for i in nums:
+        #     count=0
+        #     for j in range(0,11):
+        #         if 1<<j&i:
+        #             count+=1
+        #     dictt[i]=count
+        # print(dictt)
+        # while True:
+        #     sortedd=True
+        #     change=False
+        #     for i in range(len(nums)-1):
+        #         if nums[i]>nums[i+1]:
+        #             if dictt[nums[i]]==dictt[nums[i+1]]:
+        #                 nums[i],nums[i+1]=nums[i+1],nums[i]
+        #                 change=True
+        #                 if i!=0 and nums[i]<nums[i-1]:
+        #                     sortedd=False
+
+        #             else:
+        #                 sortedd=False
+        #     if sortedd:
+        #         return True
+        #     else:
+        #         if change==False:
+        #             return False
+        def bitscount(i):
+            count=0
+            for j in range(0,11):
+                if 1<<j&i:
+                    count+=1
+            return count
+
+
         sortedd=True
         n=len(nums)
         for i in range(n-1):
             if not nums[i]<=nums[i+1]:
                 sortedd=False
                 break
-        if sortedd==True:
+        if sortedd==True or n==1:
             return True
-          
+        result=[]
         dictt={}
-        for i in nums:
-            count=0
-            for j in range(0,11):
-                if 1<<j&i:
-                    count+=1
-            dictt[i]=count
-        print(dictt)
-        while True:
-            sortedd=True
-            change=False
-            for i in range(len(nums)-1):
-                if nums[i]>nums[i+1]:
-                    if dictt[nums[i]]==dictt[nums[i+1]]:
-                        nums[i],nums[i+1]=nums[i+1],nums[i]
-                        change=True
-                        if i!=0 and nums[i]<nums[i-1]:
-                            sortedd=False
+        prev=None
+        minn=-1
+        maxx=-1
+        for i in range(n):
+            digit=nums[i]
+            bits=bitscount(digit)
+            if not prev:
+                minn,maxx=digit,digit
+            elif prev and prev==bits:
+                if digit<minn:
+                    minn=digit
+                elif digit>maxx:
+                    maxx=digit
+            elif prev and prev!=bits:
+                result.append([minn,maxx])
+                minn,maxx=digit,digit
+            prev=bits
+        result.append([minn,maxx])
+        for i in range(len(result)-1):
+            if not result[i][1]<result[i+1][0]:
+                return False
 
-                    else:
-                        sortedd=False
-            # print(sortedd,change)
-            # print(nums)
-            if sortedd:
-                return True
-            else:
-                if change==False:
-                    return False
+        return True
+
+
+
+
 
 
     
