c025c6c955d07d7442d20d614ffcb482af98c2ff
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-11-08 11:44:34 +0530

    Time: 2533 ms (5.36%) | Memory: 32.5 MB (83.14%) - LeetSync

---
+++ 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.py
@@ -0,0 +1,36 @@
+class Solution:
+    def getMaximumXor(self, nums, maximumBit):
+        def getmax(tmax,maxx):
+            nmax=0
+            temp=""
+            move=0
+            # print(tmax)
+            # print("tmax",bin(tmax))
+            while True:
+                if tmax&(1<<move):
+                    temp+="0"
+                else:
+                    temp+="1"
+                # print(temp)
+                comp=int(temp[::-1],2)
+                # print(comp)
+                if comp>=maxx:
+                    break
+                else:
+                    nmax=comp
+                move+=1
+            return nmax
+        total_xor=0
+        for i in nums:
+            total_xor^=i
+        result=[]
+        maxx=2**(maximumBit)
+        result.append(getmax(total_xor,maxx))
+        for i in range(-1,-1*(len(nums)),-1):
+            total_xor^=nums[i]
+            # print(total_xor)
+            result.append(getmax(total_xor,maxx))
+        # result.append(getmax(total_xor,maxx))
+        return result
+
+        
