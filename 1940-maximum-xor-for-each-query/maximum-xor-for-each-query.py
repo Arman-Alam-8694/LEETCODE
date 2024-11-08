7eea96d0015eb46d12ffd30d3422d350492cc3d1
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-11-08 12:37:21 +0530

    Time: 22 ms (99.11%) | Memory: 32.7 MB (79.65%) - LeetSync

--- 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.py
+++ 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.py
@@ -1,36 +1,45 @@
 class Solution:
     def getMaximumXor(self, nums, maximumBit):
-        def getmax(tmax,maxx):
-            nmax=0
-            temp=""
-            move=0
-            # print(tmax)
-            # print("tmax",bin(tmax))
-            while True:
-                if tmax&(1<<move):
-                    temp+="0"
-                else:
-                    temp+="1"
-                # print(temp)
-                comp=int(temp[::-1],2)
-                # print(comp)
-                if comp>=maxx:
-                    break
-                else:
-                    nmax=comp
-                move+=1
-            return nmax
-        total_xor=0
-        for i in nums:
-            total_xor^=i
-        result=[]
-        maxx=2**(maximumBit)
-        result.append(getmax(total_xor,maxx))
-        for i in range(-1,-1*(len(nums)),-1):
-            total_xor^=nums[i]
-            # print(total_xor)
-            result.append(getmax(total_xor,maxx))
+        # def getmax(tmax,maxx):
+        #     # nmax=0
+        #     # temp=""
+        #     # move=0
+        #     # # print(tmax)
+        #     # # print("tmax",bin(tmax))
+        #     # while True:
+        #     #     if tmax&(1<<move):
+        #     #         temp+="0"
+        #     #     else:
+        #     #         temp+="1"
+        #     #     # print(temp)
+        #     #     comp=int(temp[::-1],2)
+        #     #     # print(comp)
+        #     #     if comp>=maxx:
+        #     #         break
+        #     #     else:
+        #     #         nmax=comp
+        #     #     move+=1
+        #     comp=int(bin(~tmax)[3:],2)
+        #     return min(comp,maxx-1)
+       
+        # total_xor=0
+        # for i in nums:
+        #     total_xor^=i
+        # result=[]
+        # maxx=2**(maximumBit)
         # result.append(getmax(total_xor,maxx))
-        return result
+        # for i in range(-1,-1*(len(nums)),-1):
+        #     total_xor^=nums[i]
+        #     # print(total_xor)
+        #     result.append(getmax(total_xor,maxx))
+        # # result.append(getmax(total_xor,maxx))
+        # return result
+        result=[]
+        mask=(1<<maximumBit)-1
+        xor=0
+        for i in nums:
+            xor^=i
+            result.append(xor^mask)
+        return result[::-1]
 
         
