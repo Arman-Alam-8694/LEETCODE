85a4c688b93b4b8414e723cd73b3101d551f0074
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-11-09 13:35:28 +0530

    Time: 3 ms (42.55%) | Memory: 16.5 MB (69.70%) - LeetSync

---
+++ 3394-minimum-array-end/minimum-array-end.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minEnd(self, n: int, x: int) -> int:
+        result=x
+        n-=1
+        mask=1
+        while n>0:
+            if not (x&mask):
+                result=result|(n&1)*mask
+                n>>=1
+            mask<<=1
+        return result
+
+
+        
