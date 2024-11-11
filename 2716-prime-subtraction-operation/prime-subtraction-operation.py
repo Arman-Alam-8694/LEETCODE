79abc6224c4d6c03a070ff4c5f112edcfe00f013
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-11-11 08:40:22 +0530

    Time: 34 ms (54.19%) | Memory: 17 MB (12.98%) - LeetSync

--- 2716-prime-subtraction-operation/prime-subtraction-operation.py
+++ 2716-prime-subtraction-operation/prime-subtraction-operation.py
@@ -1,48 +1,44 @@
 class Solution:
     def primeSubOperation(self, nums: List[int]) -> bool:
-        primes=[]
-        for i in range(2,1000):
-         
-            isPrime=True
-            for k in range(2,int(i**0.5)+1):
-                if i%k==0:
-                    isPrime=False
-                    break
-            if isPrime:
-                primes.append(i)
-        # print(primes)
-        def binsearch(find,primes):
-            length=len(primes)
-            left=0
-            right=length-1
-            while left<=right:
-                mid=(left+right)//2
-                if primes[mid]>find:
-                    right=mid-1
-                elif primes[mid]==find:
+        # Sieve of Eratosthenes for generating primes up to 1000
+        def generatePrimes(n):
+            sieve = [True] * (n + 1)
+            sieve[0] = sieve[1] = False
+            
+            for i in range(2, int(n ** 0.5) + 1):
+                if sieve[i]:
+                    # Mark all multiples as non-prime
+                    for j in range(i * i, n + 1, i):
+                        sieve[j] = False
+            
+            return [i for i in range(2, n + 1) if sieve[i]]
+
+        primes = generatePrimes(1000)
+        
+        def binsearch(find, primes):
+            length = len(primes)
+            left = 0
+            right = length - 1
+            while left <= right:
+                mid = (left + right) // 2
+                if primes[mid] > find:
+                    right = mid - 1
+                elif primes[mid] == find:
                     return primes[mid]
                 else:
-                    left=mid+1
-            if len(primes)>left>=0:
+                    left = mid + 1
+            if len(primes) > left >= 0:
                 return primes[left]
             return -1
         
-        for i in range(len(nums)-2,-1,-1):
-            
-            if nums[i]>=nums[i+1]:
-             
-                find=(nums[i]-nums[i+1])+1
-                if find<=0:
+        for i in range(len(nums) - 2, -1, -1):
+            if nums[i] >= nums[i + 1]:
+                find = (nums[i] - nums[i + 1]) + 1
+                if find <= 0:
                     return False
-                sub=binsearch(find,primes)
-                # print(find,sub,nums[i])
-                if sub>=nums[i] or sub==-1:
+                sub = binsearch(find, primes)
+                if sub >= nums[i] or sub == -1:
                     return False
-                nums[i]=nums[i]-sub
-                # print(nums)
-               
-
-
-            
+                nums[i] = nums[i] - sub
+        
         return True
-
