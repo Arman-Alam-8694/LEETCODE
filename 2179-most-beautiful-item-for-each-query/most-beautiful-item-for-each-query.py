ca3c2f8348db71f925d936fb476dc3cc6d6f4cd2
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-11-12 07:15:09 +0530

    Time: 131 ms (81.71%) | Memory: 66.7 MB (59.68%) - LeetSync

--- 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.py
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.py
@@ -1,3 +1,44 @@
+#original solution
+
+# class Solution:
+#     def maximumBeauty(self, items: List[List[int]], queries: List[int]) -> List[int]:
+#         def bins(number,items):
+#             left=0
+#             right=len(items)-1
+#             while left<=right:
+#                 mid=(left+right)//2
+#                 if items[mid][0]>number:
+#                     right=mid-1
+#                 else:
+#                     left=mid+1
+#             return left
+#         mapp={}
+#         items.sort()
+#         maxx=float('-inf')
+#         for i,j in items:
+           
+#             if i not in mapp:
+#                 maxx=max(maxx,j)
+#                 mapp[i]=maxx
+#             else:
+#                 maxx=max(maxx,j)
+#                 mapp[i]=maxx
+        
+#         result=[]
+#         for i in queries:
+#             if i in mapp:
+#                 result.append(mapp[i])
+#             else:
+#                 idx=bins(i,items)
+
+#                 if idx==0:
+#                     result.append(0)
+#                 else:
+#                     value=items[idx-1][0]
+#                     result.append(mapp[value])
+#         return result
+
+
 from bisect import bisect_right
 from typing import List
 
@@ -25,3 +66,7 @@ class Solution:
                 result.append(0)  # No valid items for query price
                 
         return result
+
+
+
+        
