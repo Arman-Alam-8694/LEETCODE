c0dd8d635a0eb8f3843a28d82448f80e44369284
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-11-12 07:12:32 +0530

    Time: 160 ms (64.63%) | Memory: 66.5 MB (60.22%) - LeetSync

--- 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.py
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.py
@@ -1,12 +1,11 @@
 class Solution:
     def maximumBeauty(self, items: List[List[int]], queries: List[int]) -> List[int]:
-        def bins(number,map_temp):
-
+        def bins(number,items):
             left=0
-            right=len(map_temp)-1
+            right=len(items)-1
             while left<=right:
                 mid=(left+right)//2
-                if map_temp[mid][0]>number:
+                if items[mid][0]>number:
                     right=mid-1
                 else:
                     left=mid+1
@@ -22,20 +21,19 @@ class Solution:
             else:
                 maxx=max(maxx,j)
                 mapp[i]=maxx
-        map_temp=[]
-        for i,j in mapp.items():
-            map_temp.append((i,j))
         
         result=[]
         for i in queries:
             if i in mapp:
                 result.append(mapp[i])
             else:
-                idx=bins(i,map_temp)
+                idx=bins(i,items)
+
                 if idx==0:
                     result.append(0)
                 else:
-                    result.append(map_temp[idx-1][1])
+                    value=items[idx-1][0]
+                    result.append(mapp[value])
         return result
 
         
