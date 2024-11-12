4ceeec2dab10df8ef33077d1c3c2b85e5fe5be96
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-11-12 15:39:24 +0530

    Time: 1697 ms (47.38%) | Memory: 55.4 MB (58.24%) - LeetSync

---
+++ 204-count-primes/count-primes.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        primes=[True]*n
+        count=0
+        if n==1 or n==0:
+            return 0
+        for i in range(2,n):
+            if primes[i]:
+                count+=1
+                for j in range(i*i,n,i):
+                    primes[j]=False
+      
+        return count
+
+        
