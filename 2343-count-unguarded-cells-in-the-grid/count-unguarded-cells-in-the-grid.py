f34d58c030bf85d6fafccfd255227f9c38253db9
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-11-21 10:40:35 +0530

    Time: 637 ms (22.00%) | Memory: 42.3 MB (64.91%) - LeetSync

--- 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.py
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.py
@@ -1,9 +1,6 @@
 class Solution:
     def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
-        up=0
-        down=m-1
-        left=0
-        right=n-1
+        
         wallss=set()
         guardss=set()
         seen=set()
@@ -17,40 +14,54 @@ class Solution:
             wallss.add((a,b))
         for a,b in guards:
             guardss.add((a,b))
-
+        direction=[(1,0),(-1,0),(0,1),(0,-1)]
         for x,y in guards:
-            for x1 in range(x-1,up-1,-1):
-                if (x1,y) in seen:
-                    continue
-                elif (x1,y) not in wallss and (x1,y) not in guardss:
-                    rem-=1
-                    seen.add((x1,y))
-                else:
-                    break
-            for x1 in range(x+1,down+1):
-                if (x1,y) in seen:
-                    continue
-                elif (x1,y) not in wallss and (x1,y) not in guardss:
-                    rem-=1
-                    seen.add((x1,y))
-                else:
-                    break
-            for y1 in range(y+1,right+1):
-                if (x,y1) in seen:
-                    continue
-                elif (x,y1) not in wallss and (x,y1) not in guardss:
-                    rem-=1
-                    seen.add((x,y1))
-                else:
-                    break
-            for y1 in range(y-1,left-1,-1):
-                if (x,y1) in seen:
-                    continue
-                elif (x,y1) not in wallss and (x,y1) not in guardss:
-                    rem-=1
-                    seen.add((x,y1))
-                else:
-                    break
+            for dr,dc in direction:
+                r,c=x,y
+                while 0<=r+dr<m and 0<=c+dc<n:
+                    new=(r+dr,c+dc)
+                    if new in wallss or new in guardss:
+                        break
+                    elif new not  in seen:
+                        rem-=1
+                        seen.add(new) 
+                    r+=dr
+                    c+=dc
+                    
+                    
+
+            # for x1 in range(x-1,up-1,-1):
+            #     if (x1,y) in seen:
+            #         continue
+            #     elif (x1,y) not in wallss and (x1,y) not in guardss:
+            #         rem-=1
+            #         seen.add((x1,y))
+            #     else:
+            #         break
+            # for x1 in range(x+1,down+1):
+            #     if (x1,y) in seen:
+            #         continue
+            #     elif (x1,y) not in wallss and (x1,y) not in guardss:
+            #         rem-=1
+            #         seen.add((x1,y))
+            #     else:
+            #         break
+            # for y1 in range(y+1,right+1):
+            #     if (x,y1) in seen:
+            #         continue
+            #     elif (x,y1) not in wallss and (x,y1) not in guardss:
+            #         rem-=1
+            #         seen.add((x,y1))
+            #     else:
+            #         break
+            # for y1 in range(y-1,left-1,-1):
+            #     if (x,y1) in seen:
+            #         continue
+            #     elif (x,y1) not in wallss and (x,y1) not in guardss:
+            #         rem-=1
+            #         seen.add((x,y1))
+            #     else:
+            #         break
 
        
 
