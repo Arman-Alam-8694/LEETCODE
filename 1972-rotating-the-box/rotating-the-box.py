04ceb796ccbddd923ad10ccdec7fa7e20dd18d55
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-11-23 13:32:10 +0530

    Time: 1951 ms (31.33%) | Memory: 29.1 MB (43.28%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.py
+++ 1972-rotating-the-box/rotating-the-box.py
@@ -2,6 +2,7 @@ class Solution:
     def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
         rows=len(box)
         cols=len(box[0])
+        row=rows-1
         result=[["."]*rows for _ in range(cols)]
         
         for i in range(rows):
@@ -10,7 +11,7 @@ class Solution:
 
             place=None
             for j in range(cols):
-                result[j][(rows-1)-i]=box[i][j]
+                result[j][row-i]=box[i][j]
             
                 if box[i][j]=="#" and (candidaterow==None and candidatecol==None):
                     candidaterow=i
@@ -19,8 +20,8 @@ class Solution:
                     if candidaterow!=None and candidatecol!=None:
                         box[i][j]="#"
                         box[candidaterow][candidatecol]="."
-                        result[j][(rows-1)-i]=box[i][j]
-                        result[candidatecol][(rows-1)-candidaterow]=box[candidaterow][candidatecol]
+                        result[j][row-i]=box[i][j]
+                        result[candidatecol][row-candidaterow]=box[candidaterow][candidatecol]
                         candidatecol+=1
                 elif box[i][j]=="*":
 
