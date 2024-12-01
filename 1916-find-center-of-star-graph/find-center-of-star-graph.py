5bde3a1d1889b98db1757883708d61b5c04941a7
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-12-01 17:30:47 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (99.32%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.py
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.py
@@ -1,15 +1,11 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        mapp={}
-        maxx=-1
-        for u,v in edges:
-            mapp[u]=mapp.get(u,0)+1
-            mapp[v]=mapp.get(v,0)+1
-            if mapp[u]>maxx:
-                maxx=u
-            if mapp[v]>maxx:
-                maxx=v
-        return maxx
+        u=edges[0][0]
+        v=edges[0][1]
+        if u in edges[1]:
+            return u
+        else:
+            return v
         
 
             
