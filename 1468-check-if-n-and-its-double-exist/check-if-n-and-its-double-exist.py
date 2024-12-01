ddcfce5bd73f383e3fea73d2e184f2daa04263fa
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-12-01 11:40:12 +0530

    Time: 3 ms (57.43%) | Memory: 17.2 MB (5.25%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -3,13 +3,13 @@ class Solution:
         mapp={}
         n=len(arr)
         for i in range(n):
-            mapp[arr[i]]=i
-        for i in range(n):
-            number=arr[i]*2
+            number=(arr[i])/2
+            number1=(arr[i])*2
             if number in mapp:
-                if i!=mapp[number]:
-                    return True
+                return True
+            elif number1 in mapp:
+                return True
+            mapp[arr[i]]=i
         return False
-
 
         
