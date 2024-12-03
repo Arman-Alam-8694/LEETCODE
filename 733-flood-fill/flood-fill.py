059c8261c8b45b50f7a3eb27b4f9615d591e9ecb
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-12-03 14:09:49 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (6.15%) - LeetSync

---
+++ 733-flood-fill/flood-fill.py
@@ -0,0 +1,32 @@
+from collections import deque
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+
+        original=image[sr][sc]
+
+        if original==color:
+            return image
+        Queue=deque([[sr,sc]])
+        lower=len(image)-1
+        right=len(image[0])-1
+        visited=set()
+        while Queue:
+            nr,nc=Queue.pop()
+            visited.add((nr,nc))
+            if image[nr][nc]==original :
+                image[nr][nc]=color
+                if nr-1>=0:
+                    if (nr-1,nc) not in visited:
+                        Queue.append((nr-1,nc))
+
+                if nr+1<=lower:
+                    if (nr+1,nc) not in visited:
+                        Queue.append((nr+1,nc))
+                if nc+1<=right:
+                    if (nr,nc+1) not in visited:
+                        Queue.append((nr,nc+1))
+                if nc-1>=0:
+                    if (nr,nc-1) not in visited:
+                        Queue.append((nr,nc-1))
+        return image
+        
