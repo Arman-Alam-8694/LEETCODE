a259bebe2ec5c92008281767abb70d6bae892ed6
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-12-04 05:53:41 +0530

    Time: 55 ms (95.83%) | Memory: 17.5 MB (50.24%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
@@ -1,19 +1,11 @@
 class Solution:
     def canMakeSubsequence(self, str1: str, str2: str) -> bool:
-        alphabet="abcdefghijklmnopqrstuvwxyz"
-        mapp={}
-      
         s2=len(str2)
-        a=len(alphabet)
-
-        for i in range(a):
-            mapp[alphabet[i]]=alphabet[(i+1)%a]
-
         left=0
-        for char in str1:
-            if char==str2[left]:
+        for charr in str1:
+            if charr==str2[left]:
                 left+=1
-            elif mapp[char]==str2[left]:
+            elif (chr(ord(charr)+1)==str2[left]) or (str2[left]=="a" and charr=="z"):
                 left+=1
             if left>=s2:
                 return True
