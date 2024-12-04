ee86521122be4e511541ef6a623e8e6b3e42692c
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-12-04 06:02:28 +0530

    Time: 38 ms (98.61%) | Memory: 17.9 MB (5.69%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
@@ -4,14 +4,22 @@ class Solution:
         s2=len(str2)
         if len(str1)==len(str2):
             for charr in str1:
-                if (charr==str2[left]) or (chr(ord(charr)+1)==str2[left]) or (str2[left]=="a" and charr=="z"):
+                if charr==str2[left]:
+                    left+=1
+                elif  chr(ord(charr)+1)==str2[left]:
+                    left+=1
+                elif (str2[left]=="a" and charr=="z"):
                     left+=1
                 else:
                     return False
             return True
    
         for charr in str1:
-            if (charr==str2[left]) or (chr(ord(charr)+1)==str2[left]) or (str2[left]=="a" and charr=="z"):
+            if charr==str2[left]:
+                left+=1
+            elif  chr(ord(charr)+1)==str2[left]:
+                left+=1
+            elif (str2[left]=="a" and charr=="z"):
                 left+=1
             if left>=s2:
                 return True
