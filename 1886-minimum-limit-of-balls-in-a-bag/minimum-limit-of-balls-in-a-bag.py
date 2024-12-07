a7f503a5cb9345e4a11e56a0ce6a37a1c8637c00
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-12-07 21:35:48 +0530

    Time: 1047 ms (15.79%) | Memory: 29.5 MB (19.09%) - LeetSync

--- 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.py
+++ 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.py
@@ -1,13 +1,22 @@
 class Solution:
     def minimumSize(self, nums: List[int], maxOperations: int) -> int:
-        u = sum(nums)
-        b = maxOperations
-        splits = [max(n*b//u, 1) for n in nums]
-        heap = [(-1 * ((n+split-1)//split), n, split) for n, split in zip(nums, splits)]
-        heapq.heapify(heap)
-        for _ in range(maxOperations + len(nums) - sum(splits)):
-            item = heapq.heappop(heap)
-            _, n, split = item
-            num = (n+split)//(split+1)
-            heapq.heappush(heap, (-num, n, split+1))
-        return - heap[0][0]
+        def possible(mid,nums,k):
+            count=0
+            for i in range(len(nums)):
+                quot,rem=divmod(nums[i],mid)
+                if rem==0:
+                    count+=quot-1
+                else:
+                    count+=quot
+                if count>k:
+                    return False
+            return True
+        left=1
+        right=max(nums)
+        while left<=right:
+            mid=(left+right)//2
+            if possible(mid,nums,maxOperations):
+                right=mid-1
+            else:
+                left=mid+1
+        return left
