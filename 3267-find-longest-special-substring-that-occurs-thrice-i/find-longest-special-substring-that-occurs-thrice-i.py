022ee97fef45c3165e1185b26394123e14afd416
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-12-10 14:16:29 +0530

    Time: 19 ms (54.32%) | Memory: 17.5 MB (5.52%) - LeetSync

--- 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.py
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.py
@@ -1,34 +1,30 @@
 class Solution:
     def maximumLength(self, s: str) -> int:
-        def check(k,s):
-            stringg="abcdefghijklmnopqrstuvwxyz"
-            for ch in stringg:
-                tcount=0
-                count=0
-                for right in range(len(s)):
-                    if s[right]==ch:
-                        tcount+=1
-                    if s[right]!=ch:
-                        tcount=0
-                    while tcount>k:
-                        tcount-=1
-                    if tcount!=0 and tcount%k==0:
-                        count+=1
-                    if count==3:
-                        return True
+        n = len(s)
+        l, r = 1, n
 
-            return False
-        answer=-1
-        n=len(s)
-        left=1
-        right=n
-        while left<=right:
-            mid=(left+right)//2
-            if check(mid,s):
-                answer=max(mid,answer)
-                left=mid+1
+        if not self.helper(s, n, l):
+            return -1
+
+        while l + 1 < r:
+            mid = (l + r) // 2
+            if self.helper(s, n, mid):
+                l = mid
             else:
-                right=mid-1
-        return answer
+                r = mid
 
-        
+        return l
+
+    def helper(self, s: str, n: int, x: int) -> bool:
+        cnt = [0] * 26
+        p = 0
+
+        for i in range(n):
+            while s[p] != s[i]:
+                p += 1
+            if i - p + 1 >= x:
+                cnt[ord(s[i]) - ord('a')] += 1
+            if cnt[ord(s[i]) - ord('a')] > 2:
+                return True
+
+        return False
