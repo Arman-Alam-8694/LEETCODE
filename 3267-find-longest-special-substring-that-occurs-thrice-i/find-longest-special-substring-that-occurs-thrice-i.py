47c017a8b479f9ddc5539238568380f9aa4ec6ff
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-12-10 12:07:19 +0530

    Time: 267 ms (5.13%) | Memory: 17.4 MB (9.27%) - LeetSync

---
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.py
@@ -0,0 +1,41 @@
+class Solution:
+    def maximumLength(self, s: str) -> int:
+        def check(k,s):
+            stringg="abcdefghijklmnopqrstuvwxyz"
+            for ch in stringg:
+                left=0
+                tcount=0
+                count=0
+                right=0
+                while right<len(s):
+                    if s[right]==ch:
+                        tcount+=1
+                    if s[right]!=ch:
+                        tcount=0
+                        left=right+1
+                    while tcount>k:
+                        tcount-=1
+                        left-=1
+                    if tcount!=0 and tcount%k==0:
+                        count+=1
+                    if count==3:
+                        return True
+
+                    right+=1
+            return False
+
+           
+        answer=-1
+        n=len(s)
+        left=1
+        right=n
+        while left<=right:
+            mid=(left+right)//2
+            if check(mid,s):
+                answer=max(mid,answer)
+                left=mid+1
+            else:
+                right=mid-1
+        return answer
+
+        
