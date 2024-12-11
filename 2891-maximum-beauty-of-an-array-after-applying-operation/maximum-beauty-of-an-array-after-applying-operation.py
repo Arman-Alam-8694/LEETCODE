32ba3b9323a8ae8c97520e047791ac454dda6e0b
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-12-11 13:51:05 +0530

    Time: 117 ms (96.58%) | Memory: 28.1 MB (39.52%) - LeetSync

--- 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.py
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.py
@@ -2,9 +2,8 @@ class Solution:
     def maximumBeauty(self, nums: List[int], k: int) -> int:
         nums.sort()
         left=0
-        maxx=float("-inf")
-        for right in range(len(nums)):
-            while  nums[right]-nums[left]>k*2:
+        n=len(nums)
+        for right in range(n):
+            if nums[right]-nums[left]>k*2:
                 left+=1
-            maxx=max(maxx,(right-left+1))
-        return maxx
+        return n-left
