e80ea6d0ae1b6e9ed76674151061bfa852d11398
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-12-13 12:46:50 +0530

    Time: 55 ms (98.18%) | Memory: 29.1 MB (98.71%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.py
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.py
@@ -2,16 +2,18 @@ import heapq
 class Solution:
     def findScore(self, nums: List[int]) -> int:
         #simple sort solution(faster than heap)
-        new=[(j,i) for i,j in enumerate(nums)]
-        new.sort()
-        seen=set()
-        score=0
-        for val,idx in new:
-            if idx not in seen:
-                score+=val
-                seen.add(idx-1)
-                seen.add(idx+1)
-        return score
+        # new=[(j,i) for i,j in enumerate(nums)]
+        # new.sort()
+        # print(new)
+        # seen=set()
+        # score=0
+        # for val,idx in new:
+        #     if idx not in seen:
+        #         print(val)
+        #         score+=val
+        #         seen.add(idx-1)
+        #         seen.add(idx+1)
+        # return score
         
         
         
@@ -29,36 +31,23 @@ class Solution:
         #         seen.add(idx+1)
         # return score
 
-        #simple pointer
-        score=0
-        skip=False
-        prev=None
-        # 18 9 7 6 1 3 4 5
-        stack=[]
+        #monotonic decreasing stack
+
+        stk = []
+        res = 0
         for i in range(len(nums)):
-            if skip==True:
-                continue
-            if not prev and nums[i]<=nums[i+1]:
-                score+=nums[i]
-                skip=True
-            elif prev and nums[i]<=nums[i+1]:
-                score+=nums[i]
-                skip=True
-                if not prev and stack:
-                    stack.pop()
+            if not stk or nums[i] < stk[-1]:
+                stk.append(nums[i])
+            else:
+                while stk:
+                    res += stk.pop()
+                    if stk:
+                        stk.pop()
+        while stk:
+            res += stk.pop()
+            if stk:
+                stk.pop()
 
-            elif nums[i]>nums[i+1]:
-                stack.append(nums[i])
-                prev=True
-        skip=False
-        while stack:
-            if not skip:
-                score+=stack.pop()
-                skip=True
-            if skip:
-                stack.pop()
-                skip=False
-        return score
-
+        return res
 
 
