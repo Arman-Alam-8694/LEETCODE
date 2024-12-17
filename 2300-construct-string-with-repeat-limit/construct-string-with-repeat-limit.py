c31129a74596a1ccce11c4fd62797fc4f4692129
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-12-17 11:36:43 +0530

    Time: 309 ms (30.07%) | Memory: 19.5 MB (11.89%) - LeetSync

--- 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.py
+++ 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.py
@@ -2,30 +2,22 @@ from collections import Counter,deque
 class Solution:
     def repeatLimitedString(self, s: str, k: int) -> str:
         counts=Counter(s)
-        # n=len(count)
-        # check=[[]*n]
         ch=deque(sorted(counts.items(),key=lambda x:x[0],reverse=True))
         check=[]
         for i in ch:
             check.append(list(i))
-        # print(check)
         prev=0
         curr=0
         answer=[]
         while True:
-            # print(check)
             if curr>=len(check):
                 return "".join(answer)
-           
             while check[curr][1]==0:
                 curr+=1
                 if curr>=len(check):
                     return "".join(answer)
-            while check[prev][1]==0:
-                prev+=1
-                if curr>=len(check):
-                    return "".join(answer)
-            
+            if check[prev][1]==0:
+                prev=curr
             if prev==curr:
                 if check[curr][0]=="b":
                     print(check)
