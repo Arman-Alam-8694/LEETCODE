c7f2b7a97d29b127879c4ed9dd0e1ce0aa3ad2a2
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-12-20 22:09:32 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (6.66%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.py
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -1,16 +1,15 @@
 from collections import deque 
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        def check(start,visited,graph,mapp):
+        def check(start,mappp,graph):
             queue=deque([start])
             mapp[start]=0
             
 
             while queue:
                 node=queue.popleft()
-                visited.add(node)
                 for son in graph[node]:
-                    if son not in visited:
+                    if son not in mapp:
                         queue.append(son)
                         mapp[son]=1-mapp[node]
                     else:
@@ -20,10 +19,9 @@ class Solution:
             return True
        
         mapp={}
-        visited=set()
         for i in range(len(graph)):
-            if i not in visited:
-                if not check(i,visited,graph,mapp):
+            if i not in mapp:
+                if not check(i,mapp,graph):
                     return False
 
         return True
