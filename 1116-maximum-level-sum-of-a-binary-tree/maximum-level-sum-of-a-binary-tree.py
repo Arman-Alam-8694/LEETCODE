f7a67f9caad886837b77ee629265e726a14db74a
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-12-20 16:04:47 +0530

    Time: 11 ms (93.81%) | Memory: 21.9 MB (5.07%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.py
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.py
@@ -4,32 +4,34 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
+  
 class Solution:
-    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
-        stack=deque()
-        stack.append((root,1))
-        large=1
-        lsum=float("-inf")
-        prev=1
-        summ=0
-        while stack:
-            node,level=stack.popleft()
-            if prev!=level:
-                if summ>lsum:
-                    large=prev
-                    lsum=summ
-                prev=level
-                summ=0
-            summ+=node.val
-            if node.left:
-                stack.append((node.left,level+1))
-            if node.right:
-                stack.append((node.right,level+1))
-        if summ>lsum:
-            large=level
-        return large
-            
-            
+    def maxLevelSum(self, root):
+        if not root:
+            return 0
 
-        
+        queue = [root]
+        max_level = 1
+        max_sum = float('-inf')
+        level = 1
+
+        while queue:
+            level_sum = 0
+            next_level = []
+
+            for node in queue:
+                level_sum += node.val
+
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+
+            if level_sum > max_sum:
+                max_sum = level_sum
+                max_level = level
+
+            queue = next_level
+            level += 1
+
+        return max_level
