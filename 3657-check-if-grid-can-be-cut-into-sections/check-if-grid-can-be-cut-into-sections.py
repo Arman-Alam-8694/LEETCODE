9a896bd266169866c22b14c8310b6329f0121c34
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-12-22 02:59:40 +0530

    Time: 303 ms (100.00%) | Memory: 83.5 MB (100.00%) - LeetSync

--- 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.py
+++ 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.py
@@ -1,76 +1,52 @@
 class Solution:
     def checkValidCuts(self, n: int, rectangles: List[List[int]]) -> bool:
-        rectangles.sort(key=lambda x: x[1])
-        ycnt = 0
-        yy = 0
-        py = -1
-        ymax = float("-inf")
-        ymin = float("inf")
-        prevy = rectangles[0][1]
-     
-        for idx in rectangles:
-            if prevy != idx[1]:
+        def calculate(rectangles, index_start, index_end):
+            rectangles.sort(key=lambda x: x[index_start])
+            count = 0
+            segment_count = 0
+            prev_coord = rectangles[0][index_start]
+            max_val = float("-inf")
+            min_val = float("inf")
+            prev_max = -1
 
-                if py != -1 and py < ymax:
-                  
-                    ycnt += ymax - py
-                    if ymin >= py:
-                        yy += 1
+            for rect in rectangles:
+                if prev_coord != rect[index_start]:
+                    if prev_max != -1 and prev_max < max_val:
+                        count += max_val - prev_max
+                        if min_val >= prev_max:
+                            segment_count += 1
 
-                if py == -1:
-                  
-                    ycnt += ymax
-                    if ymin >= py:
-                        yy += 1
-                py = max(py, ymax)
-                prevy = idx[1]
-                ymin = prevy
+                    if prev_max == -1:
+                        count += max_val
+                        if min_val >= prev_max:
+                            segment_count += 1
 
-            ymax = max(ymax, idx[3])
-            ymin = min(ymin, idx[1])
-       
-        if ymax > py:
-            ycnt += ymax - py
-            if ymin >= py:
-                yy += 1
+                    prev_max = max(prev_max, max_val)
+                    prev_coord = rect[index_start]
+                    min_val = prev_coord
 
-   
-        if ycnt == ymax and yy >= 3:
+                max_val = max(max_val, rect[index_end])
+                min_val = min(min_val, rect[index_start])
+
+            if max_val > prev_max:
+                count += max_val - prev_max
+                if min_val >= prev_max:
+                    segment_count += 1
+
+            return count, segment_count, max_val
+
+        y_count, y_segments, y_max = calculate(rectangles, 1, 3)
+        if y_count == y_max and y_segments >= 3:
             return True
-        rectangles.sort(key=lambda x: x[0])
-     
-        xcnt = 0
-        xmax = float("-inf")
-        xmin = float("inf")
-        px = -1
-        xx = 0
-        prevx = rectangles[0][0]
-        for idx in rectangles:
-            if prevx != idx[0]:
-               
-                if px != -1 and px < xmax:
-                    xcnt += xmax - px
-                    if xmin >= px:
-                        xx += 1
-                if px == -1:
-                    xcnt += xmax
-                    if xmin >= px:
-                        xx += 1
 
-                prevx = idx[0]
-                px = max(px, xmax)
-               
-                xmin = prevx
-            xmax = max(xmax, idx[2])
-            xmin = min(xmin, idx[0])
-
-       
-        if xmax > px:
-            xcnt += xmax - px
-            if xmin >= px:
-                xx += 1
-
-    
-        if xcnt == xmax and xx >= 3:
+        x_count, x_segments, x_max = calculate(rectangles, 0, 2)
+        if x_count == x_max and x_segments >= 3:
             return True
+
         return False
+
+                
+            
+        
+        
+        
