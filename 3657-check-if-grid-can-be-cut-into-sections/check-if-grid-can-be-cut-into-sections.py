13b90df53932064644abae273a46649ed84e3916
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-12-22 02:33:23 +0530

    Time: 303 ms (100.00%) | Memory: 83 MB (100.00%) - LeetSync

--- 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.py
+++ 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.py
@@ -1,13 +1,76 @@
 class Solution:
-    def checkValidCuts(self, n: int, A: List[List[int]]) -> bool:
-        def check(A):
-            res = 0
-            A.sort()
-            pre = A[0][1]
-            for a,b in A:
-                res += pre <= a
-                pre = max(pre, b)
-            return res > 1
+    def checkValidCuts(self, n: int, rectangles: List[List[int]]) -> bool:
+        rectangles.sort(key=lambda x: x[1])
+        ycnt = 0
+        yy = 0
+        py = -1
+        ymax = float("-inf")
+        ymin = float("inf")
+        prevy = rectangles[0][1]
+     
+        for idx in rectangles:
+            if prevy != idx[1]:
 
-        return check([a[::2] for a in A]) or check([a[1::2] for a in A])
-        
+                if py != -1 and py < ymax:
+                  
+                    ycnt += ymax - py
+                    if ymin >= py:
+                        yy += 1
+
+                if py == -1:
+                  
+                    ycnt += ymax
+                    if ymin >= py:
+                        yy += 1
+                py = max(py, ymax)
+                prevy = idx[1]
+                ymin = prevy
+
+            ymax = max(ymax, idx[3])
+            ymin = min(ymin, idx[1])
+       
+        if ymax > py:
+            ycnt += ymax - py
+            if ymin >= py:
+                yy += 1
+
+   
+        if ycnt == ymax and yy >= 3:
+            return True
+        rectangles.sort(key=lambda x: x[0])
+     
+        xcnt = 0
+        xmax = float("-inf")
+        xmin = float("inf")
+        px = -1
+        xx = 0
+        prevx = rectangles[0][0]
+        for idx in rectangles:
+            if prevx != idx[0]:
+               
+                if px != -1 and px < xmax:
+                    xcnt += xmax - px
+                    if xmin >= px:
+                        xx += 1
+                if px == -1:
+                    xcnt += xmax
+                    if xmin >= px:
+                        xx += 1
+
+                prevx = idx[0]
+                px = max(px, xmax)
+               
+                xmin = prevx
+            xmax = max(xmax, idx[2])
+            xmin = min(xmin, idx[0])
+
+       
+        if xmax > px:
+            xcnt += xmax - px
+            if xmin >= px:
+                xx += 1
+
+    
+        if xcnt == xmax and xx >= 3:
+            return True
+        return False
