50702dfdb9a11b90dc00a079e3a5e39f912fbd55
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-12-21 10:56:53 +0530

    Time: 147 ms (59.76%) | Memory: 40.1 MB (12.20%) - LeetSync

--- 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.py
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.py
@@ -1,21 +1,19 @@
 class Solution:
     def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:
         def dfs(node, parent):
-            nonlocal cnt
-            current_sum = values[node] % k  # Start with the value of the current node
-            divisible_count = 0  # Count of divisible components
-
-            for neighbor in graph[node]:
-                if neighbor != parent:  # Avoid revisiting the parent node
-                    child_sum, child_divisible_count = dfs(neighbor, node)
-                    divisible_count += child_divisible_count  # Add child counts
-                    current_sum += child_sum  # Add child sum to current node
-
-            # Check if the current subtree is divisible
-            if current_sum % k == 0:
-                divisible_count += 1  # This subtree forms a component
-                return 0, divisible_count  # Return 0 as the sum for the parent
-            return current_sum, divisible_count  # Return current sum and count
+            csumm=values[node]
+            divisiblecnt=0
+            for son in graph[node]:
+                if son!=parent:
+                    summ,cnt=dfs(son,node)
+                    csumm+=summ
+                    divisiblecnt+=cnt
+     
+            if csumm%k==0:
+                divisiblecnt+=1
+                return [0,divisiblecnt]
+            else:
+                return [csumm,divisiblecnt]
 
         # Build the graph from edges
         graph = {i: [] for i in range(n)}
@@ -23,9 +21,6 @@ class Solution:
             graph[u].append(v)
             graph[v].append(u)
 
-        # Initialize variables
-        cnt = 0
-
         # Start DFS from node 0
-        _, cnt = dfs(0, -1)
-        return cnt
+        _, cntt = dfs(0, -1)
+        return cntt
