b6e58e4ba710f6a34b9842befdf36805934893d7
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-12-22 14:56:01 +0530

    Time: 382 ms (81.44%) | Memory: 45.8 MB (20.62%) - LeetSync

--- 3181-find-building-where-alice-and-bob-can-meet/find-building-where-alice-and-bob-can-meet.py
+++ 3181-find-building-where-alice-and-bob-can-meet/find-building-where-alice-and-bob-can-meet.py
@@ -1,22 +1,10 @@
 import heapq
 class Solution:
     def leftmostBuildingQueries(self, heights: List[int], queries: List[List[int]]) -> List[int]:
-        def search(mono,ch):
-            start=0
-            end=len(mono)-1
-            while start<=end:
-                mid=(start+end)//2
-                if mono[mid][0]<=ch:
-                    end=mid-1
-                else:
-                    start=mid+1
-            return end
-            
         q_len=len(queries)
         h_len=len(heights)
         result=[-1]*q_len
         mapp=defaultdict(list)
-        
         for idx,hei in enumerate(queries):
             l,r=sorted(hei)
             h=max(heights[l],heights[r])
@@ -24,24 +12,16 @@ class Solution:
                 result[idx]=r
             else:
                 mapp[r].append((h,idx))
-        mono=[]
-        for idx in range(h_len-1,-1,-1):
-            for q_elem,q_idx in mapp[idx]:
-                pos=search(mono,q_elem)
-                if pos!=-1:
-                    result[q_idx]=mono[pos][1]
-                
-            while mono and mono[-1][0]<=heights[idx]:
-                mono.pop()
-            mono.append((heights[idx],idx))
+        min_heap=[]
+        for idx,hei in enumerate(heights):
+            while min_heap and min_heap[0][0]<hei:
+                q_hei,q_idx=heapq.heappop(min_heap)
+                result[q_idx]=idx
+            for group in mapp[idx]:
+                heapq.heappush(min_heap,group)
         return result
 
 
-
-
-            
-            
-      
 
 
         
