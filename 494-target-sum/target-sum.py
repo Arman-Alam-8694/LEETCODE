6b6f7632f3670fb13239ed68078b0f700ecd2cdc
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-12-26 12:55:52 +0530

    Time: 112 ms (79.44%) | Memory: 17.9 MB (63.39%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,10 +1,21 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        mapp=[defaultdict(int) for _ in range(len(nums)+1)]
-        mapp[0][0]=1
-        for i in range(len(nums)):
-            for k,v in mapp[i].items():
-                mapp[i+1][k+nums[i]]+=v
-                mapp[i+1][k-nums[i]]+=v
+        #2d DP
+        # mapp=[defaultdict(int) for _ in range(len(nums)+1)]
+        # mapp[0][0]=1
+        # for i in range(len(nums)):
+        #     for k,v in mapp[i].items():
+        #         mapp[i+1][k+nums[i]]+=v
+        #         mapp[i+1][k-nums[i]]+=v
 
-        return mapp[len(nums)][target]
+        # return mapp[len(nums)][target]
+        #1D DP
+        mapp=defaultdict(int)
+        mapp[0]=1
+        for i in range(len(nums)):
+            temp=defaultdict(int)
+            for k,v in mapp.items():
+                temp[k+nums[i]]+=v
+                temp[k-nums[i]]+=v
+            mapp=temp
+        return mapp[target]
