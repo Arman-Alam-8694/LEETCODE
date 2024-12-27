13a6f162d83f066e00ccc7ae905ec7519704c67f
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-12-27 14:00:17 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (6.45%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,15 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        def recur(n,dp):
-            # nonlocal dp
+        dp=[-1]*(n+1)
+        def recur(n):
+            nonlocal dp
             if n<2:
                 return 1
             if dp[n]!=-1:
                 return dp[n]
-            dp[n]=recur(n-1,dp)+recur(n-2,dp)
+            dp[n]=recur(n-1)+recur(n-2)
             return dp[n]
         
-        dp=[-1]*(n+1)
-        return recur(n,dp)
+        return recur(n)
         
         
