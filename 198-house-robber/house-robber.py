5fdb1d330367d557b7a93986ab82869417562535
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-12-27 18:21:54 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (6.93%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,22 +1,16 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n=len(nums)
-        size=len(nums)
         dp=[-1]*(n+1)
-        def recur(n,size):
+        def recur(n):
             if n<=0:
                 return 0
             if dp[n]!=-1:
                 return dp[n]
-            temp=float('-inf')
-            for i in range(2,n+2):
-                if n-i>=-1:
-                    temp=max(temp,recur(n-i,size)+nums[n-1])
-                # print(temp,n)
-            dp[n]=temp
+            dp[n]=max(recur(n-1),nums[n-1]+recur(n-2))
             return dp[n]
         
-        recur(n,size)
-        recur(n-1,size)
-        return max(dp[n-1],dp[n])
+        return recur(n)
+
+
         
