c106873e1cfb0d3ffb2f290bd0df723ffb800e7b
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-12-29 15:45:40 +0530

    Time: 1472 ms (43.37%) | Memory: 47.8 MB (15.61%) - LeetSync

--- 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.py
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.py
@@ -1,24 +1,32 @@
+from collections import deque
 class Solution:
     def minimumObstacles(self, grid: List[List[int]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
-        visited = [[False]*n for _ in range(m)]
-        q = deque([(0,0,0)])
-        visited[0][0] = True
-        dire = [(1,0),(0,1),(-1,0),(0,-1)]
-        while(q):
-            i,j,obst = q.popleft()
-            if(i == m-1 and j == n-1):
-                return obst
-            for dx,dy in dire:
-                x,y = i+dx,j+dy
-                if(0<=x<m and 0<=y<n and not visited[x][y]):
-                    visited[x][y] = True
-                    if(grid[x][y]):
-                        q.append((x,y,obst+1))
+        for i in grid:
+            print(i)
+        m, n = len(grid), len(grid[0])
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # right, down, left, up
+        queue=deque([(0,0,0)])
+        visited=set()
+        prev=0
+        def if_valid(x,y):
+            if (0<=x<m and 0<=y<n) and ((x,y) not in visited):
+                return True
+            return False
+        while queue:
+            i,j,obs=queue.popleft()
+            
+            if (i, j) == (m - 1, n - 1):  # Reached the bottom-right corner
+                    return obs
+            for u,v in directions:
+                newx=i+u
+                newy=j+v
+                if if_valid(newx,newy):
+                    visited.add((newx,newy))
+                    if grid[newx][newy]==0:
+                        queue.appendleft((newx,newy,obs))
                     else:
-                        q.appendleft((x,y,obst))
-        return -1
+                        queue.append((newx,newy,obs+1))
             
-            
-        
+   
+                
+
