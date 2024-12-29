675beed9ff782bf4e55a27a52a62d0993b5445ce
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-12-29 12:19:17 +0530

    Time: 578 ms (95.57%) | Memory: 23.3 MB (76.92%) - LeetSync

--- 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/number-of-ways-to-form-a-target-string-given-a-dictionary.py
+++ 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/number-of-ways-to-form-a-target-string-given-a-dictionary.py
@@ -1,33 +1,33 @@
 class Solution:
     def numWays(self, words: List[str], target: str) -> int:
-        MOD = 10**9 + 7
-        m, n = len(words[0]), len(target)
+        """dp[i][j] = ways to form target[:j+1] from letters[:i+1]"""
+        n = len(words[0])
+        m = len(target)
+        if m > n:
+            return 0
 
-        # Precompute frequency of characters in each column
-        freq = [Counter() for _ in range(m)]
+        d = dict(zip("abcdefghijklmnopqrstuvwxyz", range(26)))
+
+        # letter[i] = how many of each letter at index i
+        letters = [[0] * 26 for _ in range(n)]
         for word in words:
-            for i, char in enumerate(word):
-                freq[i][char] += 1
+            for i in range(n):
+                letters[i][d[word[i]]] += 1
 
-        # Initialize DP table
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
-        dp[0][0] = 1  # Base case: one way to form an empty target using no columns
+        dp = [0] * (m + 1)
+        dp[-1] = 1
 
-        # Fill DP table
-        for i in range(1, m + 1):  # Iterate over columns
-            for j in range(n + 1):  # Iterate over target characters
-                # Carry forward ways without using this column
-                dp[i][j] = dp[i - 1][j]
-                if j > 0:  # If forming at least one character of the target
-                    char = target[j - 1]
-                    dp[i][j] += dp[i - 1][j - 1] * freq[i - 1][char]
-                    dp[i][j] %= MOD
+        for i in range(n):
+            new = [0] * (m + 1)
+            new[-1] = 1
 
-        return dp[m][n]
+            for j in range(m):
+                if i < j:
+                    break
 
+                # skip + use
+                new[j] = (dp[j] + letters[i][d[target[j]]] * dp[j - 1]) % 1000000007
 
+            dp = new
 
-
-        
-
-        
+        return dp[-2] % 1000000007
