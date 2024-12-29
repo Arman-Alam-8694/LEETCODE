226be8d6030db4011bd06691e0628d1cd650a39f
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-12-29 15:43:15 +0530

    Time: 611 ms (96.34%) | Memory: 40.1 MB (81.27%) - LeetSync

--- 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.py
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.py
@@ -1,37 +1,24 @@
 class Solution:
-    # Directions for movement: right, left, down, up
-    _directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-
-    def minimumObstacles(self, grid):
-        # Helper method to check if the cell is within the grid bounds
-        def _is_valid(row, col):
-            return 0 <= row < len(grid) and 0 <= col < len(grid[0])
-
-        m, n = len(grid), len(grid[0])
-
-        # Distance matrix to store the minimum obstacles removed to reach each cell
-        min_obstacles = [[float("inf")] * n for _ in range(m)]
-        min_obstacles[0][0] = 0
-
-        deque_cells = deque([(0, 0, 0)])  # (obstacles, row, col)
-
-        while deque_cells:
-            obstacles, row, col = deque_cells.popleft()
-
-            # Explore all four possible directions from the current cell
-            for dr, dc in self._directions:
-                new_row, new_col = row + dr, col + dc
-
-                if _is_valid(new_row, new_col) and min_obstacles[new_row][
-                    new_col
-                ] == float("inf"):
-                    if grid[new_row][new_col] == 1:
-                        # If it's an obstacle, add 1 to obstacles and push to the back
-                        min_obstacles[new_row][new_col] = obstacles + 1
-                        deque_cells.append((obstacles + 1, new_row, new_col))
+    def minimumObstacles(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        visited = [[False]*n for _ in range(m)]
+        q = deque([(0,0,0)])
+        visited[0][0] = True
+        dire = [(1,0),(0,1),(-1,0),(0,-1)]
+        while(q):
+            i,j,obst = q.popleft()
+            if(i == m-1 and j == n-1):
+                return obst
+            for dx,dy in dire:
+                x,y = i+dx,j+dy
+                if(0<=x<m and 0<=y<n and not visited[x][y]):
+                    visited[x][y] = True
+                    if(grid[x][y]):
+                        q.append((x,y,obst+1))
                     else:
-                        # If it's an empty cell, keep the obstacle count and push to the front
-                        min_obstacles[new_row][new_col] = obstacles
-                        deque_cells.appendleft((obstacles, new_row, new_col))
-
-        return min_obstacles[m - 1][n - 1]
+                        q.appendleft((x,y,obst))
+        return -1
+            
+            
+        
