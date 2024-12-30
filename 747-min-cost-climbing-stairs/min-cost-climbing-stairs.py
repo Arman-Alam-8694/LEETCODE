16184d47daf9b0dd875eba0fa752e277b8a917c1
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2024-12-30 23:14:23 +0530

    Time: 8 ms (10.91%) | Memory: 20.3 MB (5.36%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,15 +1,18 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        @cache
+        dp={}
         def recur(x):
             if x>=len(cost):
                 return 0
+            if x in dp:
+                return dp[x]
             if x==-1:
                 costt=0
             else:
                 costt=cost[x]
             step=min(costt+recur(x+1),costt+recur(x+2))
-            return step
+            dp[x]=step
+            return dp[x]
         
         return recur(-1)
         
