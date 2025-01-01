5cf7a41470b41f445c6b19323de188c9630189e3
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-01-01 12:11:38 +0530

    Time: 4 ms (32.21%) | Memory: 17.9 MB (9.48%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
@@ -1,22 +1,35 @@
 class Solution:
     def maxScore(self, s: str) -> int:
-        dictt={}
-        n=len(s)
-        zero=0
-        ones=0
-        for i in range(n):
-            dictt[i]=[0,0]
-            if s[i]=="0":
-                zero+=1
-            else:
-                ones+=1
-            dictt[i][1]=ones
-            dictt[i][0]=zero
-        maxx=float("-inf")
+        # dictt={}
+        # n=len(s)
+        # zero=0
+        # ones=0
+        # for i in range(n):
+        #     dictt[i]=[0,0]
+        #     if s[i]=="0":
+        #         zero+=1
+        #     else:
+        #         ones+=1
+        #     dictt[i][1]=ones
+        #     dictt[i][0]=zero
+        # maxx=float("-inf")
        
     
+        # for i in range(n-1):
+        #     remone=dictt[n-1][1]-dictt[i][1]
+        #     maxx=max(maxx,remone+dictt[i][0])
+        # return maxx
+
+        maxx=float('-inf')
+        ones=0
+        zero=0
+        n=len(s)
+        for i in s:
+            if i=="1":
+                ones+=1
         for i in range(n-1):
-            remone=dictt[n-1][1]-dictt[i][1]
-            maxx=max(maxx,remone+dictt[i][0])
+            zero+=1 if s[i]=="0" else 0
+            ones+=0 if s[i]=="0" else -1
+            maxx=max(maxx,zero+ones)
         return maxx
         
