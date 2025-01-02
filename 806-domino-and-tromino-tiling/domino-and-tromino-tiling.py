c66151d31a99985ad3a49262f9fd00ee3de98193
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-01-03 00:13:59 +0530

    Time: 20 ms (9.97%) | Memory: 20.6 MB (5.01%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.py
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.py
@@ -1,10 +1,33 @@
 class Solution:
     def numTilings(self, n: int) -> int:
-        dp=[0]*(n+4)
         MOD=10**9+7
-        dp[0]=1
-        dp[1]=1
-        dp[2]=2
-        for i in range(3,n+1):
-            dp[i]=(2*dp[i-1]+dp[i-3])%MOD
-        return dp[n]
+        @cache
+        def recur(x,y):
+            if x>n or y>n:
+                return 0
+            if x==n and y==n:
+                return 1
+            
+            if x==y:
+                ways=(
+                    recur(x+1,y+1)+
+                    recur(x+2,y+2)+
+                    recur(x+2,y+1)+
+                    recur(x+1,y+2)
+
+                )
+          
+            elif x>y:
+                ways=(
+                    recur(x,y+2)+
+                    recur(x+1,y+2)
+                )
+            
+            else:
+                ways=(
+                    recur(x+2,y)+
+                    recur(x+2,y+1)
+                )
+            return (ways)%MOD
+    
+        return recur(0,0)
