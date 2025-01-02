b971ae4ffa418788aa282eebb7437dda48135f62
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-01-02 14:23:50 +0530

    Time: 15 ms (93.12%) | Memory: 49.6 MB (47.74%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.py
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.py
@@ -1,46 +1,18 @@
 class Solution:
     def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
-        class SegmentTree:
-            def __init__(self, n):
-                self.tree = [0] * (4 * n)
-                self.n = n
+        n=len(words)
+        pref_cnt=[0]*(n+1)
+        result=[]
+        cnt=0
+        for i,word in enumerate(words):
+            if (word[0] in "aeiou") and (word[-1] in "aeiou"):
+                cnt+=1
+            pref_cnt[i+1]=cnt
+   
+        for x,y in queries:
+            result.append(pref_cnt[y+1]-pref_cnt[x])
+        return result
 
-            def build(self, arr, start, end, node):
-                if start == end:
-                    self.tree[node] = arr[start]
-                    return
-                mid = (start + end) // 2
-                left_child = 2 * node + 1
-                right_child = 2 * node + 2
-                self.build(arr, start, mid, left_child)
-                self.build(arr, mid + 1, end, right_child)
-                self.tree[node] = self.tree[left_child] + self.tree[right_child]
 
-            def query(self, l, r, start, end, node):
-                if r < start or l > end:
-                    return 0
-                if l <= start and r >= end:
-                    return self.tree[node]
-                mid = (start + end) // 2
-                left_child = 2 * node + 1
-                right_child = 2 * node + 2
-                left_sum = self.query(l, r, start, mid, left_child)
-                right_sum = self.query(l, r, mid + 1, end, right_child)
-                return left_sum + right_sum
 
-        def is_vowel(c):
-            return c in 'aeiou'
-
-        def starts_and_ends_with_vowel(word):
-            return is_vowel(word[0]) and is_vowel(word[-1])
-
-        n = len(words)
-        arr = [1 if starts_and_ends_with_vowel(word) else 0 for word in words]
-
-        seg_tree = SegmentTree(n)
-        seg_tree.build(arr, 0, n - 1, 0)
-
-        ans = []
-        for l, r in queries:
-            ans.append(seg_tree.query(l, r, 0, n - 1, 0))
-        return ans
+        
