44631ee9890f201538c937132c3f6e481233bd76
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-01-03 11:38:31 +0530

    Time: 50 ms (75.50%) | Memory: 32.5 MB (51.34%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
@@ -1,16 +1,29 @@
 class Solution:
     def waysToSplitArray(self, nums: List[int]) -> int:
+        #prefix sum solution
+        # n=len(nums)
+        # prefsum=[0]*(n+1)
+        # cnt=0
+        # for i in range(n):
+        #     cnt+=nums[i]
+        #     prefsum[i]=cnt
+        # result=0
+        # for i in range(n-1):
+        #     if prefsum[i]>=(prefsum[n-1]-prefsum[i]):
+        #         result+=1
+        # return result
+
         n=len(nums)
-        prefsum=[0]*(n+1)
-        cnt=0
-        for i in range(n):
-            cnt+=nums[i]
-            prefsum[i]=cnt
+        tsum=sum(nums)
         result=0
+        rsum=0
         for i in range(n-1):
-            if prefsum[i]>=(prefsum[n-1]-prefsum[i]):
+            rsum+=nums[i]
+            if rsum>=(tsum-rsum):
                 result+=1
         return result
+
+
         
         
 
