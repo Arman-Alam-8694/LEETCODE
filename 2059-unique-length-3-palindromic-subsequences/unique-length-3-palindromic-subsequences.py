f81c9a9f668ea10751ca3a45ff0b36d84de9974f
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-01-04 14:25:10 +0530

    Time: 467 ms (70.99%) | Memory: 18.3 MB (27.25%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -1,22 +1,12 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        # Store unique palindromes
-        unique_palindromes = set()
-        n = len(s)
-        
-        # Iterate for each character 'a' to 'z' as the outer characters of the palindrome
-        for char in "abcdefghijklmnopqrstuvwxyz":
-            # Find the first and last occurrence of the character in the string
-            first = s.find(char)
-            last = s.rfind(char)
-            
-            # Only consider cases where the character appears at least twice
-            if first != -1 and last != -1 and first < last:
-                # Extract the substring between the first and last occurrence
-                middle_chars = set(s[first + 1:last])
-                # Form all palindromes of the form char + middle_char + char
-                for mid_char in middle_chars:
-                    unique_palindromes.add(char + mid_char + char)
-        
-        # Return the count of unique palindromes
-        return len(unique_palindromes)
+        chrr=set(s)
+        res=0
+        n=len(s)
+        for i in chrr:
+            first,last=s.index(i),s.rindex(i)
+            middle=set()
+            for i in range(first+1,last):
+                middle.add(s[i])
+            res+=len(middle)
+        return res
