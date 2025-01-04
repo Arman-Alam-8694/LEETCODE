8395a5d410a1a10235953631e6e82fdf977486d8
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-01-04 14:34:56 +0530

    Time: 106 ms (87.47%) | Memory: 18.2 MB (27.25%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -1,10 +1,10 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        res=0
-        n=len(s)
-        for i in "abcdefghijklmnopqrstuvwxyz":
-            first,last=s.find(i),s.rfind(i)
-        
-            if first!=-1 and last!=-1 and first<last:
-                res+=len(set(s[first+1:last]))
-        return res
+        return sum([len(set(s[s.index(letter)+1:s.rindex(letter)])) for letter in set(s)])
+
+
+
+
+# class Solution:
+#     def countPalindromicSubsequence(self, s: str) -> int:
+#         return sum([len(set(s[s.index(letter)+1:s.rindex(letter)])) for letter in set(s)])
