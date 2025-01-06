27f4ad70d60dc66e8d44954c06094b96b14e0c23
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-01-06 13:26:09 +0530

    Time: 9 ms (73.15%) | Memory: 17.9 MB (33.80%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -19,21 +19,20 @@ class Solution:
 
 
         #2pass O(n) using prefix and suffix and adding them up for the result
-        boxes=list(map(int,boxes))
         n=len(boxes)
         res=[0]*n
         left_ball,pref=0,0
         for i,val in enumerate(boxes):
             res[i]=pref
             pref+=left_ball
-            if val==1:
+            if int(val)==1:
                 left_ball+=1
                 pref+=1
         right_ball,suf=0,0
         for i in range(n-1,-1,-1):
             res[i]+=suf
             suf+=right_ball
-            if boxes[i]==1:
+            if int(boxes[i])==1:
                 right_ball+=1
                 suf+=1
 
