7cb9e2be670536cbbbc9f8ad7bbecbf5d0b2fb17
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-01-06 13:23:16 +0530

    Time: 7 ms (89.20%) | Memory: 18 MB (33.80%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,17 +1,40 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
+        #hash map using the index and val
+
+        # boxes=list(map(int,boxes))
+        # mapp={}
+        # n=len(boxes)
+        # for i in range(n):
+        #     if boxes[i]>0:
+        #         mapp[i]=boxes[i]
+        # result=[]
+        # for i in range(n):
+        #     total=0
+        #     for idx,val in mapp.items():
+        #         if idx!=i:
+        #             total+=abs(i-idx)*val
+        #     result.append(total)
+        # return result
+
         boxes=list(map(int,boxes))
-        mapp={}
         n=len(boxes)
-        for i in range(n):
-            if boxes[i]>0:
-                mapp[i]=boxes[i]
-        result=[]
-        for i in range(n):
-            total=0
-            for idx,val in mapp.items():
-                if idx!=i:
-                    total+=abs(i-idx)*val
-            result.append(total)
-        return result
+        res=[0]*n
+        left_ball,pref=0,0
+        for i,val in enumerate(boxes):
+            res[i]=pref
+            pref+=left_ball
+            if val==1:
+                left_ball+=1
+                pref+=1
+        right_ball,suf=0,0
+        for i in range(n-1,-1,-1):
+            res[i]+=suf
+            suf+=right_ball
+            if boxes[i]==1:
+                right_ball+=1
+                suf+=1
+
+    
+        return res
         
