ad5c17532536031c88c8f9c2e97143dade3edc8b
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-01-09 16:52:13 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (5.64%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.py
@@ -1,10 +1,15 @@
+# class Solution:
+#     def stringMatching(self, words: List[str]) -> List[str]:
+#         result=[]
+#         for i in range(len(words)):
+#             for j in range(len(words)):
+#                 if words[i] in words[j] and i!=j:
+#                     result.append(words[i])
+#                     break
+#         return result
+
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
-        result=[]
-        for i in range(len(words)):
-            for j in range(len(words)):
-                if words[i] in words[j] and i!=j:
-                    result.append(words[i])
-                    break
-        return result
+        a = " ".join(words)
+        return [w for w in words if a.count(w)>1]
         
