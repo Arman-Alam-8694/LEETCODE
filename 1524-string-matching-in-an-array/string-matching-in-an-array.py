4bd499f6238ec212f7e46e069c464f0a44f5ea86
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-01-09 17:14:58 +0530

    Time: 27 ms (7.96%) | Memory: 18.6 MB (5.64%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.py
@@ -8,8 +8,67 @@
 #                     break
 #         return result
 
-class Solution:
-    def stringMatching(self, words: List[str]) -> List[str]:
-        a = " ".join(words)
-        return [w for w in words if a.count(w)>1]
+# class Solution:
+#     def stringMatching(self, words: List[str]) -> List[str]:
+#         a = " ".join(words)
+#         return [w for w in words if a.count(w)>1]
+
+
         
+class Solution:
+    class TrieNode:
+        def __init__(self):
+            self.children={}
+            self.count=0
+
+
+    class Trie:
+        def __init__(self):
+            self.root=Solution.TrieNode()
+
+        def insert(self, word: str) -> None:
+            curr=self.root
+            temp=0
+            for a in word:
+                if a not in curr.children:
+                    curr.children[a]=Solution.TrieNode()
+                curr=curr.children[a]
+                curr.count+=1
+
+        def find(self,word):
+            curr=self.root
+            for c in word:
+                curr=curr.children[c]
+            return curr.count>1
+
+       
+
+    def stringMatching(self, words: List[str]) -> List[str]:
+        t=self.Trie()
+        result=[]
+        for w in words:
+            for i in range(len(w)):
+                t.insert(w[i:])
+        for w in words:
+            if t.find(w):
+                result.append(w)
+        return result
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
