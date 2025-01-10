cf0211672f6fa4809ed1105a8e59949379ad6976
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-01-11 00:43:59 +0530

    Time: 7 ms (26.53%) | Memory: 18.6 MB (41.10%) - LeetSync

---
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+class Solution:
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        stack = deque([(1, root)])  # Start with root node at index 1
+        max_width = 1  # Initialize max width to 1
+
+        while stack:
+            level_length = len(stack)
+            left = stack[0][0]  # Index of the first node at this level
+            right = stack[-1][0]  # Index of the last node at this level
+
+            max_width = max(max_width, right - left + 1)  # Update max width
+            
+            # Prepare the next level of nodes
+            for _ in range(level_length):
+                idx, node = stack.popleft()
+
+                # Add left child to stack (if exists)
+                if node.left:
+                    stack.append((2 * (idx-1)+1, node.left))
+
+                # Add right child to stack (if exists)
+                if node.right:
+                    stack.append((2 * (idx-1) + 2, node.right))
+
+        return max_width
