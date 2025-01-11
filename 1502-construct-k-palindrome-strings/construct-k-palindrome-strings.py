138b6ccf4d3471f821840e442e1db16acfa58778
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-01-11 12:47:22 +0530

    Time: 38 ms (36.12%) | Memory: 18.3 MB (8.81%) - LeetSync

---
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        # if k>len(s):
+        #     return False
+        # if k==len(s):
+        #     return True
+        mapp=Counter(s)
+        odd_count,even_count=0,0
+        odd_sum,even_sum=0,0
+        for i,v in mapp.items():
+            if v&1:
+                odd_count+=1
+                odd_sum+=v
+            else:
+                even_count+=(v)//2
+                even_sum+=v
+        # print(mapp)
+        if odd_count>k:
+            return False
+        if odd_count==k:
+         
+            return True
+        if odd_sum>=k:
+            return True
+        if odd_sum<=k:
+            if (k-odd_sum)<=even_sum:
+                return True
+            
+        return False
+
+
+
+ 
