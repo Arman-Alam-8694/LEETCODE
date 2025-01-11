75e07bf8100dc6ad5f3e57816f1fe73de95ecd32
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-01-11 20:26:23 +0530

    Time: 47 ms (82.05%) | Memory: 22.6 MB (5.17%) - LeetSync

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        self.answer=None
+        self.p_find=False
+        self.q_find=False
+        def dfs(root,p,q):
+            tp_find,tq_find=False,False
+            if not root:
+                return False,False
+            if root==p:
+                self.p_find=True
+                tp_find=True
+            if root==q:
+                tq_find=True
+                self.q_find=True
+            
+            
+            if self.p_find and self.q_find:
+                return tp_find,tq_find
+            p1,q1=dfs(root.left,p,q)
+            p2,q2=dfs(root.right,p,q)
+            if tp_find:
+                if q1 or q2:
+                    self.answer=p
+            elif tq_find:
+                if p1 or p2:
+                    self.answer=q
+            else:
+                if (p1 or p2) and (q1 or q2):
+                    if not self.answer:
+                        self.answer=root  
+            return (p1 or p2 or tp_find),(q1 or q2 or tq_find)
+        dfs(root,p,q)
+        return self.answer
+
+        
