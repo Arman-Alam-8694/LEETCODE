b772cccaa03d9015fa7d0f88cf40e04236e2e2a9
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-01-12 18:24:59 +0530

    Time: 134 ms (21.04%) | Memory: 22.7 MB (6.05%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.py
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.py
@@ -1,25 +1,21 @@
 class Solution:
-    def isValid(self, s: str, locked: str, open_char: str, close_char: str, forward: bool) -> bool:
+    def isValid(self, s: str, locked: str, open_char: str, close_char: str) -> bool:
         n = len(s)
         stack = []
         visited = set()
-        indices = range(n) if forward else range(n - 1, -1, -1)
-
-        for i in indices:
+        
+        for i in range(n):
             if s[i] == close_char and locked[i] == "1":
                 visited.add(i)
-                adj_index = i - 1 if forward else i + 1
-                if 0 <= adj_index < n and (s[adj_index] == open_char or locked[adj_index] == "0"):
-                    adj_next = adj_index - 1 if forward else adj_index + 1
-                    if 0 <= adj_next < n and adj_next not in visited:
-                        stack.append(adj_next)
+                if i - 1 >= 0 and (s[i - 1] == open_char or locked[i - 1] == "0"):
+                    if i - 2 not in visited:
+                        stack.append(i - 2)
                     continue
                 elif stack:
                     idx = stack.pop()
-                    if 0 <= idx < n and (s[idx] == open_char or locked[idx] == "0"):
-                        adj_next = idx - 1 if forward else idx + 1
-                        if 0 <= adj_next < n and adj_next not in visited:
-                            stack.append(adj_next)
+                    if idx >= 0 and (s[idx] == open_char or locked[idx] == "0"):
+                        if idx - 1 not in visited:
+                            stack.append(idx - 1)
                         continue
                     else:
                         return False
@@ -33,12 +29,15 @@ class Solution:
         if n % 2 != 0:
             return False
         
-        # Check forward direction
-        if not self.isValid(s, locked, "(", ")", True):
+      
+        if not self.isValid(s, locked, "(", ")"):
             return False
+     
+        reversed_s = s[::-1]
+        reversed_locked = locked[::-1]
         
-        # Check backward direction
-        if not self.isValid(s, locked, ")", "(", False):
+     
+        if not self.isValid(reversed_s, reversed_locked, ")", "("):
             return False
         
         return True
