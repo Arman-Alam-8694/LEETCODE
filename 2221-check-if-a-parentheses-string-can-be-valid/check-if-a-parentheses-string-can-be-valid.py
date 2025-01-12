8f842e6e87d4745fabd66b74c2b68fb495694910
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-01-12 18:15:50 +0530

    Time: 132 ms (21.61%) | Memory: 22.7 MB (6.05%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.py
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.py
@@ -1,6 +1,5 @@
 class Solution:
     def canBeValid(self, s: str, locked: str) -> bool:
-        left_look=None
         n=len(s)
         if n&1:
             return False
@@ -9,19 +8,11 @@ class Solution:
         for i in range(n):
             if s[i]==")" and locked[i]=="1":
                 visited.add(i)
-                if not stack:
-                    if i-1>=0 and (s[i-1]=="(" or locked[i-1]=="0"):
-                        if i-2 not in visited:
-                            stack.append(i-2)
-                        continue
-                    else:
-                        print(i,stack)
-                        return False
-                elif i-1>=0 and (s[i-1]=="(" or locked[i-1]=="0"):
+                if i-1>=0 and (s[i-1]=="(" or locked[i-1]=="0"):
                     if i-2 not in visited:
                         stack.append(i-2)
                     continue
-                else:
+                elif stack:
                     idx=stack.pop()
                     if idx>=0 and (s[idx]=="(" or locked[idx]=="0"):
                         if idx-1 not in visited:
@@ -29,6 +20,8 @@ class Solution:
                         continue
                     else:
                         return False
+                else:
+                    return False
         sr=s[::-1]
         lockedr=locked[::-1]
         stack=[]
@@ -36,18 +29,12 @@ class Solution:
         for i in range(n):
             if sr[i]=="(" and lockedr[i]=="1":
                 visited.add(i)
-                if not stack:
-                    if i-1>=0 and (sr[i-1]==")" or lockedr[i-1]=="0"):
-                        if i-2 not in visited:
-                            stack.append(i-2)
-                        continue
-                    else:
-                        return False
-                elif i-1>=0 and (sr[i-1]==")" or lockedr[i-1]=="0"):
+                if i-1>=0 and (sr[i-1]==")" or lockedr[i-1]=="0"):
                     if i-2 not in visited:
                         stack.append(i-2)
                     continue
-                else:
+                elif stack:
+
                     idx=stack.pop()
                     if idx>=0 and (sr[idx]==")" or lockedr[idx]=="0"):
                         if idx-1 not in visited:
@@ -55,6 +42,8 @@ class Solution:
                         continue
                     else:
                         return False
+                else:
+                    return False
             
         return True
 
