5ae5a82ac0aa47a61711493cbfe715abe7ba7169
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-01-12 18:22:05 +0530

    Time: 136 ms (20.46%) | Memory: 22.7 MB (6.05%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.py
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.py
@@ -1,51 +1,43 @@
 class Solution:
-    def canBeValid(self, s: str, locked: str) -> bool:
-        n=len(s)
-        if n&1:
-            return False
-        stack=[]
-        visited=set()
+    def isValid(self, s: str, locked: str, open_char: str, close_char: str) -> bool:
+        n = len(s)
+        stack = []
+        visited = set()
+        
         for i in range(n):
-            if s[i]==")" and locked[i]=="1":
+            if s[i] == close_char and locked[i] == "1":
                 visited.add(i)
-                if i-1>=0 and (s[i-1]=="(" or locked[i-1]=="0"):
-                    if i-2 not in visited:
-                        stack.append(i-2)
+                if i - 1 >= 0 and (s[i - 1] == open_char or locked[i - 1] == "0"):
+                    if i - 2 not in visited:
+                        stack.append(i - 2)
                     continue
                 elif stack:
-                    idx=stack.pop()
-                    if idx>=0 and (s[idx]=="(" or locked[idx]=="0"):
-                        if idx-1 not in visited:
-                            stack.append(idx-1)
+                    idx = stack.pop()
+                    if idx >= 0 and (s[idx] == open_char or locked[idx] == "0"):
+                        if idx - 1 not in visited:
+                            stack.append(idx - 1)
                         continue
                     else:
                         return False
                 else:
                     return False
         
-        stack=[]
-        visited=set()
-        for i in range(n-1,-1,-1):
-            if s[i]=="(" and locked[i]=="1":
-                visited.add(i)
-                if i+1<n and (s[i+1]==")" or locked[i+1]=="0"):
-                    if i+2 not in visited:
-                        stack.append(i+2)
-                    continue
-                elif stack:
-
-                    idx=stack.pop()
-                    if idx<n and (s[idx]==")" or locked[idx]=="0"):
-                        if idx+1 not in visited:
-                            stack.append(idx+1)
-                        continue
-                    else:
-                        return False
-                else:
-                    return False
-            
         return True
 
+    def canBeValid(self, s: str, locked: str) -> bool:
+        n = len(s)
+        if n % 2 != 0:
+            return False
         
-
-
+      
+        if not self.isValid(s, locked, "(", ")"):
+            return False
+     
+        reversed_s = s[::-1]
+        reversed_locked = locked[::-1]
+        
+     
+        if not self.isValid(reversed_s, reversed_locked, ")", "("):
+            return False
+        
+        return True
