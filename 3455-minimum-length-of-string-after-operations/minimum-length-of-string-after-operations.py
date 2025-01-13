2fcd1a3812d80cce5916ff7a0c65332be47fcde9
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-01-13 11:52:35 +0530

    Time: 272 ms (17.14%) | Memory: 19 MB (14.92%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
@@ -1,11 +1,16 @@
 class Solution:
     def minimumLength(self, s: str) -> int:
-        mapp=defaultdict(int)
+        array=[0]*26
         result=0
         for i in s:
-            mapp[i]+=1
-            if mapp[i]==3:
-                result+=1
-                mapp[i]-=2
+            idx=ord(i)-ord("a")
+            array[idx]+=1
+        print(array)
+        for i in array:
+            if i:
+                if i&1:
+                    result+=i//2
+                else:
+                    result+=(i-1)//2
         return len(s)-(2*result)
         
