0f9be8b943d3b3142abc30b33d74ef23f0d0d469
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-01-13 11:56:13 +0530

    Time: 66 ms (86.03%) | Memory: 18.7 MB (61.90%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
@@ -1,16 +1,47 @@
 class Solution:
     def minimumLength(self, s: str) -> int:
-        array=[0]*26
-        result=0
-        for i in s:
-            idx=ord(i)-ord("a")
-            array[idx]+=1
-        print(array)
-        for i in array:
-            if i:
-                if i&1:
-                    result+=i//2
-                else:
-                    result+=(i-1)//2
-        return len(s)-(2*result)
+        # array=[0]*26
+        # result=0
+        # for i in s:
+        #     idx=ord(i)-ord("a")
+        #     array[idx]+=1
+        # print(array)
+        # for i in array:
+        #     if i:
+        #         if i&1:
+        #             result+=i//2
+        #         else:
+        #             result+=(i-1)//2
+        # return len(s)-(2*result)
+
+
+        # mapp=Counter(s)
+        # result=0
+        # for k,v in mapp.items():
+        #     if v&1:
+        #         result+=v//2
+        #     else:
+        #         result+=(v-1)//2
+        # return len(s)-(2*result)
+
+        # mapp=defaultdict(int)
+        # result=0
+        # for i in s:
+        #     mapp[i]+=1
+        #     if mapp[i]==3:
+        #         result+=1
+        #         mapp[i]-=2
+        # return len(s)-(2*result)
+
+        s_set = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
+        ans = 0
+        for ch in s_set:
+            count = s.count(ch)
+            if count & 1:
+                ans += 1
+            elif count != 0:
+                ans += 2
+        return ans
+
+
         
