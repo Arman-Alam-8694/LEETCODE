66dd7092384776927e80e8b1aad5944802c648d2
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-01-22 11:59:52 +0530

    Time: 712 ms (78.35%) | Memory: 84.9 MB (53.88%) - LeetSync

--- 1876-map-of-highest-peak/map-of-highest-peak.py
+++ 1876-map-of-highest-peak/map-of-highest-peak.py
@@ -3,15 +3,15 @@ class Solution:
     def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
         row=len(isWater)
         col=len(isWater[0])
+        result=[[-1]*col for _ in range(row)]
         visited=set()
-        # maxx=float("-inf")
+    
         listt=[]
         for r in range(row):
             for c in range(col):
                 if isWater[r][c]==1:
                     listt.append((0,r,c))
-                    isWater[r][c]=0
-                    visited.add((r,c))
+                    result[r][c]=0
                           
         queue=deque(listt)
         dir=[(0,1),(1,0),(-1,0),(0,-1)]
@@ -20,8 +20,8 @@ class Solution:
             for x,y in dir:
                 a=x+i
                 b=y+j
-                if 0<=a<row and 0<=b<col and (a,b) not in visited and isWater[a][b]==0:
+                if 0<=a<row and 0<=b<col and result[a][b]==-1:
                     queue.append((max_h+1,a,b))
-                    isWater[a][b]=max_h+1
+                    result[a][b]=max_h+1
 
-        return isWater
+        return result
