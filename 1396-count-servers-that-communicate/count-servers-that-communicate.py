30e141e3d45f9d69c5fe90a55b266da2479e1384
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-01-23 12:59:08 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (60.67%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.py
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.py
@@ -1,52 +1,64 @@
 class Solution:
     def countServers(self, grid: List[List[int]]) -> int:
-        row=len(grid)
-        col=len(grid[0])
-        temp_col={(0,i):(0,"n") for i in range(col)}
-        # visited=set()
-        result=0
-        for i in range(row):
-            found=False
-            temp=[]
-            for j in range(col):
-                if grid[i][j]==1:
-                    if not found:
-                        temp.append((i,j))
-                    if not found and temp_col[(0,j)][0]==0:
-                        temp_col[(0,j)]=(1,"n")
-                    if found:
-                        result+=1
-                        temp_col[(0,j)]=(1,"v")
-                        grid[i][j]=-1
-                        # visited.add((i,j))
-                if len(temp)==2:
+        # row=len(grid)
+        # col=len(grid[0])
+        # temp_col={(0,i):(0,"n") for i in range(col)}
+        
+        # result=0
+        # for i in range(row):
+        #     found=False
+        #     temp=[]
+        #     for j in range(col):
+        #         if grid[i][j]==1:
+        #             if not found:
+        #                 temp.append((i,j))
+        #             if not found and temp_col[(0,j)][0]==0:
+        #                 temp_col[(0,j)]=(1,"n")
+        #             if found:
+        #                 result+=1
+        #                 temp_col[(0,j)]=(1,"v")
+        #                 grid[i][j]=-1
+                       
+        #         if len(temp)==2:
                  
-                    result+=2
-                    found=True
-                    for x,y in temp:
-                        grid[x][y]=-1
-                        # visited.add((x,y))
-                        temp_col[(0,y)]=(1,"v")
-                    temp=[]
+        #             result+=2
+        #             found=True
+        #             for x,y in temp:
+        #                 grid[x][y]=-1
+        #                 temp_col[(0,y)]=(1,"v")
+        #             temp=[]
 
-        for c in range(col):
-            temp=0
-            count=0
-            if temp_col[(0,c)][1]=="v":
-                found=True
-            else:
-                found=False
-            for r in range(row):
-                if grid[r][c]==1 :
-                    if found:
-                        temp+=1
-                    if not found:
-                        count+=1
-                        if count==2:
-                            temp+=2
-                            found=True
-            result+=temp
-        return result
+        # for c in range(col):
+        #     temp=0
+        #     count=0
+        #     if temp_col[(0,c)][1]=="v":
+        #         found=True
+        #     else:
+        #         found=False
+        #     for r in range(row):
+        #         if grid[r][c]==1 :
+        #             if found:
+        #                 temp+=1
+        #             if not found:
+        #                 count+=1
+        #                 if count==2:
+        #                     temp+=2
+        #                     found=True
+        #     result+=temp
+        # return result
+
+        #simplified logic
+        count = 0
+        for r in range(len(grid)):
+            s = sum(grid[r])
+            if s > 1:
+                count += s
+            elif s == 1:
+                column = grid[r].index(1)
+                if sum(grid[r][column] for r in range(len(grid))) > 1:
+                    count += 1
+        
+        return count
 
         
         
