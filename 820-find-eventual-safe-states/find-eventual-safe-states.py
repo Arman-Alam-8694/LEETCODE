1dc90f54bc39d8d1802612a8abbdadee5023fd10
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-01-24 12:43:21 +0530

    Time: 51 ms (50.75%) | Memory: 23.4 MB (86.29%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.py
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -2,27 +2,50 @@
 
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        visited=set()
-        nodes=len(graph)
-        terminal_nodes=set()
- 
-        def dfs(start):
-            if start in visited:
-                return False
-            if start in terminal_nodes:
-                return True
-            visited.add(start)
-            for i in graph[start]:
-                if not dfs(i):
-                    return False
-            visited.remove(start)
-            terminal_nodes.add(start)
-            return True
-            
-        result=[]
-        for i in range(nodes):
-            if dfs(i):
-                result.append(i)
-        return result
+        #DFS SOLUTION 
 
-        
+        # visited=set()
+        # nodes=len(graph)
+        # terminal_nodes=set()
+ 
+        # def dfs(start):
+        #     if start in visited:
+        #         return False
+        #     if start in terminal_nodes:
+        #         return True
+        #     visited.add(start)
+        #     for i in graph[start]:
+        #         if not dfs(i):
+        #             return False
+        #     visited.remove(start)
+        #     terminal_nodes.add(start)
+        #     return True
+            
+        # result=[]
+        # for i in range(nodes):
+        #     if dfs(i):
+        #         result.append(i)
+        # return result
+
+        #TOPOSORT SOLUTION
+
+        nodes=len(graph)
+        d=[[] for _ in range(nodes)]
+        outdegree,stack=[],[]
+        for node,neighbor in enumerate(graph):
+            for n in neighbor:
+                d[n].append(node)
+            outdegree.append(len(neighbor))
+            if len(neighbor)==0:
+                stack.append(node)
+
+        result=[]
+        while stack:
+            node=stack.pop()
+            result.append(node)
+            for n in d[node]:
+                outdegree[n]-=1
+                if outdegree[n]==0:
+                    stack.append(n)
+
+        return sorted(result)
