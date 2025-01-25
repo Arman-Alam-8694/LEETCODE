a628d4bf4d4b489414ba29e4d7ba80aeff943683
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-01-25 13:23:21 +0530

    Time: 217 ms (95.14%) | Memory: 95.5 MB (5.56%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
@@ -1,31 +1,38 @@
 class Solution:
     def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
-        # Pair each number with its original index
-        indexed_nums = sorted(enumerate(nums), key=lambda x: x[1])
-        
-        # Group numbers that can be swapped
-        groups = []
-        current_group = [indexed_nums[0]]
-        
-        for i in range(1, len(indexed_nums)):
-            if indexed_nums[i][1] - indexed_nums[i-1][1] <= limit:
-                current_group.append(indexed_nums[i])
+        groups=[]
+        elementtogroup={}
+        num=sorted(nums)
+        result=[]
+        n=len(nums)
+        temp=deque()
+        last=num[0]
+        idx=0
+        for i in range(n):
+            if abs(num[i]-last)<=limit:
+                temp.append(num[i])
+                elementtogroup[num[i]]=idx
+                last=num[i]
             else:
-                groups.append(current_group)
-                current_group = [indexed_nums[i]]
+                groups.append(temp)
+                idx+=1
+                temp=deque()
+                temp.append(num[i])
+                last=num[i]
+                elementtogroup[num[i]]=idx
+        if temp:
+            groups.append(temp)
+        for i in nums:
+            groupidx=elementtogroup[i]
+            result.append(groups[groupidx].popleft())
         
-        # Add the last group
-        groups.append(current_group)
-        
-        # Reconstruct the result
-        result = [0] * len(nums)
-        for group in groups:
-            # Sort indices and values separately
-            indices = sorted(idx for idx, _ in group)
-            values = sorted(val for _, val in group)
-            
-            # Assign values to original indices
-            for i in range(len(group)):
-                result[indices[i]] = values[i]
-        
+       
         return result
+
+
+
+            
+
+
+
+        
