da43c62902ca78841972aa70ebbabdeabd618771
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-01-27 18:18:17 +0530

    Time: 337 ms (100.00%) | Memory: 22.9 MB (100.00%) - LeetSync

--- 3751-maximum-frequency-after-subarray-operation/maximum-frequency-after-subarray-operation.py
+++ 3751-maximum-frequency-after-subarray-operation/maximum-frequency-after-subarray-operation.py
@@ -8,21 +8,13 @@ class Solution:
         if len(set(nums))==1:
             if nums[0]==k:
                 return len(nums)
+
         n = len(nums)
         start_map = {}
         prefix_ones = [0] * (n + 1)
         commulative = defaultdict(int)
-       
         start = 0
         end = n - 1
-        corners=0
-        # while start < n and nums[start] == k:
-        #     start += 1
-        #     corners+=1
-        
-        # while end >= 0 and nums[end] == k:
-        #     end -= 1
-        #     corners+=1
     
         totalOnes=0
         for i in range(start,end+1):
@@ -34,21 +26,25 @@ class Solution:
             if nums[i] not in start_map:
                 start_map[nums[i]] = i
 
-        totalOnes=totalOnes+corners
         max_freq = float('-inf')
         cumulative_max =0
         individual = defaultdict(int)
         inbetween = 0
   
         
-        for i in range(start, end + 1):
-            if nums[i] == k and individual:
-                individual_max = max(individual.values())
+        for i in range(start, end + 2):
+            if i==end+1  or (nums[i] == k ):
+                if individual:
+                    individual_max = max(individual.values())
+                else:
+                    individual_max=0
+
                 max_freq = max(max_freq, individual_max+totalOnes)
                 individual = defaultdict(int)
                 com_max=0
                 com_start=0
                 com_max_item=None
+                rem=0
                 for elem, freq in commulative.items():
                     rem=totalOnes
                     start=start_map[elem]
@@ -70,20 +66,4 @@ class Solution:
                 commulative[nums[i]] += 1
                 individual[nums[i]] += 1
 
-        if individual and commulative:
-            individual_max = max(individual.values())
-            max_freq = max(max_freq, individual_max+totalOnes)
-            individual = defaultdict(int)
-            com_max=0
-            com_start=0
-            com_max_item=None
-            for elem, freq in commulative.items():
-                rem=totalOnes
-                start=start_map[elem]
-                count_k_in_range = prefix_ones[i+1] - prefix_ones[start]
-                if (freq-count_k_in_range)>com_max:
-                    com_max=(freq-count_k_in_range)
-                    rem-=count_k_in_range
-
-            max_freq=max(max_freq,rem+com_max)
         return max_freq
