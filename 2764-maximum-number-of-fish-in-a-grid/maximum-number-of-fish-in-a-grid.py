1b28ba34d3b84e7ba3dcce70409ef14406851b76
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-01-28 11:57:30 +0530

    Time: 46 ms (44.88%) | Memory: 17.9 MB (49.02%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.py
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.py
@@ -1,38 +1,63 @@
 class Solution:
     def findMaxFish(self, grid: List[List[int]]) -> int:
-        # 8 6
-        # 2 6
+        #BFS
+        # row=len(grid)
+        # col=len(grid[0])
+        # maxx=0
+        # dir=[(0,1),(0,-1),(1,0),(-1,0)]
+        # def bfs(stack,visited):
+        #     temp=0
+        #     while stack:
+        #         i,j=stack.popleft()
+        #         visited.add((i,j))
+        #         temp+=grid[i][j]
+        #         for x,y in dir:
+        #             a=x+i
+        #             b=y+j
+        #             if 0<=a<row and 0<=b<col and grid[a][b]!=0 and (a,b) not in visited:
+        #                 visited.add((a,b))
+        #                 stack.append((a,b))
+        #     return temp
+
+        # visited=set()          
+        # for i in range(row):
+        #     for j in range(col):
+
+        #         if grid[i][j]!=0:
+        #             stack=deque([(i,j)])
+        #             visited.add((i,j))
+        #             maxx=max(maxx,bfs(stack,visited))
+        #             visited=set()
+
+        # return maxx
+        
+        #DFS
         row=len(grid)
         col=len(grid[0])
-        dist=[]
         maxx=0
         dir=[(0,1),(0,-1),(1,0),(-1,0)]
-        def bfs(stack,visited):
-            temp=0
-            while stack:
-                i,j=stack.popleft()
-                visited.add((i,j))
-                temp+=grid[i][j]
-                for x,y in dir:
-                    a=x+i
-                    b=y+j
-                    if 0<=a<row and 0<=b<col and grid[a][b]!=0 and (a,b) not in visited:
-                        visited.add((a,b))
-                        stack.append((a,b))
+        def dfs(i,j):
+            if grid[i][j]==0:
+                return 0
+            temp=grid[i][j]
+            grid[i][j]=0
+            tempp=0
+            for x,y in dir:
+                a=x+i
+                b=y+j
+                if 0<=a<row and 0<=b<col:
+                    tempp+=dfs(a,b)
 
+            return temp+tempp
 
-            return temp
 
         visited=set()          
         for i in range(row):
             for j in range(col):
-
                 if grid[i][j]!=0:
-                    stack=deque([(i,j)])
-                    visited.add((i,j))
-                    maxx=max(maxx,bfs(stack,visited))
-                    visited=set()
+                    maxx=max(maxx,dfs(i,j))
 
         return maxx
+
 
         
