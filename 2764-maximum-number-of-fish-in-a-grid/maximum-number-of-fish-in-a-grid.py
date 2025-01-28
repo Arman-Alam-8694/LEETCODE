ca81e52e1f8411b5b85542930b74c491941ebf55
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-01-28 11:59:37 +0530

    Time: 35 ms (73.41%) | Memory: 18.1 MB (18.78%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.py
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.py
@@ -37,18 +37,21 @@ class Solution:
         maxx=0
         dir=[(0,1),(0,-1),(1,0),(-1,0)]
         def dfs(i,j):
+            if not (0<=i<row and 0<=j<col):
+                return 0
             if grid[i][j]==0:
                 return 0
             temp=grid[i][j]
             grid[i][j]=0
-            tempp=0
-            for x,y in dir:
-                a=x+i
-                b=y+j
-                if 0<=a<row and 0<=b<col:
-                    tempp+=dfs(a,b)
+            # tempp=0
+            # for x,y in dir:
+            #     a=x+i
+            #     b=y+j
+            #     if 0<=a<row and 0<=b<col:
+            #         tempp+=dfs(a,b)
 
-            return temp+tempp
+            # return temp+tempp
+            return temp+dfs(i,j+1)+dfs(i+1,j)+dfs(i-1,j)+dfs(i,j-1)
 
 
         visited=set()          
