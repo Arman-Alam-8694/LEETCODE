acf28362f99c497f6c376115a9aa85ee4a323ae1
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-02-02 11:52:52 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (70.45%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -1,11 +1,21 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
-        listt="*".join(map(str,sorted(nums)))
-        list1=listt+"*"+listt
-        print(list1)
-        print(nums)
-        n="*".join(map(str,nums))
+        # listt="*".join(map(str,sorted(nums)))
+        # list1=listt+"*"+listt
+        # print(list1)
+        # print(nums)
+        # n="*".join(map(str,nums))
     
-        if n in list1:
-            return True
-        return False
+        # if n in list1:
+        #     return True
+        # return False
+
+        cnt=0
+        n=len(nums)
+        for i in range(n):
+            if nums[i]>nums[(i+1)%n]:
+                cnt+=1
+            if cnt>1:
+                return False
+        return True
+
