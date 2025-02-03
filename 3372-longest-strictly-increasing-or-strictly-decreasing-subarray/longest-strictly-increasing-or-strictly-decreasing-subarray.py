43ddc6aa4519daebc2477ebcba7f7dfc1b06190b
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-02-03 14:07:03 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (47.33%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.py
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.py
@@ -1,20 +1,36 @@
 class Solution:
     def longestMonotonicSubarray(self, nums: List[int]) -> int:
-        maxx=1
-        n=len(nums)
-        left=0
-        for right in range(n):
-            if left!=right:
-                if nums[right-1]>=nums[right]:
-                    left=right
-            maxx=max(maxx,right-left+1)
+        # maxx=1
+        # n=len(nums)
+        # left=0
+        # for right in range(n):
+        #     if left!=right:
+        #         if nums[right-1]>=nums[right]:
+        #             left=right
+        #     maxx=max(maxx,right-left+1)
         
-        left=0
-        for right in range(n):
-            if left!=right:
-                if nums[right-1]<=nums[right]:
-                    left=right
-            maxx=max(maxx,right-left+1)
+        # left=0
+        # for right in range(n):
+        #     if left!=right:
+        #         if nums[right-1]<=nums[right]:
+        #             left=right
+        #     maxx=max(maxx,right-left+1)
 
+        # return maxx
+        maxx=inc_left=dec_left=0
+        n=len(nums)
+    
+        for right in range(n):
+         
+            if nums[right-1]>nums[right]:
+                inc_left=right
+            elif nums[right-1]<nums[right]:
+                dec_left=right
+            else: 
+                inc_left=right
+                dec_left=right
+
+            maxx=max(maxx,right-dec_left+1,right-inc_left+1)
+        
         return maxx
         
