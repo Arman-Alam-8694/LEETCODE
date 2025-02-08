cdc4c06556929b014816d1852194cac2755033b8
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-02-08 12:50:44 +0530

    Time: 101 ms (86.76%) | Memory: 76.6 MB (68.14%) - LeetSync

--- 2434-design-a-number-container-system/design-a-number-container-system.py
+++ 2434-design-a-number-container-system/design-a-number-container-system.py
@@ -1,33 +1,23 @@
 class NumberContainers:
+
     def __init__(self):
-        # Map to store number -> min heap of indices
-        self.number_to_indices = defaultdict(list)
-        # Map to store index -> number
-        self.index_to_numbers = {}
+        self.mp={}
+        self.idx=defaultdict(list)
 
     def change(self, index: int, number: int) -> None:
-        # Update index to number mapping
-        self.index_to_numbers[index] = number
-
-        # Add index to the min heap for this number
-        heapq.heappush(self.number_to_indices[number], index)
+        self.mp[index]=number
+        heappush(self.idx[number], index)
+        
 
     def find(self, number: int) -> int:
-        # If number doesn't exist in our map
-        if not self.number_to_indices[number]:
+        if number not in self.idx:
             return -1
-
-        # Keep checking top element until we find valid index
-        while self.number_to_indices[number]:
-            index = self.number_to_indices[number][0]
-
-            # If index still maps to our target number, return it
-            if self.index_to_numbers.get(index) == number:
-                return index
-
-            # Otherwise remove this stale index
-            heapq.heappop(self.number_to_indices[number])
+        while self.idx[number]:
+            i=self.idx[number][0]
+            if self.mp[i]==number: return i
+            heappop(self.idx[number])
         return -1
+
 
 
 # Your NumberContainers object will be instantiated and called as such:
