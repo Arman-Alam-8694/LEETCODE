8218aa2bb3b8df9e7c91cd0af5321135a36f943f
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-02-09 12:42:44 +0530

    Time: 83 ms (51.19%) | Memory: 39 MB (28.81%) - LeetSync

---
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countBadPairs(self, nums: List[int]) -> int:
+        #NDBM
+
+        # HERE THE KEY POINT IS OBSERVATON IN THE EQUATION OR FORMING A RELATION BETWEEN THE 
+        #INDICES AND ELEMENTS SUCH THAT IT BECOMES EASIER TO LOOK BACK BECAUSE WE WON'T BE 
+        #KNOWING THE FUTURE OF WHAT WILL BE COMING NEXT SO WE WILL TWIST THE QUESITON IN 
+        #FINDING THE TOTAL NO. OF GOOD PAIRS AND THEN JUST SUBTRACTING IT WITH THE TOTAL NO. OF 
+        #PAIRS  the equation could be nums[i]-i==nums[j]-j for good pairs
+
+        count=defaultdict(int)
+        n=len(nums)
+        totalPairs=(n*(n+1))//2
+        tp=0
+        goodPairs=0
+        for i in range(n):
+            tp+=i
+            slope=nums[i]-i
+            goodPairs+=count[slope]
+            count[slope]+=1
+        return tp-goodPairs
+
+
+        
