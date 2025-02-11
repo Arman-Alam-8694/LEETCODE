8d7f2cacd6e0ee70dd6340c9418bc766db2dfb44
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-02-11 15:26:47 +0530

    Time: 3 ms (56.31%) | Memory: 17.8 MB (75.36%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.py
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.py
@@ -4,7 +4,8 @@ class Solution:
         part_size=len(part)
         for i in s:
             stack.append(i)
-            if len(stack)>=part_size and stack[-part_size:]==list(part):
+            if i==part[-1] and len(stack)>=part_size:
+                if stack[-part_size:]==list(part):
                     del stack[-part_size:]
 
         return "".join(stack)        
