8e149078641ba8b0e3593720dc1cc07ec4969de3
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-02-14 17:22:24 +0530

    Time: 39 ms (52.82%) | Memory: 31.5 MB (88.13%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.py
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.py
@@ -1,39 +1,22 @@
 class ProductOfNumbers:
 
     def __init__(self):
-        self.product=[]
-        self.last_zero=None
-     
-        
+        self.product=[1]
+        self.n=1
 
     def add(self, num: int) -> None:
         if num==0:
-            self.last_zero=len(self.product)
-        
-            if self.product:
-                self.product.append(self.product[-1])
-            else:
-                self.product.append(1)
+            self.product=[1]
+            self.n=1
         else:
-            
-        
-            if self.product:
-                self.product.append(self.product[-1]*num)
-            else:
-                self.product.append(num)
+            self.product.append(self.product[-1]*num)
+            self.n+=1
 
     def getProduct(self, k: int) -> int:
-
-       
-        if self.last_zero is not None:
-            new_zero=len(self.product)-self.last_zero
-            if k>=new_zero:
-                return 0
-        if k+1>len(self.product) or self.product[-(k+1)]==0:
-            return self.product[-1]
+        if self.n<=k:
+            return 0
         else:
-            return self.product[-1]//self.product[-(k+1)]
-
+            return self.product[-1]//self.product[-k-1]
         
 
 
