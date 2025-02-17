dfd24f260845b14f27de6fdd73f25017ab2eee4c
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-02-17 12:27:23 +0530

    Time: 51 ms (32.66%) | Memory: 19.5 MB (26.97%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.py
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.py
@@ -3,20 +3,16 @@ class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
         unique_numbers=list(set(tiles))
         available=Counter(tiles)
-   
-
         result=set()
         def backtrack(cur_list,available,result):
-            if not available:
-                return 
+        
             for i in unique_numbers:
                 if available[i]>0:
                     cur_list.append(i)
                     stringg="".join(cur_list)
-                    if stringg not in result:
-                        result.add(stringg)
-                    # else:
-                    #     return
+                  
+                    result.add(stringg)
+                 
                     available[i]-=1
                    
                     backtrack(cur_list,available,result)
