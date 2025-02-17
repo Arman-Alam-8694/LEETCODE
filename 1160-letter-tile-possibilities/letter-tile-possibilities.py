6749d50055bae27b856e9dab54edae3b09c2db2d
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-02-17 12:29:15 +0530

    Time: 41 ms (46.21%) | Memory: 17.7 MB (83.06%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.py
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.py
@@ -1,31 +1,22 @@
+from collections import Counter
+
 class Solution:
-    
     def numTilePossibilities(self, tiles: str) -> int:
-        unique_numbers=list(set(tiles))
-        available=Counter(tiles)
-        result=set()
-        def backtrack(cur_list,available,result):
+        available = Counter(tiles)  
+        count = 0  
+
+        def backtrack():
+            nonlocal count
+            for tile in available:
+                if available[tile] > 0:
+                    available[tile] -= 1  
+                    count += 1  
+                    
+                    backtrack() 
+                    
+                    available[tile] += 1  
         
-            for i in unique_numbers:
-                if available[i]>0:
-                    cur_list.append(i)
-                    stringg="".join(cur_list)
-                  
-                    result.add(stringg)
-                 
-                    available[i]-=1
-                   
-                    backtrack(cur_list,available,result)
-                    available[i]+=1
-                    cur_list.pop()
-                 
-
-        backtrack([],available,result)
-        return len(result)
-                
+        backtrack()
+        return count
 
 
-
-
-
-        
