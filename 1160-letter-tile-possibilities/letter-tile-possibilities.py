a2e13350cd75b30903dfe8388ea87ca14b4a30aa
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-02-17 12:33:46 +0530

    Time: 39 ms (57.32%) | Memory: 17.8 MB (67.21%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.py
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.py
@@ -1,21 +1,55 @@
 class Solution:
+    
     def numTilePossibilities(self, tiles: str) -> int:
-        counts = [0] * 26
-        fac = [1] * (len(tiles) + 1)
-        for i in range(1, len(tiles) + 1):
-            fac[i] = i * fac[i - 1]
-        for c in tiles:
-            counts[ord(c) - ord('A')] += 1
-        lengthcounts = [0] * (len(tiles) + 1)
-        lengthcounts[0] = 1
-        for i in range(26):
-            if counts[i] > 0:
-                temp = [0] * (len(tiles) + 1)
-                for j in range(len(tiles) + 1):
-                    if lengthcounts[j] > 0:
-                        for k in range(1, counts[i] + 1):
-                            totallength = j + k
-                            temp[totallength] += lengthcounts[j] * fac[totallength] // (fac[k] * fac[j])
-                for j in range(len(tiles) + 1):
-                    lengthcounts[j] += temp[j]
-        return sum(lengthcounts[1:])
+        #if the question included printing the sequences then this solution is better
+        # unique_numbers=list(set(tiles))
+        # available=Counter(tiles)
+        # result=set()
+        # def backtrack(cur_list,available,result):
+        
+        #     for i in unique_numbers:
+        #         if available[i]>0:
+        #             cur_list.append(i)
+        #             stringg="".join(cur_list)
+                  
+        #             result.add(stringg)
+                 
+        #             available[i]-=1
+                   
+        #             backtrack(cur_list,available,result)
+        #             available[i]+=1
+        #             cur_list.pop()
+                 
+
+        # backtrack([],available,result)
+        # return len(result)
+
+
+    
+
+        #just for numbers
+        available = Counter(tiles)  
+        count = 0  
+
+        def backtrack():
+            nonlocal count
+            for tile in available:
+                if available[tile] > 0:
+                    available[tile] -= 1  
+                    count += 1  
+                    
+                    backtrack() 
+                    
+                    available[tile] += 1  
+        
+        backtrack()
+        return count
+
+
+                
+
+
+
+
+
+        
