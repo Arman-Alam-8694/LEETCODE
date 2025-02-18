d4417f8b739f53a2d5e33cd4e15e4c504f56cf9d
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-02-18 14:50:37 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (54.04%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
@@ -3,17 +3,15 @@ class Solution:
         new_pattern="A"+pattern
         n=len(pattern)
         listt=["-1"]*(n+1)
-        print(new_pattern)
         Pidx=0
         lidx=0
         used=set()
         def backtrack(Pidx,lidx,start,used):
-          
             if Pidx==n+1:
                 return True
 
             if new_pattern[Pidx]=="A" or new_pattern[Pidx]=="I":
-                for i in range(start,10):
+                for i in range(start,n+2):
                     if i not in used:
                         listt[lidx]=str(i)
                         used.add(i)
@@ -30,11 +28,6 @@ class Solution:
                             return True
                         used.remove(i)
             return False
-
-
-            
-
-
 
 
         backtrack(0,0,1,used)
