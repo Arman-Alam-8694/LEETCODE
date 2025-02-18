86286005e0d6bf7aafc22b09e6b8f021fc43f9f3
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-02-18 15:20:16 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (38.51%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.py
@@ -1,34 +1,19 @@
 class Solution:
     def smallestNumber(self, pattern: str) -> str:
-        new_pattern="A"+pattern
-        n=len(pattern)
-        listt=["-1"]*(n+1)
-        Pidx=0
-        lidx=0
-        used=set()
-        def backtrack(Pidx,lidx,start,used):
-            if Pidx==n+1:
+        n = len(pattern)
+        result, used = ["-1"] * (n + 1), set()
+
+        def backtrack(idx, start):
+            if idx == n + 1:
                 return True
-
-            if new_pattern[Pidx]=="A" or new_pattern[Pidx]=="I":
-                for i in range(start,n+2):
-                    if i not in used:
-                        listt[lidx]=str(i)
-                        used.add(i)
-                        if backtrack(Pidx+1,lidx+1,i,used):
-                            return True
-                        used.remove(i)
-
-            else:
-                for i in range(start,0,-1):
-                    if i not in used:
-                        listt[lidx]=str(i)
-                        used.add(i)
-                        if backtrack(Pidx+1,lidx+1,i,used):
-                            return True
-                        used.remove(i)
+            for i in (range(start, n + 2) if idx == 0 or pattern[idx - 1] == "I" else range(start, 0, -1)):
+                if i not in used:
+                    result[idx] = str(i)
+                    used.add(i)
+                    if backtrack(idx + 1, i):
+                        return True
+                    used.remove(i)
             return False
 
-
-        backtrack(0,0,1,used)
-        return "".join(listt)
+        backtrack(0, 1)
+        return "".join(result)
