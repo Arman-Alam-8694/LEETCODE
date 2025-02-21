d997607d4ac0ee9103edd7482c8bf56f727d605d
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-02-21 10:59:18 +0530

    Time: 2 ms (94.39%) | Memory: 22.1 MB (66.31%) - LeetSync

--- 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.py
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.py
@@ -1,36 +1,52 @@
 
 
 class FindElements:
-    def __init__(self, root: TreeNode):
-        self.root = root
+    #memory efficient code but time complexity O(N*logN)
+    # def __init__(self, root: TreeNode):
+    #     self.root = root
 
-    def getPath(self, target: int) -> str:
-        path=[]
-        while target>0:
-            path.append("L" if target%2 else "R")
-            target=(target-1)//2 if target%2 else (target-2)//2
-        return reversed(path)
+    # def getPath(self, target: int) -> str:
+    #     path=[]
+    #     while target>0:
+    #         path.append("L" if target%2 else "R")
+    #         target=(target-1)//2 if target%2 else (target-2)//2
+    #     return reversed(path)
 
-        # reverse_array=[]
-        # for i in range(-1,(-1*len(path))-1,-1):
-        #     reverse_array.append(path[i])
-        # return reverse_array
-        # return "".join(reversed(path))
-        # return "".join(reverse_array))
-      
+
+    # def find(self, target: int) -> bool:
+    #     path=self.getPath(target)
+    #     node=self.root
+    #     for i in path:
+    #         if i=="L" and node.left:
+    #             node=node.left
+    #         elif i=="R" and node.right:
+    #             node=node.right
+    #         else:
+    #             return False
+
+    #     return True
+
+    #faster and simpler to think appraoch with time complexity of O(N)
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.findelements=set()
+        self.populatelist(root,0)
+
+    def populatelist(self,node,val):
+        self.findelements.add(val)
+        if node.left:
+            new_val=val*2+1
+            self.populatelist(node.left,new_val)
+        if node.right:
+            new_val=val*2+2
+            self.populatelist(node.right,new_val)
+
+        
 
     def find(self, target: int) -> bool:
-        path=self.getPath(target)
-        node=self.root
-        for i in path:
-            if i=="L" and node.left:
-                node=node.left
-            elif i=="R" and node.right:
-                node=node.right
-            else:
-                return False
-
-        return True
+        if target in self.findelements:
+            return True
+        return False
        
 # Usage:
 # obj = FindElements(root)
