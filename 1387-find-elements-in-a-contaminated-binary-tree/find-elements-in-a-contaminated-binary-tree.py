45406a095e243e47e6cd640022f6b177502cc81e
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-02-21 10:43:00 +0530

    Time: 16 ms (20.59%) | Memory: 21.7 MB (93.05%) - LeetSync

--- 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.py
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.py
@@ -1,31 +1,33 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+
+
 class FindElements:
+    def __init__(self, root: TreeNode):
+        self.root = root
 
-    def __init__(self, root: Optional[TreeNode]):
-        self.findelements=set()
-        self.populatelist(root,0)
-
-    def populatelist(self,node,val):
-        self.findelements.add(val)
-        if node.left:
-            self.populatelist(node.left,val*2+1)
-        if node.right:
-            self.populatelist(node.right,val*2+2)
-
-        
+    def getPath(self, target: int) -> str:
+        path = []
+        while target != 0:
+            if target % 2:
+                path.append('L')
+                target = (target - 1) // 2
+            else:
+                path.append('R')
+                target = (target - 2) // 2
+        return ''.join(reversed(path))
 
     def find(self, target: int) -> bool:
-        if target in self.findelements:
-            return True
-        return False
-        
+        path = self.getPath(target)
+        cur_node = self.root
 
+        for direction in path:
+            if direction == 'L' and cur_node.left:
+                cur_node = cur_node.left
+            elif direction == 'R' and cur_node.right:
+                cur_node = cur_node.right
+            else:
+                return False
+        return True
 
-# Your FindElements object will be instantiated and called as such:
+# Usage:
 # obj = FindElements(root)
 # param_1 = obj.find(target)
