1b2a1ebf64b1ed985ae2904639041c99ae2fb109
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-02-22 13:13:24 +0530

    Time: 22 ms (46.76%) | Memory: 18.4 MB (38.53%) - LeetSync

--- 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.py
+++ 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.py
@@ -31,4 +31,45 @@ class Solution:
             
             stack.append(new_node)
         
+        #iterative for loop more complex implementation same tc and sc for both the solutions
+        # n=len(traversal)
+        # start=0
+        # temp=0
+        # while start<n and traversal[start]!="-" :
+        #     temp*=10
+        #     temp+=int(traversal[start])
+        #     start+=1
+        # stack=[]
+        # cur_depth=0
+        # root=TreeNode(temp)
+        # stack.append(root)
+        # temp=0
+        # num_found=False
+        # for i in range(start,n):
+        #     if traversal[i]=="-" and not num_found:
+        #         cur_depth+=1
+        #     elif traversal[i]!="-":
+        #         if not num_found:
+        #             num_found=True
+        #         temp*=10
+        #         temp+=int(traversal[i])
+        #     if (traversal[i]=="-" and num_found) or i==n-1:
+        #         new_node=TreeNode(temp)
+        #         temp=0
+        #         while stack:
+        #             last_node=stack[-1]
+        #             if len(stack)==cur_depth:
+        #                 if last_node.left is  None:
+        #                     last_node.left=new_node
+        #                     stack.append(new_node)
+        #                 else:
+        #                     last_node.right=new_node
+        #                     stack.append(new_node)     
+        #                 cur_depth=1
+        #                 num_found=False
+        #                 break
+        #             else:
+        #                 stack.pop()
+ 
+        # return root
         return stack[0] if stack else None
