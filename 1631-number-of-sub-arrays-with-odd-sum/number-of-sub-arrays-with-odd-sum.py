19444ee0102905ffc6b463e2417c2a6151e5ecdc
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-02-25 12:00:21 +0530

    Time: 163 ms (11.83%) | Memory: 25.1 MB (21.77%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.py
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.py
@@ -1,14 +1,48 @@
+from typing import List
+
 class Solution:
     def numOfSubarrays(self, arr: List[int]) -> int:
-        MOD=10**9+7
-        oddsum,evensum,res,runsum=0,0,0,0
-        for n in arr:
-            runsum+=n
-            if runsum&1:
-                oddsum+=1
-                res+=evensum+1
+        MOD = 10**9 + 7
+        n = len(arr)
+        odd_indices = [ i for i in range(n) if arr[i]&1]
+        k = len(odd_indices)
+        prev_even_sum=0
+        prev_odd_sum=0
+        result = 0
+        # print(odd_indices)
+
+        for i in range(k):
+            result+=1
+            cur_idx=odd_indices[i]
+            left=0
+            right=0
+            a,b=0,0
+            if i>0:
+                left=odd_indices[i-1]
+                a=(cur_idx-left)-1
             else:
-                evensum+=1
-                res+=oddsum
-        return res%MOD
-        
+                if cur_idx!=0:
+                    left=0
+                    a=(cur_idx-left)
+            if i<k-1:
+                right=odd_indices[i+1]
+                b=(right-cur_idx)-1
+            else:
+                if cur_idx!=n-1:
+                    right=n-1
+                    b=(right-cur_idx)
+            result+=(a+b)
+            result+=(a*b)
+            # print(f"we are at {i} index having element {odd_indices[i]} having a={a} and b={b} and prev_sum:{prev_even_sum} result={result}")
+            if i%2==0:
+                result+=prev_even_sum
+                result+=(prev_even_sum*b)
+                prev_even_sum+=(1+a)
+            # print(f"after updateing dp result {result} and prev_sum={prev_even_sum}")
+            else:
+                result+=prev_odd_sum
+                result+=(prev_odd_sum*b)
+                prev_odd_sum+=(1+a)
+        return result%MOD
+
+
