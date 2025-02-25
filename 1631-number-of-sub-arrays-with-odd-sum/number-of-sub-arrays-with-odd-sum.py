bc11e064f348addcdd2afdb90aee53f75ea27396
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-02-25 05:46:57 +0530

    Time: 63 ms (84.95%) | Memory: 21.9 MB (43.01%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.py
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.py
@@ -1,6 +1,6 @@
 class Solution:
     def numOfSubarrays(self, arr: List[int]) -> int:
-        mod=(10**9)+7
+        MOD=10**9+7
         oddsum,evensum,res,runsum=0,0,0,0
         for n in arr:
             runsum+=n
@@ -10,5 +10,5 @@ class Solution:
             else:
                 evensum+=1
                 res+=oddsum
-        return res%mod
+        return res%MOD
         
