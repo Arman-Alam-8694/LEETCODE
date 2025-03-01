6766999830c66e0c63943c40b0dd9bdd3cfdec84
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-03-01 23:07:05 +0530

    Time: 707 ms (34.40%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,2 +1,5 @@
 # Write your MySQL query statement bel
-select e.name from Employee as e join  (select managerId,count(managerId) as cnt from Employee group by managerId having cnt>=5) as newtable on newtable.managerId=e.id;
+-- select e.name from Employee as e join  (select managerId,count(managerId) as cnt from Employee group by managerId having cnt>=5) as newtable on newtable.managerId=e.id;
+
+select e2.name from Employee e1 inner join Employee e2 on e1.managerId = e2.id
+group by e1.managerId having count(e1.managerId)>=5;
