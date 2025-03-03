4594bce990b7cb4eef3cad157aeee4326493ec9f
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-03-03 10:24:21 +0530

    Time: 27 ms (77.60%) | Memory: 34.4 MB (83.89%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -1,28 +1,41 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        # n=len(nums)
+        # smaller=0
+        # larger=0
+        # equal=0
+        # result=[0]*n
+        # for i in nums:
+        #     if i==pivot:
+        #         equal+=1
+        #     elif i<pivot:
+        #         smaller+=1
+        #     else:
+        #         larger+=1
+        # left=0
+        # right=smaller
+        # for i in range(equal):
+        #     result[right]=pivot
+        #     right+=1
+        # for i in nums:
+        #     if i<pivot:
+        #         result[left]=i
+        #         left+=1
+        #     elif i>pivot:
+        #         result[right]=i
+        #         right+=1
+
+        # return result
+
+        small=[]
         n=len(nums)
-        smaller=0
-        larger=0
-        equal=0
-        result=[0]*n
-        for i in nums:
-            if i==pivot:
-                equal+=1
-            elif i<pivot:
-                smaller+=1
-            else:
-                larger+=1
-        left=0
-        right=smaller
-        for i in range(equal):
-            result[right]=pivot
-            right+=1
+        equal=[]
+        large=[]
         for i in nums:
             if i<pivot:
-                result[left]=i
-                left+=1
+                small.append(i)
             elif i>pivot:
-                result[right]=i
-                right+=1
-
-        return result
+                large.append(i)
+            else:
+                equal.append(i)
+        return small+equal+large
