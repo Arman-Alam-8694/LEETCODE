898fa8a918fd0a8ed0c209695a7e756a727c558d
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-03-03 10:31:12 +0530

    Time: 63 ms (16.23%) | Memory: 33.9 MB (93.26%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -1,41 +1,42 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        # n=len(nums)
-        # smaller=0
-        # larger=0
-        # equal=0
-        # result=[0]*n
-        # for i in nums:
-        #     if i==pivot:
-        #         equal+=1
-        #     elif i<pivot:
-        #         smaller+=1
-        #     else:
-        #         larger+=1
-        # left=0
-        # right=smaller
-        # for i in range(equal):
-        #     result[right]=pivot
-        #     right+=1
-        # for i in nums:
-        #     if i<pivot:
-        #         result[left]=i
-        #         left+=1
-        #     elif i>pivot:
-        #         result[right]=i
-        #         right+=1
-
-        # return result
-
-        small=[]
         n=len(nums)
-        equal=[]
-        large=[]
+        smaller=0
+        larger=0
+        equal=0
+        result=[0]*n
+        for i in nums:
+            if i==pivot:
+                equal+=1
+            elif i<pivot:
+                smaller+=1
+            else:
+                larger+=1
+        left=0
+        equal=smaller
+        right=n-larger
         for i in nums:
             if i<pivot:
-                small.append(i)
+                result[left]=i
+                left+=1
             elif i>pivot:
-                large.append(i)
+                result[right]=i
+                right+=1
             else:
-                equal.append(i)
-        return small+equal+large
+                result[equal]=i
+                equal+=1
+
+        return result
+
+        # small=[]
+        # n=len(nums)
+        # equal=[]
+        # large=[]
+        # for i in nums:
+        #     if i<pivot:
+        #         small.append(i)
+        #     elif i>pivot:
+        #         large.append(i)
+        #     else:
+        #         equal.append(i)
+        # return small+equal+large
