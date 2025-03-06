bb7a4e4a689160af1cfd2696efd37b36ceff6f40
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-03-07 00:02:58 +0530

    Time: 15 ms (31.25%) | Memory: 18.8 MB (8.94%) - LeetSync

---
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        mapp={}
+        result=[0,0]
+        m=len(grid)
+        sett=set([i for i in range(1,m*m+1)])
+        print(sett)
+        for i in range(m):
+            for j in range(m):
+                curr=grid[i][j]
+                sett.discard(curr)
+                mapp[curr]=mapp.get(curr,0)+1
+                if mapp[curr]==2:
+                    result[0]=curr
+        result[1]=next(iter(sett))
+        return result
+                
+
+
+        
