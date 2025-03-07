02152aa81fc62d9dcf3c2268abf5c921ec170027
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-03-07 10:32:40 +0530

    Time: 478 ms (60.94%) | Memory: 18 MB (73.44%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.py
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.py
@@ -1,36 +1,47 @@
-import math
 class Solution:
     def closestPrimes(self, left: int, right: int) -> List[int]:
-
-        def generate_primes(right,left):
-            listt=[True]*(right+1)
-            listt[0],listt[1]=False,False
-            for i in range(2,int(right*0.5)+1):
-                if listt[i]:
-                    for j in range(i*i,right+1,i):
-                        listt[j]=False
-            return [p for p in range(len(listt)) if listt[p] and p>=left]
-
-        listt=generate_primes(right,left)
+        # find all primes <= sqrt(right)
+        primes = [2, 3]
+        # only need to check up to ceil(sqrt(right))
+        mx = int(right ** 0.5) + 1
+        for k in range(5, mx, 2):
+            isPrime = True
+            for p in primes:
+                if k % p == 0:
+                    isPrime = False
+                    break
+            if isPrime:
+                primes.append(k)
+        #print(f"primes={primes}")
         
-                
-        minimum_dist=float('inf')
-        result=[-1,-1]
-        last_prime=None
-      
-        n=len(listt)
-
-        for j in range(n-1):
-         
-            if listt[j+1]-listt[j]<minimum_dist:
-                result[0]=listt[j]
-                result[1]=listt[j+1]
-                minimum_dist=listt[j+1]-listt[j]
-                # if minimum_dist==2:
-                #     return result
-
-        return result
-
-
-
-
+        primesSet = set(primes)
+        k = left
+        if k % 2 == 0:
+            k += 1
+        lastPrime = -1
+        minDiff = -1
+        r = [-1, -1]
+        while k <= right:
+            if k == 1:
+                lastPrime = 2
+                k += 2
+                continue
+            isPrime = True
+            if k not in primesSet:
+                for p in primes:
+                    if k % p == 0:
+                        isPrime = False
+                        break
+            #print(f"k={k} isPrime={isPrime} lastPrime={lastPrime} minDiff={minDiff} r={r}")
+            if isPrime:
+                if lastPrime != -1 and (minDiff == -1 or k - lastPrime < minDiff):
+                    r = [lastPrime, k]
+                    minDiff = k - lastPrime
+                lastPrime = k
+            # no need to check further
+            if minDiff == 2:
+                break
+            k += 2
+        return r
+        
+        
