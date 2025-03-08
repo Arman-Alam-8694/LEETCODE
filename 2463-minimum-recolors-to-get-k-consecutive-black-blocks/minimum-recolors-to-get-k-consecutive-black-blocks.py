7ab409b5f41f0d890e2d2aee249a5dab350919e5
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-03-08 10:12:52 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (88.78%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -3,18 +3,15 @@ class Solution:
         result=float('inf')
         n=len(blocks)
         left=0
-        bcnts,wcnts=0,0
+        wcnts=0
         for right in range(n):
             if blocks[right]=="W":
                 wcnts+=1
-            else:
-                bcnts+=1
+
             if ((right-left)+1)==k:
                 result=min(result,wcnts)
                 if blocks[left]=="W":
                     wcnts-=1
-                else:
-                    bcnts-=1
                 left+=1
 
         return result
