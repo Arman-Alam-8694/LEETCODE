19e56094498e398d62cbee0fb162742791bc4f16
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-03-11 10:53:54 +0530

    Time: 65 ms (94.66%) | Memory: 17.9 MB (67.33%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
@@ -8,33 +8,45 @@ class Solution:
         lasta=-1
         lastb=-1
         lastc=-1
-        for right in range(n):
-            if Found:
-                if s[right] not in sett:
-                    sett[s[right]]=0
-                sett[s[right]]+=1
-                if s[left]==s[right]:
-                    while left<right:
-                        sett[s[left]]-=1
-                        if sett[s[left]]==0:
-                            sett[s[left]]+=1
-                            break
-                        left+=1
-                result+=(left-(-1))
+        # for right in range(n):
+        #     if Found:
+        #         if s[right] not in sett:
+        #             sett[s[right]]=0
+        #         sett[s[right]]+=1
+        #         if s[left]==s[right]:
+        #             while left<right:
+        #                 sett[s[left]]-=1
+        #                 if sett[s[left]]==0:
+        #                     sett[s[left]]+=1
+        #                     break
+        #                 left+=1
+        #         result+=(left-(-1))
     
+        #     else:
+        #         if s[right] not in sett:
+        #             sett[s[right]]=0
+        #         sett[s[right]]+=1
+        #         if len(sett)==3:
+        #             while left<right:
+        #                 sett[s[left]]-=1
+        #                 if sett[s[left]]==0:
+        #                     sett[s[left]]+=1
+        #                     break
+        #                 left+=1
+        #             result+=(left-(-1))
+        #             Found=True
+
+        for right in range(n):
+            if s[right]=="a":
+                lasta=right
+            elif s[right]=="b":
+                lastb=right
             else:
-                if s[right] not in sett:
-                    sett[s[right]]=0
-                sett[s[right]]+=1
-                if len(sett)==3:
-                    while left<right:
-                        sett[s[left]]-=1
-                        if sett[s[left]]==0:
-                            sett[s[left]]+=1
-                            break
-                        left+=1
-                    result+=(left-(-1))
-                    Found=True
+                lastc=right
+            if lasta is not None and lastb is not None and lastc is not None:
+                temp=min(lasta,lastb,lastc)
+                result+=temp-(-1)
+
 
         return result
 
