9a32be18082fb39f0caf396cbc12e55a65b1977b
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-03-11 10:56:59 +0530

    Time: 81 ms (77.53%) | Memory: 17.9 MB (39.71%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
@@ -1,54 +1,63 @@
+# class Solution:
+#     def numberOfSubstrings(self, s: str) -> int:
+#         n=len(s)
+#         left=0
+#         Found=False
+#         sett={}
+#         result=0
+#         lasta=-1
+#         lastb=-1
+#         lastc=-1
+#         # for right in range(n):
+#         #     if Found:
+#         #         if s[right] not in sett:
+#         #             sett[s[right]]=0
+#         #         sett[s[right]]+=1
+#         #         if s[left]==s[right]:
+#         #             while left<right:
+#         #                 sett[s[left]]-=1
+#         #                 if sett[s[left]]==0:
+#         #                     sett[s[left]]+=1
+#         #                     break
+#         #                 left+=1
+#         #         result+=(left-(-1))
+    
+#         #     else:
+#         #         if s[right] not in sett:
+#         #             sett[s[right]]=0
+#         #         sett[s[right]]+=1
+#         #         if len(sett)==3:
+#         #             while left<right:
+#         #                 sett[s[left]]-=1
+#         #                 if sett[s[left]]==0:
+#         #                     sett[s[left]]+=1
+#         #                     break
+#         #                 left+=1
+#         #             result+=(left-(-1))
+#         #             Found=True
+
+#         for right in range(n):
+#             if s[right]=="a":
+#                 lasta=right
+#             elif s[right]=="b":
+#                 lastb=right
+#             else:
+#                 lastc=right
+#             if lasta is not None and lastb is not None and lastc is not None:
+#                 temp=min(lasta,lastb,lastc)
+#                 result+=temp-(-1)
+
+
+#         return result
+
+
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        n=len(s)
-        left=0
-        Found=False
-        sett={}
-        result=0
-        lasta=-1
-        lastb=-1
-        lastc=-1
-        # for right in range(n):
-        #     if Found:
-        #         if s[right] not in sett:
-        #             sett[s[right]]=0
-        #         sett[s[right]]+=1
-        #         if s[left]==s[right]:
-        #             while left<right:
-        #                 sett[s[left]]-=1
-        #                 if sett[s[left]]==0:
-        #                     sett[s[left]]+=1
-        #                     break
-        #                 left+=1
-        #         result+=(left-(-1))
-    
-        #     else:
-        #         if s[right] not in sett:
-        #             sett[s[right]]=0
-        #         sett[s[right]]+=1
-        #         if len(sett)==3:
-        #             while left<right:
-        #                 sett[s[left]]-=1
-        #                 if sett[s[left]]==0:
-        #                     sett[s[left]]+=1
-        #                     break
-        #                 left+=1
-        #             result+=(left-(-1))
-        #             Found=True
-
-        for right in range(n):
-            if s[right]=="a":
-                lasta=right
-            elif s[right]=="b":
-                lastb=right
-            else:
-                lastc=right
-            if lasta is not None and lastb is not None and lastc is not None:
-                temp=min(lasta,lastb,lastc)
-                result+=temp-(-1)
-
-
-        return result
-
-
+        last = { 'a': -1, 'b': -1, 'c': -1 }
+        result = 0
         
+        for right, char in enumerate(s):
+            last[char] = right  # Update last seen index
+            result += min(last.values()) + 1  # Count valid substrings
+            
+        return result
