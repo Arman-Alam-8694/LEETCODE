75ad7a62dc0c6592fee6b3c118974ccb04f72007
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-03-14 13:14:08 +0530

    Time: 495 ms (23.58%) | Memory: 29.8 MB (18.92%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.py
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.py
@@ -3,21 +3,28 @@ class Solution:
         left=1
         right=max(candies)
         res=0
-        def ifPossible(midd,k):
-            temp=0
-            for i in candies:
-                temp+=(i//midd)
-                if temp>=k:
-                    return True
-            return False 
+        # def ifPossible(midd,k):
+        #     temp=0
+        #     for i in candies:
+        #         temp+=(i//midd)
+        #         if temp>=k:
+        #             return True
+        #     return False 
             
 
         while left<=right:
             mid=(left+right)//2
-            if ifPossible(mid,k):
-                res=mid
-                left=mid+1
-            else:
+            temp=0
+            possible=False
+            for i in candies:
+                temp+=(i//mid)
+                if temp>=k:
+                    res=mid
+                    left=mid+1
+                    possible=True
+                    break
+            if not possible:
                 right=mid-1
+            
         return res
         
