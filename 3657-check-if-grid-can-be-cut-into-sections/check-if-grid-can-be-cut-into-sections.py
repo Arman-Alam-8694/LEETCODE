15fb2f73958583312824a95202dc84ca07da251f
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-03-25 12:07:14 +0530

    Time: 510 ms (44.82%) | Memory: 83.6 MB (46.62%) - LeetSync

--- 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.py
+++ 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.py
@@ -1,52 +1,42 @@
 class Solution:
     def checkValidCuts(self, n: int, rectangles: List[List[int]]) -> bool:
-        def calculate(rectangles, index_start, index_end):
-            rectangles.sort(key=lambda x: x[index_start])
-            count = 0
-            segment_count = 0
-            prev_coord = rectangles[0][index_start]
-            max_val = float("-inf")
-            min_val = float("inf")
-            prev_max = -1
-
-            for rect in rectangles:
-                if prev_coord != rect[index_start]:
-                    if prev_max != -1 and prev_max < max_val:
-                        count += max_val - prev_max
-                        if min_val >= prev_max:
-                            segment_count += 1
-
-                    if prev_max == -1:
-                        count += max_val
-                        if min_val >= prev_max:
-                            segment_count += 1
-
-                    prev_max = max(prev_max, max_val)
-                    prev_coord = rect[index_start]
-                    min_val = prev_coord
-
-                max_val = max(max_val, rect[index_end])
-                min_val = min(min_val, rect[index_start])
-
-            if max_val > prev_max:
-                count += max_val - prev_max
-                if min_val >= prev_max:
-                    segment_count += 1
-
-            return count, segment_count, max_val
-
-        y_count, y_segments, y_max = calculate(rectangles, 1, 3)
-        if y_count == y_max and y_segments >= 3:
-            return True
-
-        x_count, x_segments, x_max = calculate(rectangles, 0, 2)
-        if x_count == x_max and x_segments >= 3:
-            return True
-
-        return False
-
-                
+        def check(listt,maxx):
+            dictt=defaultdict(int)
+            for s,e in listt:
+                dictt[s]+=1
+                dictt[e-1]-=1
+            dictt=sorted(dictt.items(),key=lambda x :x[0])
+            run=0
+            cnt=-1
+            print(dictt)
+            for k,v in dictt:
+                run+=v
+                if run==0:
+                    print(k,v)
+                    cnt+=1
+                if cnt==2:
+                    return True
             
+            return False
+        
+
+        x_cords=[]
+        y_cords=[]
+        xmax=0
+        ymax=0
+        for c in rectangles:
+            x_cords.append((c[0],c[2]))
+            xmax=max(xmax,c[2])
+            y_cords.append((c[1],c[3]))
+            ymax=max(ymax,c[3])
+        # x_cords.sort(key=lambda x:x[0])
+        if check(x_cords,xmax):
+            return True
+        # print("y here")
+        # y_cords.sort(key=lambda y:y[0])
+        if check(y_cords,ymax):
+            return True
+        return False
         
         
         
