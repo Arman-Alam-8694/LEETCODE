d9bdd3a4fa6d0b94556dff608961b791ecc5ccc9
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-03-25 12:25:16 +0530

    Time: 397 ms (72.30%) | Memory: 83.7 MB (46.62%) - LeetSync

--- 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.py
+++ 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.py
@@ -1,20 +1,28 @@
 class Solution:
     def checkValidCuts(self, n: int, rectangles: List[List[int]]) -> bool:
         def check(listt):
-            dictt=defaultdict(int)
-            for s,e in listt:
-                dictt[s]+=1
-                dictt[e-1]-=1
-            dictt=sorted(dictt.items(),key=lambda x :x[0])
-            run=0
-            cnt=-1
-            for k,v in dictt:
-                run+=v
-                if run==0:
+            # dictt=defaultdict(int)
+            # for s,e in listt:
+            #     dictt[s]+=1
+            #     dictt[e-1]-=1
+            # dictt=sorted(dictt.items(),key=lambda x :x[0])
+            # run=0
+            # cnt=-1
+            # for k,v in dictt:
+            #     run+=v
+            #     if run==0:
+            #         cnt+=1
+            #     if cnt==2:
+            #         return True
+            listt.sort()
+            end=listt[0][1]
+            cnt=0
+            for i in range(len(listt)):
+                if end<=listt[i][0]:
                     cnt+=1
+                end=max(end,listt[i][1])
                 if cnt==2:
                     return True
-            
             return False
         x_cords=[]
         y_cords=[]
