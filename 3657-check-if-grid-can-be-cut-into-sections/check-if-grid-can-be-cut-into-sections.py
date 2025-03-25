2b57846e8ebae7615a19b88139cf347de7a962b2
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-03-25 12:08:22 +0530

    Time: 347 ms (79.28%) | Memory: 83.7 MB (46.62%) - LeetSync

--- 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.py
+++ 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.py
@@ -8,29 +8,21 @@ class Solution:
             dictt=sorted(dictt.items(),key=lambda x :x[0])
             run=0
             cnt=-1
-            # print(dictt)
             for k,v in dictt:
                 run+=v
                 if run==0:
-                    # print(k,v)
                     cnt+=1
                 if cnt==2:
                     return True
             
             return False
-        
-
         x_cords=[]
         y_cords=[]
         for c in rectangles:
             x_cords.append((c[0],c[2]))
             y_cords.append((c[1],c[3]))
-          
-        # x_cords.sort(key=lambda x:x[0])
         if check(x_cords):
             return True
-        # print("y here")
-        # y_cords.sort(key=lambda y:y[0])
         if check(y_cords):
             return True
         return False
