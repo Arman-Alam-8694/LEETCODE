5edfe30827a6adc838f237e3256aac160057fbef
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-03-26 11:34:04 +0530

    Time: 873 ms (5.26%) | Memory: 38.9 MB (78.95%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
@@ -1,54 +1,34 @@
 class Solution:
     def minOperations(self, grid: List[List[int]], x: int) -> int:
-        def check(midd,array):
-            # print(midd)
-            cnt=0
+        def check(midd, array):
+            cnt = 0
             for y in array:
-                if y==midd:
+                if y == midd:
                     continue
-                temp=abs(y-midd)
-                # print("here",temp,temp%x)
-                if temp%x==0:
-
-                    cnt+=(temp//x)
+                temp = abs(y - midd)
+                if temp % x == 0:
+                    cnt += (temp // x)
                 else:
                     return -1
             return cnt
-        array=[]
-        m=len(grid)
-        n=len(grid[0])
-        odd,even=0,0
+        
+        array = []
+        m = len(grid)
+        n = len(grid[0])
         for i in range(m):
             for j in range(n):
-                if grid[i][j]&1:
-                    odd+=1
-                else:
-                    even+=1
                 array.append(grid[i][j])
         array.sort()
-        # array.pop(0)
-        # array.pop()
-        # print(array)
-        if odd!=0 and odd!=m*n and x!=1:
-            if not x&1:
-                return -1
-
-        start=0
-        end=len(array)-1
-        mini=float("inf")
-        while start<=end:
-            mid=(start+end)//2
-            res=check(array[mid],array)
-            # print(res)
-            if res==-1:
-                start=mid+1
-
+        start = 0
+        end = len(array) - 1
+        mini = float("inf")
+        while start <= end:
+            mid = (start + end) // 2
+            res = check(array[mid], array)
+            if res == -1:
+                start = mid + 1  # Move right if impossible
             else:
-                if res<mini:
-                    mini=res
-
-                    end=mid-1
-                else:
-                    start=mid+1
-        return mini if mini!=float("inf") else -1
-        
+                if res < mini:
+                    mini = res
+                end = mid - 1  # Try lower values for fewer operations
+        return mini if mini != float("inf") else -1
