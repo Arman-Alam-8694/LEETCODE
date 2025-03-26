7932d55028ab9d5612c61763c5970ecda41f3b23
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-03-26 11:39:51 +0530

    Time: 861 ms (5.26%) | Memory: 38.9 MB (78.95%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
@@ -1,23 +1,33 @@
 class Solution:
     def minOperations(self, grid: List[List[int]], x: int) -> int:
-        def check(midd, array):
-            cnt = 0
-            for y in array:
-                if y == midd:
-                    continue
-                temp = abs(y - midd)
-                if temp % x == 0:
-                    cnt += (temp // x)
-                else:
-                    return -1
-            return cnt
-        
+        # Flatten grid into array
         array = []
         m = len(grid)
         n = len(grid[0])
         for i in range(m):
             for j in range(n):
                 array.append(grid[i][j])
+        
+        # Check congruence modulo x
+        if len(array) > 1:  # Only check if more than one element
+            base_remainder = array[0] % x
+            for num in array[1:]:
+                if num % x != base_remainder:
+                    return -1
+        
+        # If congruent, proceed with binary search
+        def check(midd, array):
+            cnt = 0
+            for y in array:
+                if y == midd:
+                    continue
+                temp = abs(y - midd)
+                if temp % x == 0:  # This is guaranteed now, but keep for clarity
+                    cnt += (temp // x)
+                else:
+                    return -1  # Won’t happen due to congruence check
+            return cnt
+        
         array.sort()
         start = 0
         end = len(array) - 1
@@ -26,9 +36,10 @@ class Solution:
             mid = (start + end) // 2
             res = check(array[mid], array)
             if res == -1:
-                start = mid + 1  # Move right if impossible
+                start = mid + 1  # Move right if impossible (rare now)
             else:
                 if res < mini:
                     mini = res
                 end = mid - 1  # Try lower values for fewer operations
+        
         return mini if mini != float("inf") else -1
