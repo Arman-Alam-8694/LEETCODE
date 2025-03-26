efedbbd30bba83faa8410b2869498570635e4663
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-03-26 11:55:00 +0530

    Time: 131 ms (88.16%) | Memory: 38.9 MB (60.96%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
@@ -19,30 +19,49 @@ class Solution:
         #             return -1
         
         # If congruent, proceed with binary search
-        def check(midd, array):
-            cnt = 0
-            for y in array:
-                if y == midd:
-                    continue
-                temp = abs(y - midd)
-                if temp % x == 0:  # This is guaranteed now, but keep for clarity
-                    cnt += (temp // x)
-                else:
-                    return -1  # Won’t happen due to congruence check
-            return cnt
+        # def check(midd, array):
+        #     cnt = 0
+        #     for y in array:
+        #         if y == midd:
+        #             continue
+        #         temp = abs(y - midd)
+        #         if temp % x == 0:  # This is guaranteed now, but keep for clarity
+        #             cnt += (temp // x)
+        #         else:
+        #             return -1  # Won’t happen due to congruence check
+        #     return cnt
         
         array.sort()
-        start = 0
-        end = len(array) - 1
-        mini = float("inf")
-        while start <= end:
-            mid = (start + end) // 2
-            res = check(array[mid], array)
-            if res == -1:
-                start = mid + 1  # Move right if impossible (rare now)
-            else:
-                if res < mini:
-                    mini = res
-                end = mid - 1  # Try lower values for fewer operations
+        mid=len(array)//2
+        median1=None
+        median=array[mid]
+        # if len(array)%2==0:
+        #     median1=array[mid+1]
+
+
+        # start = 0
+        # end = len(array) - 1
+        # mini = float("inf")
+        # while start <= end:
+        #     mid = (start + end) // 2
+        #     res = check(array[mid], array)
+        #     if res == -1:
+        #         start = mid + 1  # Move right if impossible (rare now)
+        #     else:
+        #         if res < mini:
+        #             mini = res
+        #         end = mid - 1  # Try lower values for fewer operations
         
-        return mini if mini != float("inf") else -1
+        def check(val,array):
+            cnt=0
+            for  i in array:
+                temp=abs(i-val)
+                cnt+=temp//x
+            return cnt
+        mini=float("inf")
+        # if median1 is not None:
+        #     mini=check(median1,array)
+        mini=min(mini,check(median,array))
+
+        
+        return mini 
