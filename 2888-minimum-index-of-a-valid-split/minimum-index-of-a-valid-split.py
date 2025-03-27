eda42516bf89ad8371887d63d89ce7c9f7af007a
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-03-27 14:05:44 +0530

    Time: 151 ms (14.36%) | Memory: 34.5 MB (38.61%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.py
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.py
@@ -1,19 +1,22 @@
 class Solution:
     def minimumIndex(self, nums: List[int]) -> int:
         dictt={}
+        item=0
         maxx=float("-inf")
         for i in nums:
             if i not in dictt:
                 dictt[i]=0
             dictt[i]+=1
             maxx=max(maxx,dictt[i])
+            if dictt[i]==maxx:
+                item=i
 
-        item=0
+        # item=0
 
-        for k,v in dictt.items():
-            if v==maxx:
-                item=k
-                break
+        # for k,v in dictt.items():
+        #     if v==maxx:
+        #         item=k
+        #         break
         n=len(nums)
         run=0
         for i in range(len(nums)-1):
