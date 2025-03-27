9ab6dcc97eeed42bf99d998b6575d3c8c1c68d86
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-03-27 12:09:05 +0530

    Time: 135 ms (16.83%) | Memory: 34.4 MB (50.99%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.py
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.py
@@ -7,11 +7,13 @@ class Solution:
                 dictt[i]=0
             dictt[i]+=1
             maxx=max(maxx,dictt[i])
+
         item=0
 
         for k,v in dictt.items():
             if v==maxx:
                 item=k
+                break
         n=len(nums)
         run=0
         for i in range(len(nums)-1):
