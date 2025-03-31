15d74fd9784c75cebde16d45309a7b87667be4a5
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-04-01 00:01:56 +0530

    Time: 127 ms (76.03%) | Memory: 30.4 MB (42.15%) - LeetSync

--- 2681-put-marbles-in-bags/put-marbles-in-bags.py
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.py
@@ -1,15 +1,11 @@
 class Solution:
     def putMarbles(self, weights: List[int], k: int) -> int:
-        # We collect and sort the value of all n - 1 pairs.
-        n = len(weights)
-        pairWeights = [weights[i] + weights[i + 1] for i in range(n - 1)]
-
-        # Since python's sort function sorts the whole list, we don't limit it to the first n-1 elements here.
-        pairWeights.sort()
-
-        # Get the difference between the largest k - 1 values and the smallest k - 1 values.
-        answer = 0
-        for i in range(k - 1):
-            answer += pairWeights[n - 2 - i] - pairWeights[i]
-
-        return answer
+        splits=[]
+        n=len(weights)
+        if k==1 or k==n:
+            return 0
+           
+        for i in range(1,n):
+            splits.append(weights[i]+weights[i-1])
+        splits.sort()
+        return sum(splits[-1*(k-1):])-sum(splits[:k-1])
