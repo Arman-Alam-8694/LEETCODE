18ae0e8aef7b4391d2ed8c211a78ceb4ce37de0f
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-04-01 13:33:14 +0530

    Time: 191 ms (11.87%) | Memory: 67.2 MB (36.03%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.py
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.py
@@ -1,11 +1,14 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
-        @cache
+        map={}
         def recur(start):
             if start==len(questions)-1:
                 return questions[start][0]
             if start>=len(questions):
                 return 0
-            return max(questions[start][0]+recur(start+questions[start][1]+1),recur(start+1))
+            if start in map:
+                return map[start]
+            map[start]=max(questions[start][0]+recur(start+questions[start][1]+1),recur(start+1))
+            return map[start]
         return recur(0)
        
