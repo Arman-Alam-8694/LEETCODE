c05ce7bb370ad53d7bb112d7ffdee06d3a3a22d2
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-04-03 02:13:47 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (54.41%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.py
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.py
@@ -1,18 +1,57 @@
+from typing import List
+
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
-        # maxx=0
-        # n=len(nums)
-        # for i in range(n-2):
-        #     for j in range(i+1,n-1):
-        #         for k in range(j+1,n):
-        #             temp=(nums[i]-nums[j])*nums[k]
-        #             maxx=max(maxx,temp)
+        maxx = 0
+        # Our prefix state: we use three variables:
+        #   left  – stores the best “left” candidate (largest element seen in a prefix pair)
+        #   center – stores the second element in the prefix pair
+        #   right – optionally stores an additional element when updating the prefix
+        left = None
+        center = None
+        right = None
+        count = 0  # number of elements processed in the prefix
+        
+        for num in nums:
+            # When we have at least two elements in the prefix (i.e. left and center set),
+            # we treat the current num as the “right” candidate of the triplet.
+            if count >= 2 and left is not None and center is not None:
+                candidate = (left - center) * num
+                if candidate > maxx:
+                    maxx = candidate
 
-        # return 0 if maxx<0 else maxx
-        n=len(nums)
-        dmax,lmax,res=0,0,0
-        for i in nums:
-            res=max(res,dmax*i)
-            dmax=max(dmax,lmax-i)
-            lmax=max(lmax,i)
-        return res
+            # Update the prefix state using only if/else conditions
+            if center is None:
+                # No element in the prefix yet; set center to current number.
+                center = num
+            elif left is None:
+                # Exactly one element in prefix; shift center to left and update center.
+                left = center
+                center = num
+            else:
+                # We already have a pair (left, center).
+                cur_value = left - center
+                # Determine potential_new_left as the maximum among left, center, and right (if set)
+                potential_new_left = left
+                if center > potential_new_left:
+                    potential_new_left = center
+                if right is not None and right > potential_new_left:
+                    potential_new_left = right
+
+                # Decide whether to update right or to shift the prefix pair.
+                if (potential_new_left - num) < cur_value:
+                    # The drop from potential_new_left to current num is smaller than the current pair difference.
+                    if right is None:
+                        right = num
+                    else:
+                        if num > right:
+                            right = num
+                else:
+                    # Otherwise, update the prefix: shift the best candidate into left and set center to current num.
+                    left = potential_new_left
+                    center = num
+                    right = None
+            count += 1
+        return maxx if maxx > 0 else 0
+
+
