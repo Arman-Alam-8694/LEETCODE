14b6bd9fbe9639c45fac60b950109bfe76f1f244
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-04-07 12:47:06 +0530

    Time: 811 ms (47.48%) | Memory: 156.4 MB (15.13%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,20 +1,28 @@
-from functools import cache
-
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        total = sum(nums)
-        if total % 2 != 0:
+        n=len(nums)
+        total=sum(nums)
+        if total&1:
             return False
-        target = total // 2
-        n = len(nums)
-
-        @cache
-        def subset(i, curr_sum):
-            if curr_sum == target:
-                return True
-            if curr_sum > target or i >= n:
+        totall=total//2
+        map={}
+        def subset(sum,start,n,totall):
+            if (sum,start) in map:
+                return map[(sum,start)]
+          
+            if sum>totall:
                 return False
-            # include nums[i] or skip it
-            return subset(i + 1, curr_sum + nums[i]) or subset(i + 1, curr_sum)
+            if sum==totall:
+                return True
+            if start>=n:
+                return False
+            result=subset(sum+nums[start],start+1,n,totall) or subset(sum,start+1,n,totall)
+            map[(sum,start)]=result
+            return map[(sum,start)]
+            
 
-        return subset(0, 0)
+        return subset(0,0,n,totall)
+       
+
+
+        
