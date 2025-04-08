fd113e22cb60030a5b12665a8e82e2628c8a9278
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-04-08 18:53:40 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (93.16%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.py
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.py
@@ -1,34 +1,38 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        mapp=Counter(nums)
-        ndistinct=set()
-        n=len(nums)
-        for k,v in mapp.items():
-            if v>1:
-                ndistinct.add(k)
+        # mapp=Counter(nums)
+        # ndistinct=set()
+        # n=len(nums)
+        # for k,v in mapp.items():
+        #     if v>1:
+        #         ndistinct.add(k)
 
-        if len(ndistinct)==0:
-            return 0
-        if n<=3:
-            return 1
-        cnt=0
-        steps=0
-        right=-1
-        # print(ndistinct)
-        # print(mapp)
-        for i in nums:
-            right+=1
-            cnt+=1
-            mapp[i]-=1
-            if mapp[i]==1:
-                # print(i)
-                # print(mapp)
-                ndistinct.remove(i)
-            if cnt==3:
-                steps+=1
-                if len(ndistinct)==0:
-                    return steps
-                if n-right<=3:
-                    return steps+1
-                cnt=0
-        return steps
+        # if len(ndistinct)==0:
+        #     return 0
+        # if n<=3:
+        #     return 1
+        # cnt=0
+        # steps=0
+        # right=-1
+
+        # for i in nums:
+        #     right+=1
+        #     cnt+=1
+        #     mapp[i]-=1
+        #     if mapp[i]==1:
+        #         ndistinct.remove(i)
+        #     if cnt==3:
+        #         steps+=1
+        #         if len(ndistinct)==0:
+        #             return steps
+        #         if n-right<=3:
+        #             return steps+1
+        #         cnt=0
+        # return steps
+        seen=set()
+        n=len(nums)
+        for right in range(n-1,-1,-1):
+            if nums[right] in seen:
+                return right//3+1
+            seen.add(nums[right])
+        return 0
