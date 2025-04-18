d814ba47a1d030e0cd9bd1718711e3ea37c4c10b
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-04-18 12:08:41 +0530

    Time: 6 ms (79.05%) | Memory: 17.8 MB (84.73%) - LeetSync

---
+++ 38-count-and-say/count-and-say.py
@@ -0,0 +1,28 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        def recur(time,num):
+            if time==1:
+                return num
+            temp=""
+            times=1
+            prev=None
+            for i in num:
+                if prev is None:
+                    prev=i
+                elif prev==i:
+                    times+=1
+                else:
+                    temp+=str(times)
+                    temp+=prev
+                    prev=i
+                    times=1
+            # print(temp,times)
+            temp+=str(times)
+            temp+=i
+            return recur(time-1,temp)
+        return recur(n,"1")
+                
+            
+
+
+        
