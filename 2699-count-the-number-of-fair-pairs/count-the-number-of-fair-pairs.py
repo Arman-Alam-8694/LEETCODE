d563e99feb2053e6f5acd6cb4809b9ad5ae243a9
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-04-19 14:15:22 +0530

    Time: 216 ms (64.61%) | Memory: 31.5 MB (86.76%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.py
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.py
@@ -1,73 +1,9 @@
 class Solution:
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
-       
-        # smallest=float(inf)
-        # for i in range(n):
-        #     if not temp:
-        #         smallest=nums[i]
-        #         temp.append(smallest)
-        #     else:
-        #         low=binsearch(nums[i],temp)
-        #         high=binsearch(nums[i],temp)
-        def binsearch(target,lower,upper,tempn):
-            ans=[]
-            left=0
-            right=len(tempn)-1
-            while left<=right:
-                mid=(left+right)//2
-                if tempn[mid]+target>=lower:
-                    right=mid-1
-                else:
-                    left=mid+1
-            ans.append(left)
-    
-            left=0
-            right=len(tempn)-1
-            while left<=right:
-                mid=(left+right)//2
-                if tempn[mid]+target<=upper:
-                    left=mid+1
-                else:
-                    right=mid-1
-            ans.append(right)
-            return ans
-
-
-        n=len(nums)
-      
-        temp=sorted(nums)
+        nums.sort()
+        res = 0  
+        for i, num in enumerate(nums): 
+            # lower - nums[j] <= nums[i] <= upper - nums[j]
+            res += bisect_right(nums, upper - num, 0, i) - bisect_left(nums, lower - num, 0, i) 
         
-        tempnext=[]
-        n=len(nums)
-        count=0
-        for i in range(n):
-            target=temp[i]
-            if not tempnext:
-                tempnext.append(target)
-            else:
-                low,high=binsearch(target,lower,upper,tempnext)
-                # print(f"temp:{t/empnext},low={low},high={high}")
-                if low==len(tempnext) or high==-1:
-                    tempnext.append(target)
-                elif low>high:
-                    if high==-1:
-                        count+=1
-                        tempnext.append(target)
-                    else:
-                        tempnext.append(target)
-
-                    
-                elif low==high:
-                    if lower<=tempnext[low]+target<=upper:
-                        count+=1
-                        tempnext.append(target)
-                    else:
-                        tempnext.append(target)
-                else:
-                    count+=(high-low)+1
-                    tempnext.append(target)
-                # print(count)
-
-
-
-        return count
+        return res 
