8515cb0414d65fd4271e0f0c11a3bb032f20e1ac
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-04-19 19:06:44 +0530

    Time: 106 ms (90.41%) | Memory: 31.5 MB (53.88%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.py
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.py
@@ -1,4 +1,4 @@
-from typing import List
+
 
 class Solution:
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
@@ -6,31 +6,47 @@ class Solution:
         n = len(nums)
         res = 0
 
-        def bisect_left(num, start):
-            low, high = start, n - 1
-            while low <= high:
-                mid = (low + high) // 2
-                if nums[mid] < num:
-                    low = mid + 1
+        def helper(nums,target):
+            left=0
+            right=n-1
+            temp=0
+            while left<right:
+                if nums[left]+nums[right]<target:
+                    left+=1
                 else:
-                    high = mid - 1
-            return low
+                    temp+=right-left
+                    right-=1
+            return temp
+        # def bisect_left(num, start):
+        #     low, high = start, n - 1
+        #     while low <= high:
+        #         mid = (low + high) // 2
+        #         if nums[mid] < num:
+        #             low = mid + 1
+        #         else:
+        #             high = mid - 1
+        #     return low
 
-        def bisect_right(num, start):
-            low, high = start, n - 1
-            while low <= high:
-                mid = (low + high) // 2
-                if nums[mid] <= num:
-                    low = mid + 1
-                else:
-                    high = mid - 1
-            return low
+        # def bisect_right(num, start):
+        #     low, high = start, n - 1
+        #     while low <= high:
+        #         mid = (low + high) // 2
+        #         if nums[mid] <= num:
+        #             low = mid + 1
+        #         else:
+        #             high = mid - 1
+        #     return low
 
-        for i in range(n):
-            low = lower - nums[i]
-            high = upper - nums[i]
-            left = bisect_left(low, i + 1)
-            right = bisect_right(high, i + 1)
-            res += right - left
+        # for i in range(n):
+        #     low = lower - nums[i]
+        #     high = upper - nums[i]
+        #     left = bisect_left(low, i + 1)
+        #     right = bisect_right(high, i + 1)
+        #     res += right - left
 
+        # return res
+        res=helper(nums,lower)-helper(nums,upper+1)
         return res
+
+
+
