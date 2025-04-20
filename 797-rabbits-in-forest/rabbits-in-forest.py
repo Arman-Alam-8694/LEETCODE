eedddcd604e176b8c7bccb3679d094de19701d97
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-04-20 12:25:03 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (50.96%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.py
+++ 797-rabbits-in-forest/rabbits-in-forest.py
@@ -1,10 +1,20 @@
 class Solution:
     def numRabbits(self, answers: List[int]) -> int:
-        # 1 1 1
-        # 1 - 2
-        # 1 1 -2 pairs
-        # 1 -2 
-        # 1-3 times
+
+
+        # so in this the only trick part is knowing how will group the rabbits
+        #for example if rabbit i says i have seen 3 rabbits that means at least there are 4 rabbits
+        #of same kind right,so the moment we find 4 rabbits to say the same number that means the group
+        #is completed right, the moment we encounter i+1 times rabbit saying the same number i that
+        #group is completed, we have to then again start the same procedure when we encounter the same
+        #number right for example 1 1 1 - when you are index 1 you know you have seen the same group 
+        #but then you find another same rabbit saying the same number this could only mean a new 
+        #rabbit type so we start the same procedure again 
+        #if the type of rabbit is not found then we do simple result+=(i+1) until i+1 has rabbits says
+        #the same number right for 0 we remove it just after because we have found 0+1 saying the 
+        #same number 
+
+
         n=len(answers)
         mapp={}
         result=0
