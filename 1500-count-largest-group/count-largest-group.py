6c27088d969a97f43b65932543880149054bc0a6
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-04-23 14:17:09 +0530

    Time: 28 ms (68.25%) | Memory: 17.8 MB (72.25%) - LeetSync

---
+++ 1500-count-largest-group/count-largest-group.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countLargestGroup(self, n: int) -> int:
+        mapp=defaultdict(int)
+        largest=0
+        large_count=0
+        for num in range(1,n+1):
+            summ=0
+            while num!=0:
+                summ+=num%10
+                num//=10
+            mapp[summ]+=1
+            if mapp[summ]>largest:
+                largest=mapp[summ]
+                large_count=1
+            elif mapp[summ]==largest:
+                # print(summ)
+                large_count+=1
+        return large_count
+            
