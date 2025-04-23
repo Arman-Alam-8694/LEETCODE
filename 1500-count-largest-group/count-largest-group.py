6c62231aff8c1a0c2843da9da90a92414d90e848
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-04-23 14:27:01 +0530

    Time: 62 ms (32.75%) | Memory: 17.9 MB (53.00%) - LeetSync

--- 1500-count-largest-group/count-largest-group.py
+++ 1500-count-largest-group/count-largest-group.py
@@ -4,17 +4,21 @@ class Solution:
         largest=0
         large_count=0
         for num in range(1,n+1):
-            summ=0
-            while num!=0:
-                summ+=num%10
-                num//=10
+            # summ=0
+            # while num!=0:
+            #     summ+=num%10
+            #     num//=10
+            # mapp[summ]+=1
+            summ=sum([int(k) for k in str(num)])
             mapp[summ]+=1
+            # print(summ)
             if mapp[summ]>largest:
                 largest=mapp[summ]
                 large_count=1
             elif mapp[summ]==largest:
                 # print(summ)
                 large_count+=1
+            
         return large_count
         
             
