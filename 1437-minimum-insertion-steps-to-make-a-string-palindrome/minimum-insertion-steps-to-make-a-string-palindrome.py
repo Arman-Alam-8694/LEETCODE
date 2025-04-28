b0638a3de9adb76207c42d6ee78db5938d79943f
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-04-28 23:54:39 +0530

    Time: 588 ms (63.59%) | Memory: 186.1 MB (10.74%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -1,17 +1,18 @@
-from functools import lru_cache
+from functools import cache
 
 class Solution:
     def minInsertions(self, s: str) -> int:
-        n = len(s)
-        s_rev = s[::-1]
-
-        @lru_cache(maxsize=None)
-        def lcs(i: int, j: int) -> int:
-            if i == 0 or j == 0:
+        @cache
+        def longestPalindromeSubseq(start: int, end: int) -> int:
+            if start > end:
                 return 0
-            if s[i - 1] == s_rev[j - 1]:
-                return 1 + lcs(i - 1, j - 1)
-            return max(lcs(i - 1, j), lcs(i, j - 1))
-
-        longest_palindromic_subseq = lcs(n, n)
-        return n - longest_palindromic_subseq
+            if start == end:
+                return 1
+            if s[start] == s[end]:
+                return 2 + longestPalindromeSubseq(start + 1, end - 1)
+            return max(longestPalindromeSubseq(start + 1, end),
+                       longestPalindromeSubseq(start, end - 1))
+        
+        n = len(s)
+        lps = longestPalindromeSubseq(0, n - 1)
+        return n - lps
