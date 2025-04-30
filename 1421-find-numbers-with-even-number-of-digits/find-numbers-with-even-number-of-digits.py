1fbfefd30ba2fd98752c040a24bf02fdb140ab78
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-04-30 13:18:38 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (13.14%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.py
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.py
@@ -6,13 +6,22 @@ class Solution:
         #         even+=1
 
         # return even
+        # for n in nums:
+        #     cnt=0
+        #     while n!=0:
+        #         n//=10
+        #         cnt+=1
+        #     if not cnt&1:
+        #         even+=1
+        # return even
         for n in nums:
-            cnt=0
-            while n!=0:
-                n//=10
-                cnt+=1
-            if not cnt&1:
+            if 9<n<100:
+                even+=1
+            elif 999<n<10000:
+                even+=1
+            elif 99999<n<1000000:
                 even+=1
         return even
+
 
         
