313ddb4757e35567ec4658424c037b456ee13fa2
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-05-04 12:16:17 +0530

    Time: 8 ms (32.94%) | Memory: 21.2 MB (61.93%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,9 +1,11 @@
-from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         mapp=Counter(nums)
-        new =sorted(mapp.items(),key=lambda x:x[1],reverse=True)
-        result=[]
-        for i in range(k):
-            result.append(new[i][0])
-        return result    
+        stack=[]
+        for key,v in mapp.items():
+            heapq.heappush(stack,(v,key))
+            if len(stack)>k:
+                heapq.heappop(stack)
+        return [key for v,key in stack]
+
+        
