310d04dc8ee427c9d3fccd0b12db6e65e5db7746
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-05-05 19:15:45 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (40.13%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.py
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.py
@@ -1,5 +1,13 @@
 class Solution:
     def numTilings(self, n: int) -> int:
+        MOD=10**9+7
+        dp=[0]*(n+4)
+        dp[0]=1
+        dp[1]=1
+        dp[2]=2
+        for i in range(3,n+1):
+            dp[i]=(2*dp[i-1]+dp[i-3])
+        return dp[n]%MOD
         #RECURSIVE 
         # MOD=10**9+7
         # @cache
@@ -40,11 +48,11 @@ class Solution:
         # return recur(0,0)
 
 
-        dp=[0]*(n+4)
-        MOD=10**9+7
-        dp[0]=1
-        dp[1]=1
-        dp[2]=2
-        for i in range(3,n+1):
-            dp[i]=(2*dp[i-1]+dp[i-3])%MOD
-        return dp[n]
+        # dp=[0]*(n+4)
+        # MOD=10**9+7
+        # dp[0]=1
+        # dp[1]=1
+        # dp[2]=2
+        # for i in range(3,n+1):
+        #     dp[i]=(2*dp[i-1]+dp[i-3])%MOD
+        # return dp[n]
