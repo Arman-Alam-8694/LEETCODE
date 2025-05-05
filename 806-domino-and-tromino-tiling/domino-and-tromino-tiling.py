1954269b49443e22713da9f7865832ad23de4b74
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-05-05 19:20:12 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (69.39%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.py
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.py
@@ -1,48 +1,48 @@
 class Solution:
     def numTilings(self, n: int) -> int:
-        # MOD=10**9+7
-        # dp=[0]*(n+4)
-        # dp[0]=1
-        # dp[1]=1
-        # dp[2]=2
-        # for i in range(3,n+1):
-        #     dp[i]=(2*dp[i-1]+dp[i-3])
-        # return dp[n]%MOD
-        # RECURSIVE 
         MOD=10**9+7
-        @cache
-        def recur(x,y):
-            if x>n or y>n:
-                return 0
-            if x==n and y==n:
-                return 1
+        dp=[0]*(n+4)
+        dp[0]=1
+        dp[1]=1
+        dp[2]=2
+        for i in range(3,n+1):
+            dp[i]=(2*dp[i-1]+dp[i-3])
+        return dp[n]%MOD
+        # RECURSIVE 
+        # MOD=10**9+7
+        # @cache
+        # def recur(x,y):
+        #     if x>n or y>n:
+        #         return 0
+        #     if x==n and y==n:
+        #         return 1
             
-            if x==y:
+        #     if x==y:
                  
-                ways=(
-                    recur(x+1,y+1)+
-                    recur(x+2,y+2)+
-                    recur(x+2,y+1)+
-                    recur(x+1,y+2)
+        #         ways=(
+        #             recur(x+1,y+1)+
+        #             recur(x+2,y+2)+
+        #             recur(x+2,y+1)+
+        #             recur(x+1,y+2)
 
-                )
+        #         )
           
-            elif x>y:
+        #     elif x>y:
              
-                ways=(
-                    recur(x,y+2)+
-                    recur(x+1,y+2)
-                )
+        #         ways=(
+        #             recur(x,y+2)+
+        #             recur(x+1,y+2)
+        #         )
             
-            else:
+        #     else:
               
-                ways=(
-                    recur(x+2,y)+    
-                    recur(x+2,y+1)
-                )
-            return (ways)%MOD
+        #         ways=(
+        #             recur(x+2,y)+    
+        #             recur(x+2,y+1)
+        #         )
+        #     return (ways)%MOD
     
-        return recur(0,0)
+        # return recur(0,0)
 
 
         # dp=[0]*(n+4)
