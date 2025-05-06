f267462fd5c11298f5190bfb1a0e128fbfa92479
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-05-06 12:59:01 +0530

    Time: 1 ms (59.41%) | Memory: 18.2 MB (13.03%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.py
+++ 2048-build-array-from-permutation/build-array-from-permutation.py
@@ -1,7 +1,14 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        result=[]
+        # result=[]
+        # for i in range(len(nums)):
+        #     result.append(nums[nums[i]])
+        # return result
+
+        # modifying the array
         for i in range(len(nums)):
-            result.append(nums[nums[i]])
-        return result
+            nums[i]+=1000*(nums[nums[i]]%1000)
+        for i in range(len(nums)):
+            nums[i]=nums[i]//1000
+        return nums
         
