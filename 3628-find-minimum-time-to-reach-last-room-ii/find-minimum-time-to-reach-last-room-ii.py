1d93320009ea962d5fd9f0c3bf78a8c6d07b9eb8
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-05-07 14:34:49 +0530

    Time: 2314 ms (40.93%) | Memory: 145 MB (26.98%) - LeetSync

---
+++ 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.py
@@ -0,0 +1,60 @@
+class Solution:
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        seen=set()
+        row=len(moveTime)
+        col=len(moveTime[0])
+        direction=[(1,0),(0,1),(-1,0),(0,-1)]
+        heap=[(0,0,0,1)]
+        seen.add((0,0))
+        prev=(-1,-1)
+        counter=1
+
+        while heap:
+            time,x,y,counter=heapq.heappop(heap)
+            if x==row-1 and y==col-1:
+                return time
+            print(x,y,time,counter)
+            # if (x,y) in seen:
+            #     continue
+            # seen.add((x,y))
+            for dx,dy in direction:
+                nx=x+dx
+                ny=y+dy
+                if (nx,ny) not in seen and 0<=nx<row and 0<=ny<col:
+                    seen.add((nx,ny))
+                    if counter==2:
+                            heapq.heappush(heap,(max(time+counter,moveTime[nx][ny]+counter),nx,ny,counter-1))
+                    else:
+                        heapq.heappush(heap,(max(time+counter,moveTime[nx][ny]+counter),nx,ny,counter+1))
+                        
+
+                    # else:
+                    #     heapq.heappush(heap,(max(time+1,moveTime[nx][ny]+1),nx,ny,1))
+    
+            # prev=(x,y)
+
+        # 0 0 0 0
+        # 0 0 0 0
+
+                    
+                    
+                    
+                   
+        # 0 4 
+        # 4 4
+        # 3
+        
+
+        # 22
+        # 3,3
+        # 12 21 32 23
+        # 02 20 42 24
+        23 
+
+
+
+    
+                    
+                    
+
+        
