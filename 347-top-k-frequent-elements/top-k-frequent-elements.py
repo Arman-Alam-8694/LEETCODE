32e2c2e8932328c3a15c5a2ddd9dbcd56716591a
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-05-08 00:02:42 +0530

    Time: 12 ms (15.01%) | Memory: 24.6 MB (7.58%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,12 +1,13 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        mapp=Counter(nums)
-        stack=[]
-        for key,v in mapp.items():
-            heapq.heappush(stack,(v,key))
-            if len(stack)>k:
-                heapq.heappop(stack)
-        return [key for v,key in stack]
+        #nlogk better than nlogn
+        # mapp=Counter(nums)
+        # stack=[]
+        # for key,v in mapp.items():
+        #     heapq.heappush(stack,(v,key))
+        #     if len(stack)>k:
+        #         heapq.heappop(stack)
+        # return [key for v,key in stack]
 
         # mapp=Counter(nums)
         # new =sorted(mapp.items(),key=lambda x:x[1],reverse=True)
@@ -15,4 +16,24 @@ class Solution:
         #     result.append(new[i][0])
         # return result  
 
+        #BUCKET SORT basically sort on the basis of the frequency
         
+        # get the frequencies
+        mapp=Counter(nums)
+        #form the buckets
+        n=len(nums)
+        buckets=[[] for i in range(n+1)]
+        print(mapp)
+        #go throught he mapp for each number and it's frequency add the 
+        #same number to their respective bucket size (frequency)
+        for key,v in mapp.items():
+            buckets[v].append(key)    
+        result=[]
+        print(buckets)
+        #traverse backward of the bucket for most frequencies 
+        for b in range(n,-1,-1):
+            for key in buckets[b]:
+                result.append(key)
+                if len(result)==k:
+                    return result
+        return result    
