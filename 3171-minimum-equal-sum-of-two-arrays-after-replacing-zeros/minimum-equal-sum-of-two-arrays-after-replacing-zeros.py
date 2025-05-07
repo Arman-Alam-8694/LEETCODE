07aa9d3531affdfea2fcdfaa31307cdc2f70c27a
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-05-07 17:44:03 +0530

    Time: 859 ms (46.73%) | Memory: 34.9 MB (42.12%) - LeetSync

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.py
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.py
@@ -1,6 +1,6 @@
-import atexit
+# import atexit
 
-atexit.register(lambda: open("display_runtime.txt", "w").write("0"))
+# atexit.register(lambda: open("display_runtime.txt", "w").write("0"))
 
 class Solution:
     def minSum(self, nums1: List[int], nums2: List[int]) -> int:
