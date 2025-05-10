b9cbd1fde2b5c52e69395aa80f3f003bef227c91
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-05-10 18:09:21 +0530

    Time: 7572 ms (5.08%) | Memory: 17.9 MB (65.49%) - LeetSync

---
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findEvenNumbers(self, digits: List[int]) -> List[int]:
+        res=set()
+        n=len(digits)
+        for i in range(n):
+            temp=[str(digits[i])]
+            if temp==["0"]:
+                temp.pop()
+                continue
+            for j in range(n):
+                temp.append(str(digits[j]))
+                for k in range(n):
+                    temp.append(str(digits[k]))
+                    if not(i==j or j==k or k==i):
+                        if int("".join(temp))%2==0:
+                            res.add(int("".join(temp)))
+                    temp.pop()
+                temp.pop()
+            temp.pop()
+        return sorted(res)
+                        
+        
