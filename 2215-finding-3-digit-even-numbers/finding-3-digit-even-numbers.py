9ee7636f3967babdcfc0785498ad8b1ecb0c9d2e
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-05-10 18:18:27 +0530

    Time: 33 ms (72.53%) | Memory: 17.8 MB (90.23%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.py
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.py
@@ -1,22 +1,44 @@
 class Solution:
     def findEvenNumbers(self, digits: List[int]) -> List[int]:
-        res=set()
-        n=len(digits)
-        for i in range(n):
-            temp=[str(digits[i])]
-            if temp==["0"]:
-                temp.pop()
-                continue
-            for j in range(n):
-                temp.append(str(digits[j]))
-                for k in range(n):
-                    temp.append(str(digits[k]))
-                    if not(i==j or j==k or k==i):
-                        if int("".join(temp))%2==0:
-                            res.add(int("".join(temp)))
-                    temp.pop()
-                temp.pop()
-            temp.pop()
-        return sorted(res)
+        # res=set()
+        # n=len(digits)
+        # for i in range(n):
+        #     temp=[str(digits[i])]
+        #     if temp==["0"]:
+        #         temp.pop()
+        #         continue
+        #     for j in range(n):
+        #         temp.append(str(digits[j]))
+        #         for k in range(n):
+        #             temp.append(str(digits[k]))
+        #             if not(i==j or j==k or k==i):
+        #                 if int("".join(temp))%2==0:
+        #                     res.add(int("".join(temp)))
+        #             temp.pop()
+        #         temp.pop()
+        #     temp.pop()
+        # return sorted(res)
+
+        result=[]
+        mapp=Counter(digits)
+        for i in range(100,1000):
+            temp=defaultdict(int)
+            b=i
+            if i%2==0:
+                while i!=0:
+                    a=i%10
+                    temp[a]+=1
+                    i=i//10
+                found=True
+                for i,j in temp.items():
+                    if mapp[i]<j:
+                        found=False
+                        break
+                if found:
+                    result.append(b)
+        return result
+                    
+
+
                         
         
