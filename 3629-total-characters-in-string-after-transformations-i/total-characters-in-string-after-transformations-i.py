0d4cf9cd9f7de0baced91b16d4c6fe5247ca5e1a
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-05-12 15:41:21 +0530

    Time: 355 ms (93.46%) | Memory: 18.4 MB (36.60%) - LeetSync

--- 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.py
+++ 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.py
@@ -2,39 +2,34 @@ class Solution:
     def lengthAfterTransformations(self, s: str, t: int) -> int:
         res=0
         MOD=10**9+7
-        # for i in s:
-        #     req=ord("z")-ord(i)+1
-        #     # print(req)
-        #     if req>t:
-        #         res+=1
-        #     else:
-        #         rem=t-req
-        #         res+=2
-        #         #here a,b is formed
-        #         rounds,fora=divmod(rem,25)
-        #         if rounds>0 and fora>0:
-        #             res+=2*rounds
-        #         elif rounds>0 and fora==0:
-        #             res+=2*rounds-1
-
-        #     # print(req)
      
         temp=[0]*26
         for i in s:
             temp[ord(i)-ord("a")]+=1
-        for rounds in range(t):
-            next=[0]*26
-            for i in range(0,26):
-                if temp[i]>0:
-                    if i==25:
-                        next[0]+=temp[i]
-                        next[1]+=temp[i]
+        queue=deque(temp)
+        # for rounds in range(t):
+        #     next=[0]*26
+        #     for i in range(0,26):
+        #         if temp[i]>0:
+        #             if i==25:
+        #                 next[0]+=temp[i]
+        #                 next[1]+=temp[i]
                         
-                    else:
-                        next[i+1]+=temp[i]
-            # print(next)
-            temp=next
-        return sum(temp)%MOD
+        #             else:
+        #                 next[i+1]+=temp[i]
+        #     # print(next)
+        #     temp=next
+        # return sum(temp)%MOD
 
+
+        #USING DEQUE simulation kind of thing we pop the z and add it to a counts
+        #to make final b counts and then just pop the z one and appendleft to form the
+        #new A counts and by doing so we have shifted the counts of each char to char+1 
+        #hence effectively simmulating as well compared to upper brute force of 
+        #additional O(26) at each t 
+        for i in range(t):
+            queue[0]+=queue[25]
+            queue.appendleft(queue.pop())
+        return sum(queue)%MOD
 
 
