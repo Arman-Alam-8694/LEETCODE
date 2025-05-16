159b318c91671b4b25fc4bd2e4d3ef1ae59f4705
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-05-16 12:39:35 +0530

    Time: 2794 ms (6.82%) | Memory: 131.3 MB (6.82%) - LeetSync

--- 3142-longest-unequal-adjacent-groups-subsequence-ii/longest-unequal-adjacent-groups-subsequence-ii.py
+++ 3142-longest-unequal-adjacent-groups-subsequence-ii/longest-unequal-adjacent-groups-subsequence-ii.py
@@ -1,38 +1,36 @@
-from typing import List
-from functools import cache
-
 class Solution:
     def getWordsInLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
-        n = len(words)
-
-        def checkhamming(first, second):
-            cnt = 0
+        n=len(words)
+        def checkhamming(first,second):
+            cnt=0
+            
             for i in range(len(first)):
-                if second[i] != first[i]:
-                    cnt += 1
-                    if cnt > 1:
-                        return False
-            return cnt == 1  # exactly one difference
-
+                if second[i]!=first[i]:
+                    cnt+=1
+                if cnt>1:
+                    return False
+            return True
+        result=[]
         @cache
-        def recur(prev, curidx):
-            if curidx == n:
+        def recur(prev,curidx):
+            take=[]
+            if curidx==n:
                 return []
-
-            take = []
-            skip = recur(prev, curidx + 1)
-
-            if prev == -1:
-                # Always valid to start a new sequence
-                take = [words[curidx]] + recur(curidx, curidx + 1)
-            elif len(words[prev]) == len(words[curidx]) and groups[prev] != groups[curidx]:
-                if checkhamming(words[prev], words[curidx]):
-                    take = [words[curidx]] + recur(curidx, curidx + 1)
-
-            # Return the longer of take or skip
-            if len(take) > len(skip):
+            skip=recur(prev,curidx+1)
+            if prev==-1:
+                take=[words[curidx]]+recur(curidx,curidx+1)
+               
+            elif len(words[prev])==len(words[curidx]):
+                check=checkhamming(words[prev],words[curidx])
+                if check and groups[prev]!=groups[curidx]:
+                    take=[words[curidx]]+recur(curidx,curidx+1)
+            
+            if len(take)>len(skip):
                 return take
             else:
                 return skip
 
-        return recur(-1, 0)
+        return recur(-1,0)
+
+
+        
