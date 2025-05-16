dac3266ec406499b536c8c1263a424058071e7ae
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-05-16 12:51:28 +0530

    Time: 704 ms (84.09%) | Memory: 18 MB (88.64%) - LeetSync

--- 3142-longest-unequal-adjacent-groups-subsequence-ii/longest-unequal-adjacent-groups-subsequence-ii.py
+++ 3142-longest-unequal-adjacent-groups-subsequence-ii/longest-unequal-adjacent-groups-subsequence-ii.py
@@ -1,36 +1,71 @@
+# class Solution:
+#     def getWordsInLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
+#         n=len(words)
+#         def checkhamming(first,second):
+#             cnt=0
+            
+#             for i in range(len(first)):
+#                 if second[i]!=first[i]:
+#                     cnt+=1
+#                 if cnt>1:
+#                     return False
+#             return True
+#         result=[]
+#         @cache
+#         def recur(prev,curidx):
+#             take=[]
+#             if curidx==n:
+#                 return []
+#             skip=recur(prev,curidx+1)
+#             if prev==-1:
+#                 take=[words[curidx]]+recur(curidx,curidx+1)
+               
+#             elif len(words[prev])==len(words[curidx]):
+#                 check=checkhamming(words[prev],words[curidx])
+#                 if check and groups[prev]!=groups[curidx]:
+#                     take=[words[curidx]]+recur(curidx,curidx+1)
+            
+#             if len(take)>len(skip):
+#                 return take
+#             else:
+#                 return skip
+
+#         return recur(-1,0)
+
+from typing import List
+
 class Solution:
     def getWordsInLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
-        n=len(words)
-        def checkhamming(first,second):
-            cnt=0
-            
+        n = len(words)
+
+        def checkhamming(first, second):
+            cnt = 0
             for i in range(len(first)):
-                if second[i]!=first[i]:
-                    cnt+=1
-                if cnt>1:
-                    return False
-            return True
-        result=[]
-        @cache
-        def recur(prev,curidx):
-            take=[]
-            if curidx==n:
-                return []
-            skip=recur(prev,curidx+1)
-            if prev==-1:
-                take=[words[curidx]]+recur(curidx,curidx+1)
-               
-            elif len(words[prev])==len(words[curidx]):
-                check=checkhamming(words[prev],words[curidx])
-                if check and groups[prev]!=groups[curidx]:
-                    take=[words[curidx]]+recur(curidx,curidx+1)
-            
-            if len(take)>len(skip):
-                return take
-            else:
-                return skip
+                if first[i] != second[i]:
+                    cnt += 1
+                    if cnt > 1:
+                        return False
+            return cnt == 1
 
-        return recur(-1,0)
+        dp = [1] * n              # dp[i] = longest subsequence ending at i
+        prev_idx = [-1] * n       # to reconstruct path
 
+        for i in range(n):
+            for j in range(i):
+                if len(words[i]) == len(words[j]) and groups[i] != groups[j] and checkhamming(words[i], words[j]):
+                    if dp[j] + 1 > dp[i]:
+                        dp[i] = dp[j] + 1
+                        prev_idx[i] = j
+
+        # Reconstruct longest path
+        max_len = max(dp)
+        idx = dp.index(max_len)
+
+        res = []
+        while idx != -1:
+            res.append(words[idx])
+            idx = prev_idx[idx]
+
+        return res[::-1]  # reverse to get correct order
 
         
