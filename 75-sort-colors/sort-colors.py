84b7d3927eb2d432c93c666b5baa9c2da41ebebb
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-05-17 13:03:57 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (33.54%) - LeetSync

---
+++ 75-sort-colors/sort-colors.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        nums.sort()
+        # red,white,blue=0,0,0
+        # for i in nums:
+        #     if i==0:
+        #         red+=1
+        #     elif i==1:
+        #         white+=1
+        #     else:
+        #         blue+=1
+        # rp=0
+        # wp=red
+        # bp=red+white
+
+        # print(rp,wp,bp)
+        # print(nums.sort())
+        
