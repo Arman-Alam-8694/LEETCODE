05e674ef03f45eb150d937ad02ed26cdec59c9a7
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-05-17 13:39:07 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (83.24%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -9,24 +9,39 @@ class Solution:
         # for i in range(n):
         #     for j in range(i + 1, n):
         #         if nums[j] < nums[i]:
+        #             print(nums[j],nums[i])
         #             nums[i], nums[j] = nums[j], nums[i]
         
-        insert0 = 0  # Where the next 0 should go
-        insert1 = 0  # Where the next 1 should go (after 0s)
+        # insert0 = 0  # Where the next 0 should go
+        # insert1 = 0  # Where the next 1 should go (after 0s)
 
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                # Shift right from insert0 to i
-                temp = nums[i]
-                for j in range(i, insert0, -1):
-                    nums[j] = nums[j - 1]
-                nums[insert0] = temp
-                insert0 += 1
-                insert1 += 1  # 1s shift forward due to inserted 0
-            elif nums[i] == 1:
-                # Shift right from insert1 to i
-                temp = nums[i]
-                for j in range(i, insert1, -1):
-                    nums[j] = nums[j - 1]
-                nums[insert1] = temp
-                insert1 += 1
+        # for i in range(len(nums)):
+        #     if nums[i] == 0:
+        #         # Shift right from insert0 to i
+        #         temp = nums[i]
+        #         for j in range(i, insert0, -1):
+        #             nums[j] = nums[j - 1]
+        #         nums[insert0] = temp
+        #         insert0 += 1
+        #         insert1 += 1  # 1s shift forward due to inserted 0
+        #     elif nums[i] == 1:
+        #         # Shift right from insert1 to i
+        #         temp = nums[i]
+        #         for j in range(i, insert1, -1):
+        #             nums[j] = nums[j - 1]
+        #         nums[insert1] = temp
+        #         insert1 += 1
+
+        n=len(nums)
+        low,mid=0,0
+        high=n-1
+        while mid<=high:
+            if nums[mid]==0:
+                nums[mid],nums[low]=nums[low],nums[mid]
+                low+=1
+                mid+=1
+            elif nums[mid]==1:
+                mid+=1
+            else:
+                nums[mid],nums[high]=nums[high],nums[mid]
+                high-=1
