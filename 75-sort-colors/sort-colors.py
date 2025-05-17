ef8c694702b92c6eb00bf961999e6c4dbfa236df
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-05-17 13:23:43 +0530

    Time: 3 ms (14.50%) | Memory: 17.9 MB (11.82%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -2,11 +2,31 @@ class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
-        """
+        # """
         
         
-        n = len(nums)
-        for i in range(n):
-            for j in range(i + 1, n):
-                if nums[j] < nums[i]:
-                    nums[i], nums[j] = nums[j], nums[i]
+        # n = len(nums)
+        # for i in range(n):
+        #     for j in range(i + 1, n):
+        #         if nums[j] < nums[i]:
+        #             nums[i], nums[j] = nums[j], nums[i]
+        
+        insert0 = 0  # Where the next 0 should go
+        insert1 = 0  # Where the next 1 should go (after 0s)
+
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                # Shift right from insert0 to i
+                temp = nums[i]
+                for j in range(i, insert0, -1):
+                    nums[j] = nums[j - 1]
+                nums[insert0] = temp
+                insert0 += 1
+                insert1 += 1  # 1s shift forward due to inserted 0
+            elif nums[i] == 1:
+                # Shift right from insert1 to i
+                temp = nums[i]
+                for j in range(i, insert1, -1):
+                    nums[j] = nums[j - 1]
+                nums[insert1] = temp
+                insert1 += 1
