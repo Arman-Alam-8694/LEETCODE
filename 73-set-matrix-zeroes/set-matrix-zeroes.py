f509b2eb58ac62d81874f00607e0517db953b6c7
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-05-21 10:59:23 +0530

    Time: 5 ms (31.83%) | Memory: 18.4 MB (53.65%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,20 +1,41 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        left=0
-        right=len(matrix[0])
-        up=0
-        down=len(matrix)
-        row=set()
-        col=set()
-        for r in range(down):
-            for c in range(right):
-                if matrix[r][c]==0:
-                    row.add(r)
-                    col.add(c)
-        for r in row:
-            for i in range(0,right):
-                matrix[r][i]=0
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        row=len(matrix)
+        col=len(matrix[0])
+        trow=False
+        tcol=False
+        # print(matrix[0][1])
+        for i in range(row):
+            for j in range(col):
+                if i==0 or j==0:
+                    if i==0 and matrix[i][j]==0:
+                        trow=True
+                    if j==0 and matrix[i][j]==0:
+                        tcol=True
+                if matrix[i][j]==0:
+                    matrix[i][0]=0
+                    matrix[0][j]=0
+        # print(trow,tcol)
+        # print(matrix)
+        for i in range(1,row):
+            if matrix[i][0]==0:
+                for k in range(col):
+                    matrix[i][k]=0
+        
+        for i in range(1,col):
+            if matrix[0][i]==0:
+                for k in range(row):
+                    matrix[k][i]=0
+
+        if trow:
+            for k in range(col):
+                 matrix[0][k]=0
+        if tcol:
+            for k in range(row):
+                    matrix[k][0]=0
+        return matrix
+
                 
-        for c in col:
-            for j in range(0,down):
-                matrix[j][c]=0
