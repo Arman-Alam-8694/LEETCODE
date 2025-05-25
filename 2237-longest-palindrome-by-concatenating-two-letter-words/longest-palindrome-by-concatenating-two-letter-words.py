5deffc02f6fcee94c110ed7a63e15527035c9604
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-05-25 17:09:20 +0530

    Time: 195 ms (15.16%) | Memory: 35.6 MB (40.25%) - LeetSync

--- 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.py
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.py
@@ -2,23 +2,26 @@ class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
         result=0
         mapp={}
+        sett=set()
         for i,w in enumerate(words):
             rev=w[::-1]
+            same=False
+            if rev==w:
+                same=True
             if rev in mapp:
                 result+=4
                 mapp[rev]-=1
                 if mapp[rev]==0:
+                    if same and rev in sett:
+                        sett.remove(rev)
                     del mapp[rev]
             else:
                 if w not in mapp:
                     mapp[w]=0
+                if same:
+                    sett.add(rev)
                 mapp[w]+=1
-
-        if mapp:
-            for k,v in mapp.items():
-                if k==k[::-1]:
-                    result+=2
-                    return result
-        return result
+        # print(sett)
+        return result if len(sett)==0 else result+2
     
         
