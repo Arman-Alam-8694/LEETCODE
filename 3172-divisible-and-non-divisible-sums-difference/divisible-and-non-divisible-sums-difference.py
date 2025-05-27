4d5b0cb3b7927b3264ac4967c4ab9f3c4658c259
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-05-27 18:34:36 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (67.05%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.py
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.py
@@ -1,10 +1,21 @@
 class Solution:
     def differenceOfSums(self, n: int, m: int) -> int:
-        div=0
-        notdiv=0
-        for i in range(1,n+1):
-            if i%m==0:
-                div+=i
-            else:
-                notdiv+=i
-        return notdiv-div
+        # div=0
+        # notdiv=0
+        # for i in range(1,n+1):
+        #     if i%m==0:
+        #         div+=i
+        #     else:
+        #         notdiv+=i
+        # return notdiv-div
+        
+        #BETTER SOLUTION O(1) TC
+
+        #no of multiples upto n of m
+        k=n//m
+        #sum of these 
+        # num2=(k*(k+1)//2)*m
+        #we know num1+num2=total
+        #so num1=total-num2 and we already have num2 and by substituing we get
+        #n*(n+1)//2-k*(k+1)*m
+        return n*(n+1)//2-k*(k+1)*m
