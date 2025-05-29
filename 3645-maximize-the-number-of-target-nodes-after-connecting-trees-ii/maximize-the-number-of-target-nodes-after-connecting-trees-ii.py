3a6425862f651e1750b060a6d7565ebd50a6c5a5
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-05-29 14:41:21 +0530

    Time: 590 ms (27.27%) | Memory: 106.5 MB (34.09%) - LeetSync

--- 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/maximize-the-number-of-target-nodes-after-connecting-trees-ii.py
+++ 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/maximize-the-number-of-target-nodes-after-connecting-trees-ii.py
@@ -3,48 +3,57 @@ from typing import List
 
 class Solution:
     def maxTargetNodes(self, edges1: List[List[int]], edges2: List[List[int]]) -> List[int]:
-        def build_tree(edges):
-            tree = defaultdict(list)
-            for u, v in edges:
-                tree[u].append(v)
-                tree[v].append(u)
-            return tree
+        treeone = defaultdict(list)
+        treetwo = defaultdict(list)
+        nodeclr = defaultdict(str)
 
-        def color_tree(tree, n):
-            color = {}
-            queue = deque([(0, 'red')])
-            red_count = 0
-            blue_count = 0
+        # Build both trees and track all nodes
+        for s, e in edges1:
+            treeone[s].append(e)
+            treeone[e].append(s)
+        for s, e in edges2:
+            treetwo[s].append(e)
+            treetwo[e].append(s)
+
+        def colour(start, tree, save_color=False):
+            queue = deque([(start, "red")])
+            seen = set()
+            redcnt = 0
+            bluecnt = 0
             while queue:
-                node, c = queue.popleft()
-                if node in color:
+                node, prevcolor = queue.popleft()
+                if node in seen:
                     continue
-                color[node] = c
-                if c == 'red':
-                    red_count += 1
+                seen.add(node)
+
+                # Count colors
+                if prevcolor == "red":
+                    redcnt += 1
                 else:
-                    blue_count += 1
-                for nei in tree[node]:
-                    if nei not in color:
-                        queue.append((nei, 'blue' if c == 'red' else 'red'))
-            return color, red_count, blue_count
+                    bluecnt += 1
+                # Save color mapping if needed
+                if save_color:
+                    nodeclr[node] = prevcolor
 
-        n = len(edges1) + 1
-        m = len(edges2) + 1
+                # Enqueue unvisited neighbors
+                for child in tree[node]:
+                    if child not in seen:
+                        queue.append((child, "blue" if prevcolor == "red" else "red"))
+            return redcnt, bluecnt
 
-        tree1 = build_tree(edges1)
-        tree2 = build_tree(edges2)
+        # Color treeone and record each node's color
+        r, b = colour(0, treeone, save_color=True)
+        fast = {"red": r, "blue": b}
 
-        # Color both trees
-        color1, red1, blue1 = color_tree(tree1, n)
-        color2, red2, blue2 = color_tree(tree2, m)
+        # Color treetwo just to get its max color group
+        sred, sblue = colour(0, treetwo)
+        smax = max(sred, sblue)
 
-        # For each node in tree1, calculate maximum target nodes
+        # Number of nodes in treeone is len(edges1) + 1
+        n = max(treeone.keys())+1
         result = []
+        # Build result for each node 0..n-1
         for i in range(n):
-            my_color = color1[i]
-            same_color_count = red1 if my_color == 'red' else blue1
-            max_other = max(red2, blue2)
-            result.append(same_color_count + max_other)
+            result.append(fast[nodeclr[i]] + smax)
 
         return result
