6b8c5cff69679032300f077529d9dafb25e15d8b
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-05-31 15:50:33 +0530

    Time: 27 ms (40.75%) | Memory: 17.8 MB (82.91%) - LeetSync

--- 945-snakes-and-ladders/snakes-and-ladders.py
+++ 945-snakes-and-ladders/snakes-and-ladders.py
@@ -1,5 +1,8 @@
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
+        #BACKTRACKING WITH PRUINING O(N^2) AT WORST BUT
+        #WITH LOT OF OVERHEAD 
+
         n = len(board)
         target = n * n
 
@@ -11,29 +14,47 @@ class Solution:
                 col=n-1-col
             return row,col
 
-        result=[float("inf")]
-        # @cache
-        seen=defaultdict(int)
-        def simulate(num,steps):
-            if num in seen and steps>=seen[num]:
-                return
-            seen[num]=steps
+        #BACKTRACK WITH PRUNING PART-
+        # seen=defaultdict(int)
+        # def simulate(num,steps):
+        #     if num in seen and steps>=seen[num]:
+        #         return
+        #     seen[num]=steps
+        #     if num==target:
+        #         return 
+        #     start=num+1
+        #     end=min(num+6,target)
+        #     for i in range(start,end+1):
+        #         tr,tc=numtocord(i)
+        #         if board[tr][tc]==-1:
+        #             simulate(i,steps+1)
+        #         else:
+        #             simulate(board[tr][tc],steps+1)
+        #     return 
+
+        # simulate(1,0)
+        # return seen.get(target,-1)
+
+
+        #BFS OPTIMISED SOL O(N^2)
+
+        queue=deque([(1,0)])
+        seen=set()
+        seen.add(1)
+        while queue:
+            num,step=queue.popleft()
             if num==target:
-                return 
-                
+                return step
             start=num+1
             end=min(num+6,target)
             for i in range(start,end+1):
+            
                 tr,tc=numtocord(i)
-                if board[tr][tc]==-1:
-                    simulate(i,steps+1)
-                else:
-                    simulate(board[tr][tc],steps+1)
-            return 
-
-        steps=simulate(1,0)
-        return seen.get(target,-1)
-
-
-
-        
+                if board[tr][tc]==-1 and i not in seen:
+                    queue.append((i,step+1))
+                    seen.add(i)
+                elif board[tr][tc]!=-1 and board[tr][tc] not in seen:
+                    queue.append((board[tr][tc],step+1))
+                    seen.add(board[tr][tc])
+            
+        return -1
