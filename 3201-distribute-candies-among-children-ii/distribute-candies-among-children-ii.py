84ea54149df9734ab63cf36fa4f10b0412303ec9
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-06-01 12:18:40 +0530

    Time: 4461 ms (10.96%) | Memory: 17.7 MB (48.63%) - LeetSync

--- 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.py
+++ 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.py
@@ -1,14 +1,23 @@
-def cal(x):
-    if x < 0:
-        return 0
-    return x * (x - 1) // 2
-
-
 class Solution:
     def distributeCandies(self, n: int, limit: int) -> int:
-        return (
-            cal(n + 2)
-            - 3 * cal(n - limit + 1)
-            + 3 * cal(n - (limit + 1) * 2 + 2)
-            - cal(n - 3 * (limit + 1) + 2)
-        )
+        result=0
+        for i in range(min(limit,n)+1):
+            rem_sum=n-i
+            if limit*2>=rem_sum:
+                # sec=min(limit,rem_sum)
+                # last=min(limit,rem_sum-sec)
+                # if sec==last:
+                #     result+=1
+                # if sec!=last:
+                #     result+=2
+                # x,y=divmod((sec-last),2)
+                # if x!=0:
+                #     result+=x*2
+                #     if y==0:
+                #         result-=1
+                max_b=min(limit,rem_sum)
+                min_b=max(0,rem_sum-limit)
+                result+=max_b-min_b+1
+        return result
+
+        
