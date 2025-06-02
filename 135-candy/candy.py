ae4aca91753eb5d4096c85743ec02901eaded3c3
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-06-02 22:00:54 +0530

    Time: 54 ms (5.03%) | Memory: 19.9 MB (36.16%) - LeetSync

---
+++ 135-candy/candy.py
@@ -0,0 +1,45 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        result=0
+        start=None
+        prev=1
+        handle=0
+        thandle=0
+        for end in range(len(ratings)):
+           
+            if start is None:
+                start=end
+                result+=1
+                prev=1
+            elif ratings[end-1]==ratings[end]:
+                result+=1
+                start=end
+                prev=1
+                handle=None
+            elif ratings[end-1]>ratings[end]:
+                if prev>1:
+                    result+=1
+                    handle=prev
+                    thandle=end-1
+                    prev=1
+                    start=end
+                else:
+                    calc=(end-start)+1
+                    if handle and calc>=handle:
+                        start=thandle
+                        result+=(end-start)+1
+                        handle=None
+                    else:
+                        result+=calc
+                    prev=1
+
+            elif ratings[end-1]<ratings[end]:
+                prev=prev+1
+                result+=prev
+                start=end
+                hanlde=None
+            print(f"current={end},start={start},result={result},rpev={prev}")
+        return result
+
+
+        
