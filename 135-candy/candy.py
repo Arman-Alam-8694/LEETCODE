1a775d3adab65d2c7e95a22c976e6274dcd12af9
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-06-02 22:06:47 +0530

    Time: 7 ms (98.11%) | Memory: 19.9 MB (68.66%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -1,12 +1,24 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
+        #single pass O(n) solutin with constant space the problem with this question is 
+        #i did miss some of edge cases like if there is increasing slope and then a decreasing
+        #slope then the accumulated value of the increaseing slope we to take in account
+        #because the next if decreasing slope  by greedy i can make it 1 but if n the future 
+        #we get bigger decresing slope then i might need to change the accumulated value aswell 
+        #because we are just maintain the start of the decreasing slope to track down the number
+        # of elemeents you need add 1 to each but if the accumulated slope value is bigger 
+        # than the number of elements in the decreasing slope then it's fine other wise we have to 
+        #even change the accumulated value as well and also we are maintaing the prev value 
+        #at each iteration and making it change value when we encounter the same rating becuase
+        #[IMPORTANT]---if there is same rating then it doesn't matter if the next is smaller or bigger so we 
+        #reset start here and also when we are about to start the increasing slope
+
         result=0
         start=None
         prev=1
         handle=0
         thandle=0
         for end in range(len(ratings)):
-           
             if start is None:
                 start=end
                 result+=1
