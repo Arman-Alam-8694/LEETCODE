ec9cb7aa792241395c209104eb91da3d729b2bff
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-06-03 10:21:48 +0530

    Time: 26 ms (41.76%) | Memory: 28.2 MB (65.13%) - LeetSync

---
+++ 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maxCandies(self, status: List[int], candies: List[int], keys: List[List[int]], containedBoxes: List[List[int]], initialBoxes: List[int]) -> int:
+        result=0
+        queue=deque([])
+        seen=set()
+        for i in initialBoxes:
+            queue.append(i)
+            seen.add(i)
+        keyy=[0]*1000
+        used=[0]*1000
+        while queue:
+            boxx=queue.popleft()
+            if used[boxx]:
+                continue
+
+            if status[boxx] or keyy[boxx]:
+                result+=candies[boxx]
+                used[boxx]=1
+
+                for x in containedBoxes[boxx]:
+                    if x not in seen:
+                        queue.append(x)
+                        seen.add(x)
+                    elif keyy[x] and used[y]==0:
+                        queue.append(x)
+
+                for y in keys[boxx]:
+                    keyy[y]=1
+                    if y in seen and used[y]==0:
+                        queue.append(y)
+                        
+        return result
