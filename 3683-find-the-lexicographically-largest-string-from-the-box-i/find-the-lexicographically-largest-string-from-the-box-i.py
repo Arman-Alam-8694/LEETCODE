fcb83b2fdb6f01403e58874859f57bdfe0d982f0
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-06-04 12:16:53 +0530

    Time: 2583 ms (9.85%) | Memory: 18.2 MB (18.94%) - LeetSync

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.py
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.py
@@ -1,49 +1,55 @@
 class Solution:
     def answerString(self, word: str, numFriends: int) -> str:
-        # if numFriends==1:
-        #     return word
-        # max_string=[]
-        # def get_rank(a):
-        #     return ord(a)-ord("a")
-        # n=len(word)
-        # max_contain=n-((numFriends)-1)
+        if numFriends==1:
+            return word
+        max_string=[]
+        def get_rank(a):
+            return ord(a)-ord("a")
+        n=len(word)
+        max_contain=n-((numFriends)-1)
+        max_char=max(word)
+        max_indices=[]
+        for i in range(n):
+            if word[i]==max_char:
+                max_indices.append(i)
 
-        # for i in range(n):
-        #     temp=0
-        #     tlist=[]
-        #     smaller=False
-        #     while temp<len(max_string):
-        #         # print("here")
-        #         # print(i+temp)
-        #         if i+temp>=n:
-        #             smaller=True
-        #             break
-        #         rank1=get_rank(word[i+temp])
-        #         rank2=get_rank(max_string[temp])
-        #         if rank1==rank2:
-        #             tlist.append(word[i+temp])
-        #             temp+=1
-        #         elif rank1<rank2:
-        #             smaller=True
-        #             break
-        #         else:
-        #             break
+
+        for i in max_indices:
+            temp=0
+            tlist=[]
+            smaller=False
+            while temp<len(max_string):
+                # print("here")
+                # print(i+temp)
+                if i+temp>=n:
+                    smaller=True
+                    break
+                rank1=get_rank(word[i+temp])
+                rank2=get_rank(max_string[temp])
+                if rank1==rank2:
+                    tlist.append(word[i+temp])
+                    temp+=1
+                elif rank1<rank2:
+                    smaller=True
+                    break
+                else:
+                    break
    
 
-        #     if not smaller:
-        #         while i+temp<n and temp<max_contain:
-        #             tlist.append(word[i+temp])
-        #             temp+=1
-        #     # print(tlist)
-        #     if not smaller:
-        #         max_string=tlist.copy()
+            if not smaller:
+                while i+temp<n and temp<max_contain:
+                    tlist.append(word[i+temp])
+                    temp+=1
+            # print(tlist)
+            if not smaller:
+                max_string=tlist.copy()
 
-        # return "".join(max_string)
+        return "".join(max_string)
 
         #SAME TC BUT FASTER BECAUSE OF INBUILT FUNCTION IN C LANGUAGE
-        if numFriends == 1:
-            return word
-        n = len(word) - numFriends + 1
-        return max( word[i:i+n] for i in range(len(word)) )
+        # if numFriends == 1:
+        #     return word
+        # n = len(word) - numFriends + 1
+        # return max( word[i:i+n] for i in range(len(word)) )
 
         
