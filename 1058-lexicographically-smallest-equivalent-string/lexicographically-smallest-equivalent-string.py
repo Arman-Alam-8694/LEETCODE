d49f47f288e33da95c74ccf475dc3df85b9a324d
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-06-05 12:54:56 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (87.59%) - LeetSync

--- 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.py
+++ 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.py
@@ -1,85 +1,39 @@
+from collections import defaultdict
+
 class Solution:
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
-        # equivalency=defaultdict(str)
-        # listt=[]
-        # for i,j in zip(s1,s2):
-        #     listt.append((i,j))
-        # listt.sort()
-        # for i,j in listt:
-        #     if i>j:
-        #         prev=equivalency.get(j,"z")
-        #         equivalency[j]=min(prev,j)
-        #         equivalency[i]=equivalency[j]
-              
-        #     else:
-        #         prev=equivalency.get(i,"z")
-        #         equivalency[i]=min(prev,i)
-        #         equivalency[j]=equivalency[i]
-      
-        # result=[]
-        # for i in baseStr:
-        #     result.append(equivalency[i])
-        # return "".join(result)
-        listt=[]
-        chartoslot=defaultdict(int)
-        cur_slot=0
-        chains=defaultdict(set)
-        for i,j in zip(s1,s2):
-            # print(i,j)
-            temp1="z"
-            temp2="z"
-  
-            slot1=None
-            slot2=None
-            if i in chartoslot:
-                slot1=chartoslot[i]
-                temp1=listt[slot1]
-    
-            if j in chartoslot:
-                slot2=chartoslot[j]
-                temp2=listt[slot2]
+        char_to_slot = {}
+        slot_min_char = []
+        cur_slot = 0
 
-            if slot1!=None and slot2!=None:
-                listt[slot1]=min(temp1,temp2,i,j)
-                listt[slot2]=min(temp1,temp2,i,j)
-                chartoslot[i]=slot1
-                chartoslot[j]=slot2
-                chains[slot1].add(slot2)
-                chains[slot2].add(slot1)
-            elif slot1!=None and slot2==None:
-                listt[slot1]=min(temp1,i,j)
-                chartoslot[i]=slot1
-                chartoslot[j]=slot1 
-            elif slot2!=None and slot1==None:
-                listt[slot2]=min(temp2,i,j)
-                chartoslot[i]=slot2
-                chartoslot[j]=slot2
+        for a, b in zip(s1, s2):
+            slot_a = char_to_slot.get(a)
+            slot_b = char_to_slot.get(b)
+
+            if slot_a is not None and slot_b is not None:
+                if slot_a != slot_b:
+                    # Merge slot_b into slot_a (lower index preferred)
+                    if slot_min_char[slot_b] < slot_min_char[slot_a]:
+                        slot_a, slot_b = slot_b, slot_a
+                    for ch in char_to_slot:
+                        if char_to_slot[ch] == slot_b:
+                            char_to_slot[ch] = slot_a
+                    slot_min_char[slot_a] = min(slot_min_char[slot_a], slot_min_char[slot_b])
+            elif slot_a is not None:
+                char_to_slot[b] = slot_a
+                slot_min_char[slot_a] = min(slot_min_char[slot_a], a, b)
+            elif slot_b is not None:
+                char_to_slot[a] = slot_b
+                slot_min_char[slot_b] = min(slot_min_char[slot_b], a, b)
             else:
-                listt.append("")
-                listt[cur_slot]=min(i,j)
-                chartoslot[i]=cur_slot
-                chartoslot[j]=cur_slot
-                cur_slot+=1
+                char_to_slot[a] = char_to_slot[b] = cur_slot
+                slot_min_char.append(min(a, b))
+                cur_slot += 1
 
-        print(listt)
-        print(chains)
-        print(chartoslot)
-        visited=set()
-        for i,k in chains.items():
-            if i not in visited:
-                for j in k:
-                    listt[i]=min(listt[i],listt[j])
-                    listt[j]=min(listt[i],listt[j])
-
-        result=[]
-        for i in baseStr:
-            if i not in chartoslot:
-                result.append(i)
+        result = []
+        for ch in baseStr:
+            if ch in char_to_slot:
+                result.append(slot_min_char[char_to_slot[ch]])
             else:
-                result.append(listt[chartoslot[i]])
+                result.append(ch)
         return "".join(result)
-            
-
-
-
-        
