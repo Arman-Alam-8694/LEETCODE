3d62ac86887e348ff63405ab86dd347d12dc3834
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-06-05 13:36:29 +0530

    Time: 1 ms (96.84%) | Memory: 18 MB (21.17%) - LeetSync

--- 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.py
+++ 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.py
@@ -2,6 +2,12 @@ from collections import defaultdict
 
 class Solution:
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
+        #KIND OF BRUTE FORCE SOLUTION NOT GOOD FOR LOT OF UNIQUE DOMAINS
+        #BUT AS THE DOMAINS HERE IS LIMITED TO LOWERCASE A-Z IT'S ONLY O(26) PER MERGE
+        #SO THIS WORKS FINE WE COULD HAVE ALSO USED DFS/BFS SIMILAR TO THIS 
+        #BUT FOR THIS PROBLEM I WAS GETTING COOL 100% SO I WILL KEEP MY BRUTE FORCE 
+        #KIND OF SOLUTION
+        
         char_to_slot = {}
         slot_min_char = []
         cur_slot = 0
@@ -37,3 +43,37 @@ class Solution:
             else:
                 result.append(ch)
         return "".join(result)
+        
+        #BUT USING UNION-FIND IS MOST IMPORTANT FOR LARGER DOMAINS SO WE DO THIS 
+
+        # parent={}
+        #initialize each element parent to itself
+        # for i in set(baseStr+s1+s2):
+        #     parent[i]=i
+        #suppose we have a and it's parent is not a then we have find the parent of 
+        #it's parent until the parent are same that means here by lexicographically smallest
+        #one will the parent to itself and also forming the chains and we use recursion 
+        #to change the parents of all the bigger one to this smaller one using find recursion
+        # def find(x):
+        #     if parent[x]!=x:
+        #         parent[x]=find(parent[x])
+        #     return parent[x]
+
+        #making union function to do this job of mergin two disjoint sets into one by 
+        #priotizing the smaller root of both the set and assiginng it to the bigger one
+        # def union(a,b):
+        #     rootA=find(a)
+        #     rootB=find(b)
+        #     if rootA!=rootB:
+        #         if rootB<rootA:
+        #             parent[rootA]=rootB
+        #         else:
+        #             parent[rootB]=rootA
+
+        # for i,j in zip(s1,s2):
+        #     union(i,j)
+
+        # result=[]
+        # for i in baseStr:
+        #     result.append(find(i))
+        # return "".join(result)
