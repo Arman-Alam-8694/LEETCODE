57b16c27b88f5a1939b081013a1025f1724ea334
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-06-06 11:24:51 +0530

    Time: 984 ms (22.13%) | Memory: 20.8 MB (59.16%) - LeetSync

---
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.py
@@ -0,0 +1,34 @@
+class Solution:
+    def robotWithString(self, s: str) -> str:
+        position=defaultdict(list)
+        n=len(s)
+        last=-1
+        for i in "abcdefghijklmnopqrstuvwxyz":
+            for j in range(n):
+                if last<j and s[j]==i:
+                    position[i].append(j)
+                    last=j
+        # print(position)
+        result=[]
+        stack=[]
+        start=0
+        for k in "abcdefghijklmnopqrstuvwxyz":
+            elem=k
+            pos=position[elem]
+            while stack and stack[-1]<=elem:
+                t=stack.pop()
+                result.append(t)
+            stack_pos=0
+            if pos:
+                for i in range(start,pos[-1]+1):
+                    if pos[stack_pos]==i:
+                        result.append(elem)
+                        stack_pos+=1
+                    else:
+                        stack.append(s[i])
+                start=i+1
+
+        stack.reverse()
+        final=result+stack
+        return "".join(final)
+
