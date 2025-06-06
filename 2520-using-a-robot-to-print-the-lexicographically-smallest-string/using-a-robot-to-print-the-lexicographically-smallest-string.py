a955105b841d4b586fa41fa57b5409a4f33b0d59
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-06-06 11:40:01 +0530

    Time: 274 ms (90.46%) | Memory: 20.7 MB (67.94%) - LeetSync

--- 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.py
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.py
@@ -1,32 +1,28 @@
 class Solution:
     def robotWithString(self, s: str) -> str:
-        position=defaultdict(list)
+        position=defaultdict(int)
+        
+
         n=len(s)
+        for i in range(n):
+            position[s[i]]=i
         last=-1
-        for i in "abcdefghijklmnopqrstuvwxyz":
-            for j in range(n):
-                if last<j and s[j]==i:
-                    position[i].append(j)
-                    last=j
-        # print(position)
         result=[]
         stack=[]
         start=0
-        for k in "abcdefghijklmnopqrstuvwxyz":
-            elem=k
-            pos=position[elem]
+        for elem in "abcdefghijklmnopqrstuvwxyz":
             while stack and stack[-1]<=elem:
                 t=stack.pop()
                 result.append(t)
-            stack_pos=0
-            if pos:
-                for i in range(start,pos[-1]+1):
-                    if pos[stack_pos]==i:
-                        result.append(elem)
-                        stack_pos+=1
-                    else:
-                        stack.append(s[i])
-                start=i+1
+            last=position[elem]
+            for i in range(start,last+1):
+                if s[i]==elem:
+                    result.append(elem)
+                else:
+                    stack.append(s[i])
+            start=i+1
+            # print(stack,start)
+
 
     
         return "".join(result)
