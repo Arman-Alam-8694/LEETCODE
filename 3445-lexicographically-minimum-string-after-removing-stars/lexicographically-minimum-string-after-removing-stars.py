8c2f3c2ae4287d3ec78002c3bc1876b541d759c5
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-06-07 13:08:07 +0530

    Time: 495 ms (78.42%) | Memory: 23.6 MB (60.43%) - LeetSync

--- 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.py
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.py
@@ -1,33 +1,13 @@
 class Solution:
     def clearStars(self, s: str) -> str:
-        # abcdefghijklmnopqrstuvwxyz
-        char_idx=[[] for _ in range(27)]
-        # print(char_idx)
-        rem=ord("a")
-        n=len(s)
-        smallest=28
-        skip=set()
-        for i in range(n):
-            if s[i]=="*":
-                # print(char_idx)
-                # print(f"curr {i} smallest{smallest} char_idx-{char_idx[smallest]}")
-                skip.add(i)
-                for j in range(26):
-                    if len(char_idx[j])>=1:
-                        smallest=j
-                        break
-                t=char_idx[smallest].pop()
-                skip.add(t)
-                    
+        cnt = [[] for _ in range(26)]
+        arr = list(s)
+        for i, c in enumerate(arr):
+            if c != "*":
+                cnt[ord(c) - ord("a")].append(i)
             else:
-                new=ord(s[i])-ord("a")
-                if new<=smallest:
-                    smallest=new
-                char_idx[new].append(i)
-        # print(skip)
-        result=[]
-        for i in range(n):
-            if i not in skip:
-                result.append(s[i])
-        return "".join(result)
-        
+                for j in range(26):
+                    if cnt[j]:
+                        arr[cnt[j].pop()] = "*"
+                        break
+        return "".join(c for c in arr if c != "*")
