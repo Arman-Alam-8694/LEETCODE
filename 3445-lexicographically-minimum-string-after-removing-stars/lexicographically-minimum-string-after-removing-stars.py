40b0d8ed6c9ecfe6769a48437e29b8ca4b3d5c4d
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-06-07 18:29:38 +0530

    Time: 556 ms (63.31%) | Memory: 28.8 MB (40.29%) - LeetSync

--- 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.py
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.py
@@ -1,51 +1,54 @@
-#BETTER SOLUTION WITH o(N) 
+#BETTER SOLUTION WITH o(N*26) SO O(N) 
 
 
-# class Solution:
-#     def clearStars(self, s: str) -> str:
-#         char_idx=[[] for _ in range(27)]
-#         n=len(s)
-#         smallest=28
-#         skip=set()
-#         for i in range(n):
-#             if s[i]=="*":
-#                 skip.add(i)
-   
-#                 t=char_idx[smallest].pop()
-#                 skip.add(t)
-           
-#                 if len(char_idx[smallest])==0:
-#                     temp=smallest
-#                     smallest=26
-#                     for j in range(temp,26):
-#                         if len(char_idx[j])>=1:
-#                             smallest=j
-#                             break
-#             else:
-#                 new=ord(s[i])-ord("a")
-#                 if new<=smallest:
-#                     smallest=new
-#                 char_idx[new].append(i)
-       
-#         result=[]
-#         for i in range(n):
-#             if i not in skip:
-#                 result.append(s[i])
-#         return "".join(result)
-        
 class Solution:
     def clearStars(self, s: str) -> str:
-        
-        ans = list(s)
-        heap = []
-        
-
-        for i, char in enumerate(s):
-            if char == '*' and heap:
-                del_char, j = heapq.heappop(heap)
-                ans[-j] = ''
-                ans[i] = ''
+        char_idx=[[] for _ in range(27)]
+        n=len(s)
+        smallest=28
+        skip=set()
+        for i in range(n):
+            if s[i]=="*":
+                skip.add(i)
+   
+                t=char_idx[smallest].pop()
+                skip.add(t)
+           
+                if len(char_idx[smallest])==0:
+                    temp=smallest
+                    smallest=26
+                    for j in range(temp,26):
+                        if len(char_idx[j])>=1:
+                            smallest=j
+                            break
             else:
-                heapq.heappush(heap, (char, -i))
+                new=ord(s[i])-ord("a")
+                if new<=smallest:
+                    smallest=new
+                char_idx[new].append(i)
+       
+        result=[]
+        for i in range(n):
+            if i not in skip:
+                result.append(s[i])
+        return "".join(result)
 
-        return ''.join(ans)
+
+#LITTLE BIT WORSE WITH O(NLOGN)
+        
+# class Solution:
+#     def clearStars(self, s: str) -> str:
+        
+#         ans = list(s)
+#         heap = []
+        
+
+#         for i, char in enumerate(s):
+#             if char == '*' and heap:
+#                 del_char, j = heapq.heappop(heap)
+#                 ans[-j] = ''
+#                 ans[i] = ''
+#             else:
+#                 heapq.heappush(heap, (char, -i))
+
+#         return ''.join(ans)
