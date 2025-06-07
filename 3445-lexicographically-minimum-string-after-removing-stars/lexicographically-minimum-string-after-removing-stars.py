4c090e3882da430a6b5642b99666053d57c9d626
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-06-07 18:12:47 +0530

    Time: 541 ms (67.63%) | Memory: 28.8 MB (38.85%) - LeetSync

--- 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.py
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.py
@@ -1,19 +1,16 @@
 class Solution:
     def clearStars(self, s: str) -> str:
-        # abcdefghijklmnopqrstuvwxyz
         char_idx=[[] for _ in range(27)]
-        # print(char_idx)
-        # rem=ord("a")
         n=len(s)
         smallest=28
         skip=set()
         for i in range(n):
             if s[i]=="*":
                 skip.add(i)
-                # print(f"before smallest{smallest},char_idx {char_idx[smallest]}")
+   
                 t=char_idx[smallest].pop()
                 skip.add(t)
-                # print(f"after pop smallest{smallest},char_idx {char_idx[smallest]}")
+           
                 if len(char_idx[smallest])==0:
                     temp=smallest
                     smallest=26
@@ -21,13 +18,12 @@ class Solution:
                         if len(char_idx[j])>=1:
                             smallest=j
                             break
-                # print(f"change smallest{smallest},char_idx {char_idx[smallest]}")
             else:
                 new=ord(s[i])-ord("a")
                 if new<=smallest:
                     smallest=new
                 char_idx[new].append(i)
-        # print(skip)
+       
         result=[]
         for i in range(n):
             if i not in skip:
