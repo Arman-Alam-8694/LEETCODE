ec86985ce24292a04ffd00b4186a0ef42cece40e
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-06-08 11:23:20 +0530

    Time: 5 ms (97.84%) | Memory: 21.1 MB (92.81%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.py
+++ 386-lexicographical-numbers/lexicographical-numbers.py
@@ -1,5 +1,21 @@
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
+        
+        #recusive but takes extra space not ideal
+
+        # result=[]
+        # def dfs(num):
+        #     if num>n:
+        #         return 
+        #     result.append(num)
+        #     for i in range(10):
+        #         temp=num*10
+        #         temp+=i
+        #         dfs(temp)
+
+        # for k in range(1,10):
+        #     dfs(k)
+        # return result
         result=[]
         start=1
         for _ in range(n):
