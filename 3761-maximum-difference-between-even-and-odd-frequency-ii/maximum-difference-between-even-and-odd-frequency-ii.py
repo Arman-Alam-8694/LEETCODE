57112ef85a4052ca58555dff47ea2520e5b68ece
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-06-12 20:07:56 +0530

    Time: 3076 ms (49.29%) | Memory: 17.8 MB (100.00%) - LeetSync

--- 3761-maximum-difference-between-even-and-odd-frequency-ii/maximum-difference-between-even-and-odd-frequency-ii.py
+++ 3761-maximum-difference-between-even-and-odd-frequency-ii/maximum-difference-between-even-and-odd-frequency-ii.py
@@ -1,35 +1,53 @@
 class Solution:
     def maxDifference(self, s: str, k: int) -> int:
-        def getStatus(cnt_a: int, cnt_b: int) -> int:
-            return ((cnt_a & 1) << 1) | (cnt_b & 1)
+        #didn't get in the first attempt good question to code again 
+        #the hint was in the constraint that's why i think this is little 
+        #bit of different problem compared to others, once i did notice that 
+        #but thought of doing it very optimal way which was messier that time
+        #so that's couldn't think of just going by making couples of different
+        #numbers and try it linearly 
 
-        n = len(s)
-        ans = float("-inf")
-        for a in ["0", "1", "2", "3", "4"]:
-            for b in ["0", "1", "2", "3", "4"]:
-                if a == b:
+        #for this question the time complexity will O(2On)
+        #there total total 5*4 unique pairs so yeh 20 times linear
+        #which is doable but not easy to go that path you know
+
+        #let's go and i got the idea of prefix sum and all as well 
+        #in the first time but still couldn't solve this question 
+        
+        #BECAUSE I WAS NOT GOING STEP BY STEP , I WAS TRYING TO HANDLE
+        #LOT OF CASES IN JUST ONE PASS SO THAT'S WHAT MADE ME LOSE SO MUCH 
+        # AND HENCE THE INTEREST AS WELL    
+
+        n=len(s)
+        ans=float("-inf")
+        pairs=["0","1","2","3","4"]
+
+
+        def get_status(ca,cb):
+            return (ca&1)<<1|(cb&1)
+
+        for a in pairs:
+            for b in pairs:
+                if a==b:
                     continue
-
-                best = [float("inf")] * 4
-                cnt_a = cnt_b = 0
-                prev_a = prev_b = 0
-                left = -1
+                #there are four states in terms of odd and even 
+                #00 01 10 11
+                best=[float("inf")]*4
+                left=-1
+                cnt_a,cnt_b=0,0
+                prev_a,prev_b=0,0
                 for right in range(n):
-                    cnt_a += s[right] == a
-                    cnt_b += s[right] == b
-                    while right - left >= k and cnt_b - prev_b >= 2:
-                        left_status = getStatus(prev_a, prev_b)
-                        best[left_status] = min(
-                            best[left_status], prev_a - prev_b
-                        )
-                        left += 1
-                        prev_a += s[left] == a
-                        prev_b += s[left] == b
-
-                    right_status = getStatus(cnt_a, cnt_b)
-                    if best[right_status ^ 0b10] != float("inf"):
-                        ans = max(
-                            ans, cnt_a - cnt_b - best[right_status ^ 0b10]
-                        )
+                    cnt_a+=s[right]==a
+                    cnt_b+=s[right]==b
+                    while right-left>=k and cnt_b-prev_b>=2:
+                        ll=get_status(prev_a,prev_b)
+                        best[ll]=min(best[ll],prev_a-prev_b)
+                        left+=1
+                        prev_a+=s[left]==a
+                        prev_b+=s[left]==b
+                    rr=get_status(cnt_a,cnt_b)
+                    if best[rr^0b10]!=float("inf"):
+                        ans=max(ans,cnt_a-cnt_b-best[rr^0b10])
 
         return ans
+
