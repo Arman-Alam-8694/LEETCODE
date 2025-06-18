59af43db8c6b0bcb31423b7505a7cb65076d30d9
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-06-18 20:31:09 +0530

    Time: 194 ms (5.06%) | Memory: 34.9 MB (5.06%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
@@ -1,6 +1,16 @@
 class Solution:
-    def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
-        nums.sort()
+    def divideArray(self, num: List[int], k: int) -> List[List[int]]:
+        # nums.sort()
+        #using sort gives nlogn we will use counting sort and rebuild the sorted array
+        store=[0]*(max(num)+1)
+        for i in num:
+            store[i]+=1
+
+        nums=[]
+        for i in range(len(store)):
+            nums.extend([i]*store[i])
+        
+
         result=[[]]
         temp=[]
         for i in nums:
