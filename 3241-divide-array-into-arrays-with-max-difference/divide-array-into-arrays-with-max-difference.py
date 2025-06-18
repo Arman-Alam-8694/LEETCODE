996c0a0aaf5d3cf281e3896401da6ca19b66241f
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-06-18 20:33:50 +0530

    Time: 137 ms (8.02%) | Memory: 33.3 MB (69.20%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
@@ -1,15 +1,22 @@
 class Solution:
-    def divideArray(self, num: List[int], k: int) -> List[List[int]]:
-        # nums.sort()
+    def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
+        nums.sort()
         #using sort gives nlogn we will use counting sort and rebuild the sorted array
-        store=[0]*(max(num)+1)
-        for i in num:
-            store[i]+=1
+        # store=[0]*(max(num)+1)
+        # for i in num:
+        #     store[i]+=1
 
-        nums=[]
-        for i in range(len(store)):
-            nums.extend([i]*store[i])
-        #i tried
+        # nums=[]
+        # for i in range(len(store)):
+            # nums.extend([i]*store[i])
+        #i tried using the counting sort but i think the test cases are not good enought
+        #for this approach because if the array consist of number like 
+        #1 2 3 398393893
+        #so the max of this array is very huge so if we use the couting sort then 
+        #we have to first make the array of that size and then cound and rebuild 
+        #so at the tnd nlogn for this type of test cases are much better
+        #so i think that's why the overall runtime of this solution using the count sort 
+        #is comparatively slower than the nlogn simple sort solution
         
 
         result=[[]]
