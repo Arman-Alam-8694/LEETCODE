c4c6813d338b115aa7b3d1ae89a346dd345255c1
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-06-21 17:34:33 +0530

    Time: 55 ms (80.62%) | Memory: 18.2 MB (20.15%) - LeetSync

---
+++ 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minimumDeletions(self, word: str, k: int) -> int:
+        freq=Counter(word)
+        # prefix=[]
+        listt=[i if i!=0 else None for i in freq.values()]
+        listt.sort()
+        n=len(listt)
+        prefix=[0]*(n+1)
+        for i in range(n):
+            prefix[i+1]=prefix[i]+listt[i]
+        print(listt)
+        print(prefix)
+        answer=float("inf")
+        right=0
+        for left in range(n):
+            while right<n and listt[right]-listt[left]<=k:
+                right+=1
+            left_discard=prefix[left]
+            right_part=prefix[n]-prefix[right]
+            adjust=right_part-(listt[left]+k)*(n-right)
+            answer=min(answer,left_discard+adjust)
+
+
+        return answer
+        
