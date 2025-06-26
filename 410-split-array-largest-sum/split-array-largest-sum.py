6c836e87b8b7aceba57a7f7fe06290cef4c6db5a
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-06-26 20:51:13 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (97.70%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.py
@@ -0,0 +1,30 @@
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        left=min(nums)
+        right=sum(nums)
+        def possible(limit):
+            cur_sum=0
+            partition=0
+            for i in nums:
+                cur_sum+=i
+                if cur_sum>limit:
+                    partition+=1
+                    cur_sum=i
+                    if cur_sum>limit:
+                        return False
+           
+            print(limit,partition)
+            return partition<=k-1
+
+
+        answer=left
+        while left<=right:
+            mid=(left+right)//2
+            if possible(mid):
+                right=mid-1
+                answer=mid
+            else:
+                left=mid+1
+
+        return answer
+        
