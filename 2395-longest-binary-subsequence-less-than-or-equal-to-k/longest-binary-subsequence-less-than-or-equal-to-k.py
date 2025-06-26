d994d9adace22005a90380be17e1aba1bdc3c972
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-06-26 13:03:25 +0530

    Time: 31 ms (17.89%) | Memory: 17.8 MB (66.67%) - LeetSync

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.py
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.py
@@ -3,24 +3,27 @@ class Solution:
         #BINARY SEARCH OVER THE ANSWER WHICH IS HERE THE LENGTH OF THE SUBSEQUENCE
 
         n = len(s)
-        def canit(x):
-            to_remove = n - x
-            val = 0
-            curr = 1
-            for c in s:
-                if c == "1" and to_remove > 0:
-                    to_remove -= 1
+        def possible(x):
+            to_remove=n-x
+            val=0
+            for i in s:
+                if i=="1" and to_remove>0:
+                    to_remove-=1
                 else:
-                    val <<= 1
-                    val += int(c) 
-                if val > k:
-                    return False
+                    val=(val<<1)+int(i)
+                    if val>k:
+                        return False
+
             return True
-        l, r = 0, n
-        while l <= r:
-            mid = (l + r) // 2
-            if canit(mid):
-                l = mid + 1
+
+        l,r=0,n
+        answer=0
+        while l<=r:
+            mid=(l+r)//2
+            if possible(mid):
+                l=mid+1
+                answer=mid
             else:
-                r = mid - 1
-        return r
+                r=mid-1
+
+        return answer
