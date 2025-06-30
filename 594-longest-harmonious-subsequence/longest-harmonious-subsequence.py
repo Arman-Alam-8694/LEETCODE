17f7df11b44cdb1f9977cde0285fa511b3c5a7a3
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-06-30 10:07:59 +0530

    Time: 22 ms (79.46%) | Memory: 19.2 MB (46.65%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.py
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.py
@@ -1,25 +1,21 @@
 class Solution:
     def findLHS(self, nums: List[int]) -> int:
-        nums.sort()
-        n=len(nums)
-        count=0
-        answer=0
-        # left,right=0,n-1
-        # while left<=right:
-        #     while right>=0 and nums[right]-nums[left]!=1:
-        #         right-=1
-        #     result=max(result,right-left+1)
-        #     left+=1
-
-        dictt=defaultdict(int)
-        left=0
-        for right in range(n):
-            while left<right and nums[right]-1>nums[left]:
-                left+=1
-            if nums[left]==nums[right]-1:
-                answer=max(answer,right-left+1)
-          
-        return answer
+        # nums.sort()
+        # n=len(nums)
+        # answer=0
+        # left=0
+        # for right in range(n):
+        #     while left<right and nums[right]-1>nums[left]:
+        #         left+=1
+        #     if nums[left]==nums[right]-1:
+        #         answer=max(answer,right-left+1)
+        # return answer
+        cnts=Counter(nums)
+        result=0
+        for i,j in cnts.items():
+            if i+1 in cnts:
+                result=max(result,cnts[i+1]+j)
+        return result
 
         # return result
 
