bf33f66bcaffc180e7448402f176c31819b2e804
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-07-08 11:55:01 +0530

    Time: 185 ms (20.74%) | Memory: 59.4 MB (5.12%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
@@ -1,23 +1,23 @@
 class Solution:
     def maxEvents(self, events: List[List[int]]) -> int:
-        events.sort()
-        min_heap=[]
-        i,day=0,0
-        n=len(events)
-        result=0
-        while i<n or min_heap:
-            if not min_heap:
-                day=events[i][0]
-            while i<n and events[i][0]<=day :
-                heapq.heappush(min_heap,events[i][1])
-                i+=1
-            while min_heap and min_heap[0] < day:
-                heapq.heappop(min_heap)
-            if min_heap:
-                heapq.heappop(min_heap)
-                day+=1
-                result+=1
-            else:
-                day+=1
-        return result
-            
+        events.sort(key=lambda x: x[1])  # Sort by end day
+        parent = {}
+
+        def find(x):
+            if x not in parent:
+                parent[x] = x
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+
+        def union(x, y):
+            parent[find(x)] = find(y)
+
+        count = 0
+        for start, end in events:
+            day = find(start)
+            if day <= end:
+                count += 1
+                union(day, day + 1)
+
+        return count
