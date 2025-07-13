5e4a5f765ce5afa4fc5d5a65352643c216654b65
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-07-13 12:30:02 +0530

    Time: 48 ms (5.41%) | Memory: 57.7 MB (62.33%) - LeetSync

---
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.java
@@ -0,0 +1,37 @@
+// class Solution:
+//     def matchPlayersAndTrainers(self, players: List[int], trainers: List[int]) -> int:
+//         players.sort()
+//         trainers.sort()
+//         result=0
+//         t=len(trainers)-1
+//         for i in range(len(players)-1,-1,-1):
+//             if t>=0 and trainers[t]>=players[i]:
+//                 result+=1
+//                 t-=1
+//             elif t<0:
+//                 break
+//         return result
+
+
+class Solution {
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+        Arrays.sort(players);
+        Arrays.sort(trainers);
+        System.out.println(Arrays.toString(players));
+        int t=trainers.length-1;
+        int result=0;
+        for(int i=players.length-1;i>=0;i--){
+            if(t>=0 && trainers[t]>=players[i]){
+                result+=1;
+                t-=1;
+            }
+            else if(t<0){
+                break;
+            }
+
+
+        }
+        return result;
+        
+    }
+}
