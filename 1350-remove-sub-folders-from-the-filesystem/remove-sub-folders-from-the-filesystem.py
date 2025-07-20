17fdf851f0e30d6f02e22adc9bc48fc4d0e31eb4
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-07-20 16:27:58 +0530

    Time: 138 ms (34.18%) | Memory: 30.5 MB (77.26%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -1,29 +1,25 @@
 class Solution:
-    def removeSubfolders(self, folder) -> list[str]:
-        # Create a set to store all folder paths for fast lookup
-        folder_set = set(folder)
-        result = []
-
-        # Iterate through each folder to check if it's a sub-folder
-        for f in folder:
-            is_sub_folder = False
-            prefix = f
-
-            # Check all prefixes of the current folder path
-            while not prefix == "":
-                pos = prefix.rfind("/")
-                if pos == -1:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        new_folder=set(folder)
+        answer=[]
+        for fol in folder:
+            prefix=list(fol)
+            found=False
+            while True:
+                while prefix and prefix[-1]!="/":
+                    prefix.pop()
+                if not prefix:
                     break
-
-                # Reduce the prefix to its parent folder
-                prefix = prefix[0:pos]
-
-                # If the parent folder exists in the set, mark as sub-folder
-                if prefix in folder_set:
-                    is_sub_folder = True
+                prefix.pop()
+                new="".join(prefix)
+                
+                if new in new_folder:
+                    found=True
                     break
-
-            # If not a sub-folder, add it to the result
-            if not is_sub_folder:
-                result.append(f)
-        return result
+            if not found:
+                answer.append(fol)
+        return answer
+            
+            
+            
+        
