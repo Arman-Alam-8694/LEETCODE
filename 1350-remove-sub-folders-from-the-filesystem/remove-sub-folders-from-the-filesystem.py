b5ec7ef6e0fae9dbb1cd70659864ac8691ed3c49
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-07-20 16:32:22 +0530

    Time: 27 ms (76.55%) | Memory: 30.6 MB (60.45%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -1,25 +1,35 @@
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
-        new_folder=set(folder)
-        answer=[]
-        for fol in folder:
-            prefix=list(fol)
-            found=False
-            while True:
-                while prefix and prefix[-1]!="/":
-                    prefix.pop()
-                if not prefix:
-                    break
-                prefix.pop()
-                new="".join(prefix)
+     
+       
+
+        #THIS SOLUTION WORKS BUT THIS IS COMPLEX
+
+        # new_folder=set(folder)
+        # answer=[]
+        # for fol in folder:
+        #     prefix=list(fol)
+        #     found=False
+        #     while True:
+        #         while prefix and prefix[-1]!="/":
+        #             prefix.pop()
+        #         if not prefix:
+        #             break
+        #         prefix.pop()
+        #         new="".join(prefix)
                 
-                if new in new_folder:
-                    found=True
-                    break
-            if not found:
-                answer.append(fol)
-        return answer
+        #         if new in new_folder:
+        #             found=True
+        #             break
+        #     if not found:
+        #         answer.append(fol)
+        # return answer
             
             
-            
+        folder.sort()
+        res=[]
+        for fol in folder:
+            if not res or not fol.startswith(res[-1]+"/"):
+                res.append(fol)
+        return res
         
