9cb37b257621746910a03ee41a67e7c9df5a34ed
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-07-30 15:18:32 +0530

    Time: 70 ms (39.05%) | Memory: 30.5 MB (97.63%) - LeetSync

---
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        max_and=max(nums)
+        result=1
+        temp=0
+        for i in nums:
+            if i==max_and:
+                temp+=1
+            else:
+                temp=0
+            result=max(result,temp)
+        return result
+        
