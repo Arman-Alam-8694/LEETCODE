2d3888b26c94376a72c241b2d806adfe6ad41103
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-08-04 18:19:46 +0530

    Time: 41 ms (90.14%) | Memory: 55.1 MB (62.92%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,3 +1,23 @@
+// class Solution:
+//     def totalFruit(self, fruits: List[int]) -> int:
+//         result=0
+//         mapp=defaultdict(int)
+//         left=0
+//         n=len(fruits)
+//         counts=0
+//         for right in range(n):
+//             mapp[fruits[right]]+=1
+//             counts+=1
+//             while len(mapp)>2:
+//                 mapp[fruits[left]]-=1
+//                 counts-=1
+//                 if mapp[fruits[left]]==0:
+//                     del mapp[fruits[left]]
+//                 left+=1
+//             result=max(result,counts)
+//         return result
+        
+
 class Solution {
     public int totalFruit(int[] fruits) {
         Map<Integer,Integer> mapp=new HashMap<>();
