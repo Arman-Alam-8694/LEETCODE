eeef9072af5bf168da3a47e545c219aaa45abf2c
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-08-05 14:36:31 +0530

    Time: 2 ms (61.24%) | Memory: 44.1 MB (99.25%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
@@ -2,17 +2,23 @@ class Solution {
     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
         int n = fruits.length;
         int unplaced = 0;
+        int leftMost=0;
+        int j=leftMost;
 
         for (int i = 0; i < n; i++) {
             boolean placed = false;
 
             // Always look from the start to find leftmost available basket
-            for (int j = 0; j < n; j++) {
+            for (j = leftMost; j < n; j++) {
                 if (baskets[j] != -1 && baskets[j] >= fruits[i]) {
                     baskets[j] = -1;  // Mark as used
                     placed = true;
                     break;
                 }
+            }
+            if(j==(leftMost)){
+                leftMost+=1;
+
             }
 
             if (!placed) {
