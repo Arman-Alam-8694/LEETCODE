fe9df02afc7b7f81c640ddaa4ababc9de3d3354e
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-08-15 17:29:48 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (35.19%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -1,20 +1,33 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
         if(n<=0){return false;}
-        String bin =Integer.toBinaryString(n);
-        int ones=0;
-        int zeroes=0;
-        for(int i=bin.length()-1;i>=0;i--){
-            if(bin.charAt(i)=='0'){
-                zeroes+=1;
-            }
-            else{
-                ones+=1;
-            }
-        }
-        if(ones==1 && (zeroes%2==0)){
+        if((n&(n-1))==0 && Integer.bitCount(n-1)%2==0){
             return true;
         }
         return false;
+        
     }
 }
+
+
+
+// class Solution {
+//     public boolean isPowerOfFour(int n) {
+//         if(n<=0){return false;}
+//         String bin =Integer.toBinaryString(n);
+//         int ones=0;
+//         int zeroes=0;
+//         for(int i=bin.length()-1;i>=0;i--){
+//             if(bin.charAt(i)=='0'){
+//                 zeroes+=1;
+//             }
+//             else{
+//                 ones+=1;
+//             }
+//         }
+//         if(ones==1 && (zeroes%2==0)){
+//             return true;
+//         }
+//         return false;
+//     }
+// }
