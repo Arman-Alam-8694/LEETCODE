530f818210c8ce110cc728e2682a1a93ca84c2b7
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-08-19 19:07:22 +0530

    Time: 41 ms (44.08%) | Memory: 28.2 MB (97.48%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.py
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.py
@@ -2,16 +2,11 @@ class Solution:
     def zeroFilledSubarray(self, nums: List[int]) -> int:
         result=0
         left=0
-        mapp=defaultdict(int)
         for right in range(len(nums)):
-            mapp[nums[right]]+=1
-            while len(mapp)>1 :
-                mapp[nums[left]]-=1
-                if mapp[nums[left]]==0:
-                    del mapp[nums[left]]
-                left+=1
-            if len(mapp)==1 and (0 in mapp.keys()):
+            if nums[right]==0:
                 result+=(right-left+1)
+            else:
+                left=right+1
         return result
             
 
