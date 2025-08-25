0594545d3e2681538b8900f5542fab64f999e217
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-08-25 12:52:13 +0530

    Time: 11 ms (63.77%) | Memory: 19.7 MB (86.08%) - LeetSync

---
+++ 498-diagonal-traverse/diagonal-traverse.py
@@ -0,0 +1,63 @@
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        row=len(mat)
+        col=len(mat[0])
+        start=[0,0]
+        answer=[]
+        count=0
+        total=row*col
+        while count<total:
+
+            #upside
+            while True:
+                curr=start[0]
+                curc=start[1]
+                answer.append(mat[curr][curc])
+                count+=1
+                nextr=curr-1
+                nextc=curc+1
+                start=[nextr,nextc]
+
+                
+                if nextc>=col:
+                    start=[nextr+2,nextc-1]
+                    break
+                elif nextr<0:
+                    start=[nextr+1,nextc]
+                    break
+            
+            if count>=total:
+                break
+            while True:
+                curr=start[0]
+                curc=start[1]
+                # print(answer)
+                # print(curr)
+                answer.append(mat[curr][curc])
+                count+=1
+                nextr=curr+1
+                nextc=curc-1
+                # print(answer)
+                # print(nextr,nextc)
+                start=[nextr,nextc]
+                if nextc<0 and nextr>=row:
+                    
+                    start=[nextr-1,nextc+2]
+                    break
+                elif nextc<0:
+                    
+                    start=[nextr,nextc+1]
+                    break
+                elif nextr>=row:
+                    # print(nextr,nextc)
+                    # print("in this one")
+                    start=[nextr-1,nextc+2]
+                    # print(start)
+                    break
+
+        return answer
+
+                
+
+
+        
