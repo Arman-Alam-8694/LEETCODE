b9dcb958d99158bb7db5817a60056cc4722e70fa
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-08-30 12:49:53 +0530

    Time: 3 ms (52.39%) | Memory: 44.2 MB (95.64%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        int row=board.length;
+        int col=board[0].length;
+        List<HashSet<Character>> rows= new ArrayList<>();
+        List<HashSet<Character>> cols= new ArrayList<>();
+        for(int i=0;i<row;i++){
+            rows.add(new HashSet<Character>());
+        }
+        for(int i=0;i<col;i++){
+            cols.add(new HashSet<Character>());
+        }
+
+        
+        for(int r=0;r<row;r+=3){
+            
+            for(int c=0;c<col;c+=3){
+                HashSet<Character> curr=new HashSet<>();
+                for(int tr=r;tr<r+3;tr++){
+                    for(int tc=c;tc<c+3;tc++){
+                        char value=board[tr][tc];
+                        // System.out.println(tr);
+                        // System.out.println(tc);
+                        
+                        // System.out.println(value);
+                        if(value!='.'){
+                            if(rows.get(tr).contains(value) | cols.get(tc).contains(value)|curr.contains(value)){
+                                // System.out.println(tr);
+                                // System.out.println(tc);
+                                // System.out.println(rows);
+                                // System.out.println(cols);
+                                return false;
+                            }
+                            rows.get(tr).add(value);
+                            cols.get(tc).add(value);
+                            curr.add(value);
+                        }
+
+                    }
+                }
+
+            }
+        }
+        return true;
+    }
+}
