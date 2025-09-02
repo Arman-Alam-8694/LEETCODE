579c691368196844af8a7fb1f693932e98dc7247
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-09-03 01:59:04 +0530

    Time: 3 ms (16.39%) | Memory: 133.7 MB (74.48%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
         int[] stack=new int[]{Integer.MAX_VALUE,Integer.MAX_VALUE,Integer.MAX_VALUE};
-        System.out.println(Arrays.toString(stack));
+       
         for(int i=0;i<nums.length;i++){
             if(stack[0]>=nums[i]){
                 stack[0]=nums[i];
