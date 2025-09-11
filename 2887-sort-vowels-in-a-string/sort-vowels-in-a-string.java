ed4b0ce748fc5d5961e9dfc6740a2a40eb5d95cf
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-09-11 23:27:23 +0530

    Time: 941 ms (5.79%) | Memory: 55.7 MB (5.04%) - LeetSync

---
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String sortVowels(String s) {
+        List<Integer> indexx=new ArrayList<>();
+        Set<Character> vowel=new HashSet<>(Arrays.asList('a','e','i','o','u','A','E','I','O','U'));
+
+        List<Character> chhar=new ArrayList<>();
+        for(int i=0;i<s.length();i++){
+            if(vowel.contains(s.charAt(i))){
+                indexx.add(i);
+                chhar.add(s.charAt(i));
+
+            }
+        }
+        Collections.sort(chhar);
+        System.out.println(chhar);
+        int pointer=0;
+        String answer="";
+        for(int i=0;i<s.length();i++){
+            if((indexx.size()>pointer) && (indexx.get(pointer)==i)){
+                answer+=chhar.get(pointer);
+                pointer+=1;
+            }else{
+                answer+=s.charAt(i);
+            }
+
+        }
+        return answer;
+
+       
+
+        
+    }
+}
