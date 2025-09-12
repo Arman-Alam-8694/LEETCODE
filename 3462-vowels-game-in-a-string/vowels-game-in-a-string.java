db8f269edd85f1fe540c2efe21c7f977b08081d8
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-09-12 13:35:53 +0530

    Time: 2 ms (100.00%) | Memory: 45.6 MB (45.41%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
@@ -1,15 +1,13 @@
 class Solution {
     public boolean doesAliceWin(String s) {
-        Set<Character> vowels=new HashSet<>(Arrays.asList('a','e','i','o','u'));
-        for(int i=0;i<s.length();i++){
-            if(vowels.contains(s.charAt(i))){
-                return true;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            switch (c) {
+                case 'a','e','i','o','u':
+                    return true;
             }
         }
+
         return false;
-
-
-        
-        
     }
 }
