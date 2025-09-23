3cdf66c64a7e220e3ffd97ca4beb9629dc49dadd
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-09-23 12:46:17 +0530

    Time: 3 ms (7.90%) | Memory: 42 MB (6.38%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.java
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -1,27 +1,25 @@
+import java.util.*;
+
 class Solution {
     public int compareVersion(String version1, String version2) {
-   
-        List<Integer> val=Arrays.stream(version1.split("\\.")).map(Integer::parseInt).toList();
-        List<Integer> val2=Arrays.stream(version2.split("\\.")).map(Integer::parseInt).toList();
-        int size=val.size()>val2.size()?val.size():val2.size();
-        for(int i=0;i<size;i++){
-            int first=0;
-            int second=0;
-            if(i<val.size()){
-                first=val.get(i);
-            }
-            if(i<val2.size()){
-                second=val2.get(i);
-            }
-            if(first>second){
-                return 1;
-            }else if(second>first){
-                return -1;
-            }
+        int[] v1 = Arrays.stream(version1.split("\\."))
+                         .mapToInt(Integer::parseInt)
+                         .toArray();
 
+        int[] v2 = Arrays.stream(version2.split("\\."))
+                         .mapToInt(Integer::parseInt)
+                         .toArray();
+
+        int size = Math.max(v1.length, v2.length);
+
+        for (int i = 0; i < size; i++) {
+            int first = (i < v1.length) ? v1[i] : 0;
+            int second = (i < v2.length) ? v2[i] : 0;
+
+            if (first > second) return 1;
+            if (first < second) return -1;
         }
-     
+
         return 0;
-        
     }
 }
