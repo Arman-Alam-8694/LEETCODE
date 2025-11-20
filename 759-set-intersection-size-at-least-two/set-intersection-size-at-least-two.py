214c566c4acdfa8696165d170ae304f3bd74aac5
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-11-21 00:01:50 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (32.41%) - LeetSync

--- 759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.py
+++ 759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.py
@@ -1,14 +1,13 @@
 class Solution:
     def intersectionSizeTwo(self, intervals: List[List[int]]) -> int:
         sortt=sorted(intervals,key=lambda a:(a[1],-a[0]))
-        answer=[]
+        answer=0
         a,b=-1,-1
         for i in sortt:
             if a==-1 and b==-1:
                 a=i[1]-1
                 b=i[1]
-                answer.append(a)
-                answer.append(b)
+                answer+=2
             else:
                 if (i[0]<=a<=i[1]) and (i[0]<=b<=i[1] ):
                     continue
@@ -16,8 +15,7 @@ class Solution:
                 elif (not i[0]<=a<=i[1]) and (not i[0]<=b<=i[1] ):
                     a=i[1]-1
                     b=i[1]
-                    answer.append(a)
-                    answer.append(b)
+                    answer+=2
 
                     
                 
@@ -25,17 +23,17 @@ class Solution:
                 elif i[1]==b:
             
                     a=i[1]-1
-                    answer.append(a)
+                    answer+=1
                 else:
                     temp=b
                     b=i[1]
                     a=temp
-                    answer.append(b)
+                    answer+=1
                     
 
                     
 
                 
        
-        return len(answer)
+        return answer
         
