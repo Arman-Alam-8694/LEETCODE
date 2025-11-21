3fdd8649cbecdc4c56c1e857e8e774c3ece2bfac
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-11-21 21:23:30 +0530

    Time: 1335 ms (27.10%) | Memory: 18.3 MB (39.64%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -1,10 +1,37 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        return sum([len(set(s[s.index(letter)+1:s.rindex(letter)])) for letter in set(s)])
+        chars=set(s)
+        
+        answer=0
+        done=set()
+        for i in chars:
+            new=set()
+            Found=False
+            for j in range(len(s)):
+                if s[j]==i and Found:
+                    
+                    if len(new)!=0:
+                        done.update(new)
+                        new=set()
+                        new.add(i)
+                    else:
+                        new.add(i)
+                    
+                elif s[j]==i:
+                    Found=True
+                elif Found:
+                    if s[j] not in done:
+                        new.add(s[j])
+            answer+=len(done)
+            done=set()
+        return answer
+
+
+                    
 
 
 
 
-# class Solution:
-#     def countPalindromicSubsequence(self, s: str) -> int:
-#         return sum([len(set(s[s.index(letter)+1:s.rindex(letter)])) for letter in set(s)])
+
+
+        
