f87fde17edc8174617f1daa67f3e7d1cb0dbd464
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-11-27 14:27:20 +0530

    Time: 329 ms (62.76%) | Memory: 50 MB (33.10%) - LeetSync

--- 3653-maximum-subarray-sum-with-length-divisible-by-k/maximum-subarray-sum-with-length-divisible-by-k.py
+++ 3653-maximum-subarray-sum-with-length-divisible-by-k/maximum-subarray-sum-with-length-divisible-by-k.py
@@ -1,28 +1,17 @@
 class Solution:
     def maxSubarraySum(self, nums: List[int], k: int) -> int:
         n = len(nums)
-
-        # build prefix array
         prefix = [0] * (n + 1)
         for i in range(n):
             prefix[i+1] = prefix[i] + nums[i]
-
+     
         dp = [float('-inf')] * (n + 1)
         ans = float('-inf')
 
-        # process starting positions of blocks
-        for i in range(0, n - k + 1):
-            block = prefix[i+k] - prefix[i]   # sum of nums[i : i+k]
-
-            if i - k >= 0:
-                dp[i] = max(block, dp[i-k] + block)
-            else:
-                dp[i] = block  # first possible block (cannot extend)
-
+        for i in range(k-1 ,n):
+            blockSum = prefix[i+1] - prefix[i - k+1]
+            dp[i] = max(blockSum, dp[i - k] + blockSum)
             ans = max(ans, dp[i])
 
         return ans
-
-        
-        
-        
+            
