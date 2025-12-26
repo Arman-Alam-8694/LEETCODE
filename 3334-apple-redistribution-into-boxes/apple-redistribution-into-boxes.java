bded7e62dfb40c76ef02ed94330bde72fb2d54e7
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-12-26 12:07:16 +0530

    Time: 8 ms (5.43%) | Memory: 45 MB (5.73%) - LeetSync

--- 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.java
+++ 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.java
@@ -1,19 +1,22 @@
 class Solution {
     public int minimumBoxes(int[] apple, int[] capacity) {
-        List<Integer> c=IntStream.of(capacity).mapToObj(Integer::valueOf).sorted((a,b)->b-a).toList();
-        int total=IntStream.of(apple).sum();
-        int temp=0;
-       
-        for(int i=0;i<c.size();i++){
-            temp+=c.get(i);
-            if(temp>=total){
-                return i+1;
+
+        int total = IntStream.of(apple).sum();
+
+        // Sort primitive array (much faster)
+        Arrays.sort(capacity); // ascending
+
+        int temp = 0;
+        int count = 0;
+
+        // Traverse from largest to smallest
+        for (int i = capacity.length - 1; i >= 0; i--) {
+            temp += capacity[i];
+            count++;
+            if (temp >= total) {
+                return count;
             }
-
         }
-        return c.size();
-
-
-        
+        return count;
     }
 }
