58b29fa820fa6119d666866acc073294e1ec4b92
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2025-12-26 12:48:30 +0530

    Time: 1431 ms (5.40%) | Memory: 44.2 MB (62.84%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.py
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.py
@@ -1,19 +1,44 @@
 class Solution:
-    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+    def maximumHappinessSum(self, happiness, k):
+        def radix_sort(nums):
+            if not nums:
+                return nums
 
-        happiness.sort(reverse=True)
-        r=0
-        ans=0
-        for i in happiness:
-            if (i-r)<=0:
-                return ans
-            ans+=(i-r)
-            r+=1
-            k-=1
-            if k==0:
-                return ans
+            max_val = max(nums)
+            exp = 1  # 1, 10, 100, ...
+
+            while max_val // exp > 0:
+                count = [0] * 10
+                output = [0] * len(nums)
+
+                # Count digits
+                for num in nums:
+                    digit = (num // exp) % 10
+                    count[digit] += 1
+
+                # Prefix sum
+                for i in range(1, 10):
+                    count[i] += count[i - 1]
+
+                # Stable placement (right to left)
+                for i in range(len(nums) - 1, -1, -1):
+                    digit = (nums[i] // exp) % 10
+                    count[digit] -= 1
+                    output[count[digit]] = nums[i]
+
+                nums = output
+                exp *= 10
+
+            return nums
+
+        happiness = radix_sort(happiness)
+
+        ans = 0
+        for i in range(k):
+            val = happiness[-1 - i] - i
+            if val <= 0:
+                break
+            ans += val
+
         return ans
 
-    
-
-        
