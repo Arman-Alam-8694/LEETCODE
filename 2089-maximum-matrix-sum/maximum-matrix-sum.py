37bbdea4c1743ce8c3c74b41c3b8db58e8135adf
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2026-01-05 14:57:26 +0530

    Time: 116 ms (12.03%) | Memory: 26.7 MB (15.19%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.py
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.py
@@ -3,24 +3,17 @@ class Solution:
         bigN=float("inf")
         neg=0
         possum=0
-    
         n=len(matrix)
         for i in range(n):
             for j in range(n):
                 if matrix[i][j]<0:
                     neg+=1
-
-             
                 bigN=min(bigN,abs(matrix[i][j]))
-                
                 possum+=abs(matrix[i][j])
-               
-
-    
+            
         if neg%2==0:
             return possum
         else:
-        
             return possum+(-bigN)+(-bigN)
 
         
