941579b548b759d7313bd74412bd725785568be7
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2026-01-05 14:48:40 +0530

    Time: 97 ms (43.04%) | Memory: 26.8 MB (6.96%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.py
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.py
@@ -1,34 +1,26 @@
 class Solution:
     def maxMatrixSum(self, matrix: List[List[int]]) -> int:
-        bigN=float("-inf")
+        bigN=float("inf")
         neg=0
-        negsum=0
         possum=0
     
         n=len(matrix)
-        zero=0
         for i in range(n):
             for j in range(n):
                 if matrix[i][j]<0:
                     neg+=1
-                    negsum+=matrix[i][j]
-                    bigN=max(bigN,matrix[i][j])
-                
 
-                elif matrix[i][j]==0:
-                    zero+=1
-                else:
-                    bigN=max(bigN,-matrix[i][j])
+             
+                bigN=min(bigN,abs(matrix[i][j]))
                 
-                    possum+=matrix[i][j]
+                possum+=abs(matrix[i][j])
                
 
-        total=possum+((negsum)*(-1))
     
-        if neg%2==0 or zero>=(neg%2):
-            return total
+        if neg%2==0:
+            return possum
         else:
         
-            return total+bigN+bigN
+            return possum+(-bigN)+(-bigN)
 
         
