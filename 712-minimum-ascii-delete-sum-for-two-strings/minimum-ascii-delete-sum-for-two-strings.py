382747d2b05cae698999fec798d1fa4c21a6eb13
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2026-01-10 13:34:25 +0530

    Time: 262 ms (83.03%) | Memory: 24.1 MB (24.47%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
@@ -3,18 +3,18 @@ class Solution:
         n=len(s1)
         m=len(s2)
 
-        @cache
-        def dp(x,y):
-            if x==n:
-                return sum(map(ord,s2[y:]))
-            if y==m:
-                return sum(map(ord,s1[x:]))
-            if s1[x]==s2[y]:
-                return dp(x+1,y+1)
+        # @cache
+        # def dp(x,y):
+        #     if x==n:
+        #         return sum(map(ord,s2[y:]))
+        #     if y==m:
+        #         return sum(map(ord,s1[x:]))
+        #     if s1[x]==s2[y]:
+        #         return dp(x+1,y+1)
            
-            first=ord(s1[x])+dp(x+1,y)
-            second=ord(s2[y])+dp(x,y+1)
-            return min(first,second)
+        #     first=ord(s1[x])+dp(x+1,y)
+        #     second=ord(s2[y])+dp(x,y+1)
+        #     return min(first,second)
 
         dp=[[0]*(m+1) for _ in range(n+1)]
         for i in range(1,n+1):
