5a9766ed18c3628cb671d278ce222d9701bbd716
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2026-01-10 13:34:13 +0530

    Time: 293 ms (77.32%) | Memory: 24.1 MB (24.92%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
@@ -15,7 +15,19 @@ class Solution:
             first=ord(s1[x])+dp(x+1,y)
             second=ord(s2[y])+dp(x,y+1)
             return min(first,second)
-        return dp(0,0)
+
+        dp=[[0]*(m+1) for _ in range(n+1)]
+        for i in range(1,n+1):
+            dp[i][0]=ord(s1[i-1])+dp[i-1][0]
+        for i in range(1,m+1):
+            dp[0][i]=ord(s2[i-1])+dp[0][i-1]
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if s1[i-1]==s2[j-1]:
+                    dp[i][j]+=dp[i-1][j-1]
+                else:
+                    dp[i][j]=min(ord(s1[i-1])+dp[i-1][j],ord(s2[j-1])+dp[i][j-1])
+        return dp[n][m]
 
             
             
