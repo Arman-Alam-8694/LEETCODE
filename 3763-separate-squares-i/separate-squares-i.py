9751c8386fa054d7e205307bec8b3137662a7e61
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2026-01-13 09:56:33 +0530

    Time: 2154 ms (53.95%) | Memory: 48.5 MB (16.42%) - LeetSync

---
+++ 3763-separate-squares-i/separate-squares-i.py
@@ -0,0 +1,26 @@
+class Solution:
+    def separateSquares(self, squares: List[List[int]]) -> float:
+        start,end=0,10**9+1
+        total_area=0
+        n=len(squares)
+        for i in range(n):
+            x,y,l=squares[i]
+            total_area+=l*l
+        target=total_area/2.0
+
+        while end-start>0.000001:
+            mid=(start+end)/2
+            tarea=0.0
+            for x,y,l in squares:
+                if mid<=y:
+                    continue
+                elif mid>=(y+l):
+                    tarea+=(l*l)
+                else:
+                    tarea+=(mid-y)*l
+            if tarea<target:
+                start=mid
+            else:
+                end=mid
+
+        return start
