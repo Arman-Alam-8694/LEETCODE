0e9dafd5798cf3c2e3857deb0a14ffb72e0a5111
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2026-02-01 13:56:36 +0530

    Time: 3 ms (35.86%) | Memory: 19.5 MB (15.15%) - LeetSync

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.py
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.py
@@ -1,25 +1,14 @@
 class Solution:
     def minimumCost(self, nums: List[int]) -> int:
-        counts=[0]*51
-        for i in nums:
-            counts[i]+=1
-        start=1
-        top=[]
-        while start<51:
-            while counts[start]!=0 and len(top)!=3:
-                top.append(start)
-                counts[start]-=1
-            if len(top)==3:
-                break
-            start+=1
-     
-        result=0
-        left=2
-        curr=nums[0]
-        if curr in top:
-            return sum(top)
-        else:
-            return curr+top[0]+top[1]
+        first,second=float("inf"),float("inf")
+        for i in range(1,len(nums)):
+            if nums[i]<first:
+                second=first
+                first=nums[i]
+            elif nums[i]<second:
+                second=nums[i]
+        return nums[0]+first+second
+
 
         
         
