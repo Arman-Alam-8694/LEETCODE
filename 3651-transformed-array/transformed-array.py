4b0de641cf209bbc09192c19bc45e388993d51fb
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2026-02-05 22:40:53 +0530

    Time: 59 ms (61.88%) | Memory: 19.4 MB (10.76%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def constructTransformedArray(self, nums: List[int]) -> List[int]:
+        n=len(nums)
+        result=[0]*n
+        for i in range(n):
+            val=nums[i]
+ 
+            if val>0:
+                nxt=(i+val)%n
+                result[i]=nums[nxt]
+
+            elif val<0:
+                nxt=(((n-1)-i)-val)%n
+                result[i]=nums[(n-1)-nxt]
+            else:
+                result[i]=0
+        return result
+            
+
+    # 0 1 2 3 4 5
+
+        
