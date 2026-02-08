2346c16d29d90cebb54327e7fea589d111668182
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2026-02-08 16:06:44 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (5.56%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -8,25 +8,24 @@ class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         def dfs(node):
             if not node:
-                return 0
+                return True,0
             ll,rr=0,0
-            if node.left:
-                yes,lval=dfs(node.left)
-                if not yes:
-                    return False,lval
-                ll=1+lval
-            if node.right:
-                ryes,rval=dfs(node.right)
-                if not ryes:
-                    return False,rval
-                rr=1+rval
+           
+            yes,lval=dfs(node.left)
+            if not yes:
+                return False,lval
+            ll=1+lval
+          
+            ryes,rval=dfs(node.right)
+            if not ryes:
+                return False,rval
+            rr=1+rval
             temp=max(ll,rr)
             if abs(ll-rr)>1:
                 return False,temp
             else:
                 return True,temp   
-        if not root:
-            return True
+      
         res,val=dfs(root)
         return res             
         
