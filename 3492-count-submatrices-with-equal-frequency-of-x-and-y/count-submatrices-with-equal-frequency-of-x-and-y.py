e365c56cc35b4ce5cf2059dcdaf9ce663f1cf9ab
__ARMXN__ <143007837+Arman-Alam-8694@users.noreply.github.com>
2026-03-19 23:15:58 +0530

    Time: 442 ms (78.49%) | Memory: 103.5 MB (96.77%) - LeetSync

---
+++ 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.py
@@ -0,0 +1,30 @@
+class Solution:
+    def numberOfSubmatrices(self, grid: List[List[str]]) -> int:
+        row=len(grid)
+        col=len(grid[0])
+        store=[[0,0]  for i in range(col)]
+        print(store)
+        res=0
+
+        def check(curr):
+            if curr=="X":
+                return 0
+            elif curr=="Y":
+                return 1
+            else:
+                return -1
+        for i in range(row):
+            rsum=[0,0]
+            for j in range(col):
+               
+                temp=check(grid[i][j])
+                if temp!=-1:
+                    store[j][temp]=(store[j][temp]+1)
+                rsum[0]+=store[j][0]
+                rsum[1]+=store[j][1]
+                if rsum[0]==rsum[1] and rsum[0]>=1:
+                    res+=1
+
+                        
+        return res
+        
